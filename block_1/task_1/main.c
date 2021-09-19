e209c5909b66c0c04f4cdbd21254cdf4a014f1db
airh4ck <dudnikoff98@gmail.com>
2021-09-19 19:38:49 +0300

    Added solution and build scripts for Task #1

--- block_1/task_1/main.c
+++ block_1/task_1/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int is_prime(long long n) {
+    if (n == 1)
+        return 0;
 
+    for (long long j = 2; j * j <= n; j++) 
+    {
+        if (n % j == 0) 
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    printf("This programm prints out the Mersenne prime numbers\n");
+
+
+    for (long long i = 2; i <= (1LL << 31); i *= 2)
+    {
+        if (is_prime(i - 1)) 
+        {
+            printf("%lld\n", i - 1);
+        }
+    }
     return 0;
 }
