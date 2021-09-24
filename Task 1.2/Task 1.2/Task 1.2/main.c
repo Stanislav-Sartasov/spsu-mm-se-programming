cd44a51f3ced2275c3d22a0668e042c960df9877
arthur100500 <57834711+arthur100500@users.noreply.github.com>
2021-09-24 19:32:33 +0300

    Custom input implemented

--- Task 1.2/Task 1.2/Task 1.2/main.c
+++ Task 1.2/Task 1.2/Task 1.2/main.c
@@ -3,8 +3,7 @@
 // The function checks if two integers are coprime
 int coprime(int first, int second)
 {
-    // If two numbers are coprime then their GCD is 1
-    while (first != 0 && second != 0)
+    while(first != 0 && second != 0)
     {
         if (second > first)
             second %= first;
@@ -17,16 +16,59 @@ int coprime(int first, int second)
         return 0;
 }
 
+// Fills given array with counted angles
+void get_angles(double* out_angles, float a, float b, float c)
+{
+    // Cosine theorem is used here to determine angles
+    out_angles[0] = (float)acos((a * a + b * b - c * c) / (2.0 * a * b));
+    out_angles[1] = (float)acos((b * b + c * c - a * a) / (2.0 * c * b));
+    out_angles[2] = (float)acos((a * a + c * c - b * b) / (2.0 * a * c));
+    // Normalize angles from rad to degrees
+    for (int i = 0; i < 3; i++)
+    {
+        out_angles[i] = out_angles[i] * 180.0 / 3.14159265352f;
+    }
+}
+
+float my_scanf_decimal(const char* message)
+{
+    // Output message
+    printf(message);
+    int result;
+    int scanf_result;
+    // Endless loop awaiting user input
+    while (1) {
+        // Check if scanf was a success
+        if (!scanf_s("%d", &result))
+        {
+            getchar();
+        }
+        // Check if number is greater than zero
+        if (result < 0) {
+            printf("Number should be greater than zero and be a number\nInput again:");
+            continue;
+        }
+        // End the loop
+        break;
+    }
+    return result;
+}
 int main()
 {
     int a, b, c;
+    // Information output
     printf("This programs checks if 3 inputted numbers are coprime Pythagorean numbers\n");
-    printf("Please input 3 numbers\n");
-    scanf_s("%d%d%d", &a, &b, &c);
+    printf("Please, input 3 numbers\n");
+    
+    // Input
+    a = my_scanf_decimal("Input first number:");
+    b = my_scanf_decimal("Input second number:");
+    c = my_scanf_decimal("Input third number:");
+    
     // Pythagorean triple check
     if (a * a + b * b == c * c || a * a + c * c == b * b || c * c + b * b == a * a)
         // Coprime check
-        if (coprime(a, b) && coprime(b, c) && coprime(c, a))
+        if(coprime(a, b) && coprime(b, c) && coprime(c, a))
             printf("These numbers are coprime Pythagorean triple");
         else
             printf("These numbers are not coprime, but are Pythagorean triple");
