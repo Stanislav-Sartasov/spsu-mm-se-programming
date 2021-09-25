3d33004854e2b398d4b0d02ef2b11ffd78077e5a
SmukerSPB <st096822@student.spbu.ru>
2021-09-25 16:46:28 +0300

    Russian: Добавлены модульи для проверкзанчений со стороны пользователя (Задание 2) English: Added modules for user side checks (Task 2)

--- Module1/Task2/Task_2.c
+++ Module1/Task2/Task_2.c
@@ -3,6 +3,8 @@
 #include <stdio.h> 
 #include <math.h>
 #include <stdbool.h>
+#include <limits.h>
+#define CLI_BUFFER_SIZE 12
 
 bool check_number(int number)
 {
@@ -17,25 +19,98 @@ bool check_number(int number)
 
 bool check_Pifagor(int a, int b, int c)
 {
-    if (pow(a, 2) + pow(b, 2) == pow(c, 2))
+    if (a > 0 && b > 0 && c > 0)
     {
-        return true;
+
+        if (pow(a, 2) + pow(b, 2) == pow(c, 2))
+        {
+            return true;
+        }
     }
     return false;
 }
+
+bool check_error(int *value)
+{
+    size_t length = 0;
+    char* end = NULL;
+    char buf[CLI_BUFFER_SIZE] = "";
+
+
+
+    fflush(stdout);
+
+
+    if (!fgets(buf, sizeof(buf), stdin)) {
+        return false;
+    }
+
+    
+    length = strlen(buf);
+    if (buf[length - 1] == '\n') 
+    {
+        buf[--length] = '\0';
+
+      
+        errno = 0;
+        *value = strtod(buf, &end);
+
+    
+        if (length == 0) {
+            printf("Ошибка: введена пустая строка.\n");
+            return false;
+        }
+        if (errno != 0 || *end != '\0') {
+            printf("Ошибка: некорректный символ.\n");
+            printf("\t%s\n", buf);
+            printf("\t%*c\n", (int)(end - buf) + 1, '^');
+            
+            return false;
+        }
+
+        if (INT_MAX<*value || INT_MIN >= *value)
+        {
+            printf("Ошибка: целочисленное переполнение параметра .\n");
+            return false;
+        }
+            
+    }
+    else 
+    {
+        scanf("%*[^\n]");
+        scanf("%*c");
+        printf("Ошибка: не вводите больше чем %d символа(ов).\n", CLI_BUFFER_SIZE-2);
+
+        return false;
+    }
+
+    return true;
+}
+
+void check_enter(int *cislo)
+{
+    bool status = false;
+   
+    do {
+        status = check_error(cislo);
+        if (!status) {
+            printf("Пожалуйста, попробуйте еще раз.\n");
+        }
+    } while (!status);
+}
+
 
 
 int main()
 {
     system("chcp 1251");
     system("cls");
-    int a = 0, b = 0, c = 0;
+    int a = 0 , b =0 , c = 0 ;
 
     printf("Пожалуйста, задайте тройку натуральных чисел: \n");
-    printf("a = "); scanf("%d", &a);
-    printf("b = "); scanf("%d", &b);
-    printf("c = "); scanf("%d", &c);
-
+    printf("a = "); check_enter(&a);
+    printf("b = "); check_enter(&b);
+    printf("c = "); check_enter(&c);
     printf("Результат анализ введенных значений: \n");
 
     if (check_Pifagor(a, b, c))
--- Module1/Task3/Task_3.c
+++ Module1/Task3/Task_3.c
@@ -4,13 +4,16 @@
 #include <stdio.h> 
 #include <math.h>
 #include <stdbool.h>
+#include <limits.h>
+
+#define CLI_BUFFER_SIZE  10
 
 
-bool CheckTangle(double* angle)
+bool CheckTangle(double a,double b,double c )
 {
-    if (angle[0] > 0 && angle[1] > 0 && angle[2] > 0)
+    if (a > 0 && b > 0 &&  c>0)
     {
-        if (angle[0] + angle[1] > angle[2] && angle[0] + angle[2] > angle[1] && angle[2] + angle[1] > angle[1])
+        if (a + b > c && a + c > b && c + b > a)
         {
             return true;
         }
@@ -25,33 +28,100 @@ double Result_angle(double a, double b, double c)
 {
     return acos((pow(a, 2) + pow(b, 2) - pow(c, 2)) / (2 * a * b)) * 180 / M_PI;
 }
+
+
+bool check_error(double *value)
+{
+    size_t length = 0;
+    char* end = NULL;
+    char buf[CLI_BUFFER_SIZE] = "";
+
+
+
+    fflush(stdout);
+
+
+    if (!fgets(buf, sizeof(buf), stdin)) {
+        return false;
+    }
+
+
+    length = strlen(buf);
+    if (buf[length - 1] == '\n')
+    {
+        buf[--length] = '\0';
+
+
+        errno = 0;
+        *value = strtod(buf, &end);
+
+
+        if (length == 0) {
+            printf("Ошибка: введена пустая строка.\n");
+            return false;
+        }
+        if (errno != 0 || *end != '\0') {
+            printf("Ошибка: некорректный символ.\n");
+            printf("\t%s\n", buf);
+            printf("\t%*c\n", (int)(end - buf) + 1, '^');
+
+            return false;
+        }
+
+    
+
+    }
+    else
+    {
+        scanf("%*[^\n]");
+        scanf("%*c");
+        printf("Ошибка: не вводите больше чем %d символа(ов).\n", CLI_BUFFER_SIZE - 2);
+
+        return false;
+    }
+
+    return true;
+}
+
+void check_enter(double* cislo)
+{
+    bool status = false;
+
+    do {
+        status = check_error(cislo);
+        if (!status) {
+            printf("Пожалуйста, попробуйте еще раз.\n");
+        }
+    } while (!status);
+}
+
+
+
 int main()
 {
-    double  angle[3];
+ 
+    double a = 0, b = 0, c = 0;
+
     bool error = false;
     system("chcp 1251");
     system("cls");
 
     printf("Пожалуйста, задайте 3 стороны треугольника: \n");
-    for (int i = 0; i < 3; i++)
-    {
-        printf("Введите велечену стороны № %d -", i); scanf("%lf", &angle[i]);
-    }
+    printf("Введите велечену стороны № 1 -"); check_enter(&a);
+    printf("Введите велечену стороны № 2 -"); check_enter(&b);
+    printf("Введите велечену стороны № 3 -"); check_enter(&c);
+    
 
-    if (CheckTangle(angle))
+   if (CheckTangle(a,b,c))
     {
-        double a = 0, b = 0, c = 0;
-        a = angle[0];
-        b = angle[1];
-        c = angle[2];
-
+       
         printf("Угол № 1 - %.2lf \n", Result_angle(a, c, b));
         printf("Угол № 2 - %.2lf \n", Result_angle(a, b, c));
         printf("Угол № 3 - %.2lf \n", Result_angle(b, c, a));
     }
     else
     {
-        printf("К сожалению, построить невырожденный треугольник с указанными сторонами невозможно!");
+        printf("\n Ответ: К сожалению, построить невырожденный треугольник с указанными сторонами невозможно! \n");
     }
     return 0;
 }
