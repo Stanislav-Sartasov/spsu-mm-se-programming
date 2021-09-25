fbea1ae2f2c9b1d778caacf7cc9aecf1941bf0cc
YITSUKEN <gametopsss@gmail.com>
2021-09-25 15:55:58 +0300

    Added solution for the first task

---
+++ AlexanderKharitonov/First/1.Mersenne numbers/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool is_prime_number(int number) { //Default prime check
+    if (number == 1)
+        return false;
+    for (int i = 2; i <= sqrt(number); i++) {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool Lucas_Lehmer_primality_test(int p) { //Lucas–Lehmer primality test https://en.wikipedia.org/wiki/Lucas%E2%80%93Lehmer_primality_test
+    if (p == 2)
+        return true;
+    if (!is_prime_number(p))
+        return false;
+    const long long unsigned m_p = (long long unsigned) pow(2, p) - 1;
+    long long unsigned s = 4;
+    int i;
+    for (i = 3; i <= p; i++) {
+        s = (s * s - 2) % m_p;
+    }
+    return s == 0;
+}
+
+int main() {
+    for (int i = 2; i <= 31; i++) {
+        if (Lucas_Lehmer_primality_test(i)) {
+            printf("%lld ", (long long) pow(2, i) - 1);
+        }
+    }
+}
