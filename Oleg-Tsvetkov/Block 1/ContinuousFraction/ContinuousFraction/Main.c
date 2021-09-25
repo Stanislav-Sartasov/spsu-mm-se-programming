72047bdcdfca9a0e038ea0cc784f65dc23398291
Copysiper <oleg-tsv@yandex.ru>
2021-09-25 19:48:55 +0300

    Fixed codestyle for all tasks for 2.4

--- Oleg-Tsvetkov/Block 1/ContinuousFraction/ContinuousFraction/Main.c
+++ Oleg-Tsvetkov/Block 1/ContinuousFraction/ContinuousFraction/Main.c
@@ -4,80 +4,80 @@
 
 void clear_excess_chars()
 {
-    char temp;
-    do
-    {
-        temp = getchar();
-    } 
-    while (temp != '\n' && temp != EOF);
+	char temp;
+	do
+	{
+		temp = getchar();
+	} 
+	while (temp != '\n' && temp != EOF);
 }
 
 long long get_number()
 {
-    short success = 0;
-    long long result;
-    char after_number;
-    do
-    {
-        printf("Введите число: ");
-        if (scanf_s("%10lld%c", &result, &after_number, 10) != 2 || after_number != '\n')
-        {
-            printf("Ошибка! К вводу принимаются только нат. числа. Они также не должны вызывать переполнения\n");
-            clear_excess_chars(); //Если не удалось сосканировать число, пропускаем лишние символы до след. строки
-        }
-        else if (result <= 0)
-        {
-            printf("Ошибка! Введённое число должно быть больше нуля\n");
-        }
-        else if (sqrtl(result) - (long long)sqrtl(result) < 0.000001)
-        {
-            printf("Ошибка! Введённое число не должно быть квадратом целого числа\n");
-        }
-        else
-        {
-            success = 1;
-        }
+	short success = 0;
+	long long result;
+	char after_number;
+	do
+	{
+		printf("Введите число: ");
+		if (scanf_s("%10lld%c", &result, &after_number, 10) != 2 || after_number != '\n')
+		{
+			printf("Ошибка! К вводу принимаются только нат. числа. Они также не должны вызывать переполнения\n");
+			clear_excess_chars(); //Если не удалось сосканировать число, пропускаем лишние символы до след. строки
+		}
+		else if (result <= 0)
+		{
+			printf("Ошибка! Введённое число должно быть больше нуля\n");
+		}
+		else if (sqrtl(result) - (long long)sqrtl(result) < 0.000001)
+		{
+			printf("Ошибка! Введённое число не должно быть квадратом целого числа\n");
+		}
+		else
+		{
+			success = 1;
+		}
 
-    } 
-    while (!success);
-    return result;
+	} 
+	while (!success);
+	return result;
 }
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    printf("Описание: Программа получает число от пользователя, берёт корень, и говорит\n");
-    printf("период цепной дроби, а также представляет его элементы.\n");
-    long long start_number;
-    start_number = get_number();
-    long long period[50000];
-    int period_length = 1;
-    period[0] = (long long)sqrtl(start_number);
-    long long element, substract = period[0], divider = 1;
-    do
-    {
-        divider = (start_number - substract * substract) / divider;
-        element = (long long)((substract + period[0]) / divider);
-        period[period_length] = element;
-        ++period_length;
-        substract = element * divider - substract;
-    } 
-    while (divider != 1);
-    printf("Период: %d ", period_length - 1);
-    printf("Последовательность: [%lld; ", period[0]);
-    for (int i = 1; i < period_length; ++i)
-    {
-        if (i != period_length - 1)
-        {
-            printf("%lld,", period[i]);
-        }
-        else
-        {
-            printf("%lld", period[i]);
-        }
-    }
-    printf("]");
+	setlocale(LC_ALL, "Russian");
+	printf("Описание: Программа получает число от пользователя, берёт корень, и говорит\n");
+	printf("период цепной дроби, а также представляет его элементы.\n");
+	long long start_number;
+	start_number = get_number();
+	long long period[50000];
+	int period_length = 1;
+	period[0] = (long long)sqrtl(start_number);
+	long long element, substract = period[0], divider = 1;
+	do
+	{
+		divider = (start_number - substract * substract) / divider;
+		element = (long long)((substract + period[0]) / divider);
+		period[period_length] = element;
+		++period_length;
+		substract = element * divider - substract;
+	} 
+	while (divider != 1);
+	printf("Период: %d ", period_length - 1);
+	printf("Последовательность: [%lld; ", period[0]);
+	for (int i = 1; i < period_length; ++i)
+	{
+		if (i != period_length - 1)
+		{
+			printf("%lld,", period[i]);
+		}
+		else
+		{
+			printf("%lld", period[i]);
+		}
+	}
+	printf("]");
 
 
-    return 0;
+	return 0;
 }
--- Oleg-Tsvetkov/Block 1/PythagoreanTriples/PythagoreanTriples/Main.c
+++ Oleg-Tsvetkov/Block 1/PythagoreanTriples/PythagoreanTriples/Main.c
@@ -3,102 +3,102 @@
 
 void swap(long long* first, long long* second) 
 {
-    long long temp = *first;
-    *first = *second;
-    *second = temp;
+	long long temp = *first;
+	*first = *second;
+	*second = temp;
 }
 
 void clear_excess_chars() 
 {
-    char temp;
-    do 
-    {
-        temp = getchar();
-    } 
-    while (temp != '\n' && temp != EOF);
+	char temp;
+	do 
+	{
+		temp = getchar();
+	} 
+	while (temp != '\n' && temp != EOF);
 }
 
 long long get_number(counter) 
 {
-    short success = 0;
-    long long result;
-    char after_integer;
-    do 
-    {
-        printf("Введите %d-е число: ", counter);
-        if (scanf_s("%10lld%c", &result, &after_integer, 6) != 2 || after_integer != '\n') 
-        {
-            printf("Ошибка! К вводу принимаются только натуральные числа, которые не вызовут переполнение\n");
-            clear_excess_chars(); //Если не удалось сосканировать число, пропускаем лишние символы до след. строки
-        }
-        else if (result <= 0) 
-        {
-            printf("Ошибка! Введённое число должно быть натуральным\n");
-        }
-        else 
-        {
-            success = 1;
-        }
-    } 
-    while (!success);
-    return result;
+	short success = 0;
+	long long result;
+	char after_integer;
+	do 
+	{
+		printf("Введите %d-е число: ", counter);
+		if (scanf_s("%10lld%c", &result, &after_integer, 6) != 2 || after_integer != '\n') 
+		{
+			printf("Ошибка! К вводу принимаются только натуральные числа, которые не вызовут переполнение\n");
+			clear_excess_chars(); //Если не удалось сосканировать число, пропускаем лишние символы до след. строки
+		}
+		else if (result <= 0) 
+		{
+			printf("Ошибка! Введённое число должно быть натуральным\n");
+		}
+		else 
+		{
+			success = 1;
+		}
+	} 
+	while (!success);
+	return result;
 }
 
 int main() 
 {
-    setlocale(LC_ALL, "Russian");
-    printf("Описание: Программа получает три натуральных числа от пользователя и говорит,\n");
-    printf("являются ли они Пифагоровой тройкой. Если да, то программа также скажет, является ли она простой.\n");
-    long long numbers[3];
-    for (int i = 1; i <= 3; ++i) 
-    {
-        numbers[i - 1] = get_number(i);
-    }
-    if (numbers[0] >= numbers[1] && numbers[0] >= numbers[2])
-    {
-        swap(&numbers[0], &numbers[2]);
-    }
-    else if (numbers[1] >= numbers[0] && numbers[1] >= numbers[2]) 
-    {
-        swap(&numbers[1], &numbers[2]);
-    }
-    if (numbers[0] * numbers[0] + numbers[1] * numbers[1] == numbers[2] * numbers[2]) 
-    {
-        printf("Представленные числа являются Пифагоровой тройкой.\n");
+	setlocale(LC_ALL, "Russian");
+	printf("Описание: Программа получает три натуральных числа от пользователя и говорит,\n");
+	printf("являются ли они Пифагоровой тройкой. Если да, то программа также скажет, является ли она простой.\n");
+	long long numbers[3];
+	for (int i = 1; i <= 3; ++i) 
+	{
+		numbers[i - 1] = get_number(i);
+	}
+	if (numbers[0] >= numbers[1] && numbers[0] >= numbers[2])
+	{
+		swap(&numbers[0], &numbers[2]);
+	}
+	else if (numbers[1] >= numbers[0] && numbers[1] >= numbers[2]) 
+	{
+		swap(&numbers[1], &numbers[2]);
+	}
+	if (numbers[0] * numbers[0] + numbers[1] * numbers[1] == numbers[2] * numbers[2]) 
+	{
+		printf("Представленные числа являются Пифагоровой тройкой.\n");
 
-        long long minimal;
-        if (numbers[0] > numbers[1]) 
-        {
-            minimal = numbers[1];
-        }
-        else 
-        {
-            minimal = numbers[0];
-        }
+		long long minimal;
+		if (numbers[0] > numbers[1]) 
+		{
+			minimal = numbers[1];
+		}
+		else 
+		{
+			minimal = numbers[0];
+		}
 
-        short is_coprime = 1;
+		short is_coprime = 1;
 
-        for (long long i = 2; i <= minimal; ++i) 
-        {
-            if (numbers[0] % i == 0 && numbers[1] % i == 0 && numbers[2] % i == 0) 
-            {
-                is_coprime = 0;
-                break;
-            }
-        }
+		for (long long i = 2; i <= minimal; ++i) 
+		{
+			if (numbers[0] % i == 0 && numbers[1] % i == 0 && numbers[2] % i == 0) 
+			{
+				is_coprime = 0;
+				break;
+			}
+		}
 
-        if (is_coprime) 
-        {
-            printf("Представленные числа являются примитивной Пифагоровой тройкой (взаимно простые).\n");
-        }
-        else 
-        {
-            printf("Представленные числа не являются примитивной Пифагоровой тройкой (не взаимно простые).\n");
-        }
-    }
-    else 
-    {
-        printf("Представленные числа не являются Пифагоровой тройкой.\n");
-    }
-    return 0;
+		if (is_coprime) 
+		{
+			printf("Представленные числа являются примитивной Пифагоровой тройкой (взаимно простые).\n");
+		}
+		else 
+		{
+			printf("Представленные числа не являются примитивной Пифагоровой тройкой (не взаимно простые).\n");
+		}
+	}
+	else 
+	{
+		printf("Представленные числа не являются Пифагоровой тройкой.\n");
+	}
+	return 0;
 }
