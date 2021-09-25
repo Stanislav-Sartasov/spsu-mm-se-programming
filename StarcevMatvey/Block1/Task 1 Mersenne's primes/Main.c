2b13a81ccea09764f6022298199deb7d30d0142c
Tozarin <89902951+Tozarin@users.noreply.github.com>
2021-09-26 01:41:57 +0300

    Fixed spaces (2.4 codestyle)

--- StarcevMatvey/Block1/Task 1 Mersenne's primes/Main.c
+++ StarcevMatvey/Block1/Task 1 Mersenne's primes/Main.c
@@ -3,26 +3,26 @@
 
 int is_prime(int number)
 {
-    for (int i = 3; i < floor(sqrt(number)) + 1; i = i + 2)
-    {
-        if (number % i == 0)
-        {
-            return 0;
-        }
-    }
-    return 1;
+	for (int i = 3; i < floor(sqrt(number)) + 1; i = i + 2)
+	{
+		if (number % i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int main()
 {
-    printf("Calculates Mersenne's primes\n");
-    for (int n = 1; n < 31; n++)
-    {
-        int number = (2 << n) - 1;
-        if (is_prime(number))
-        {
-            printf("%d ", number);
-        }
-    }
-    return 0;
+	printf("Calculates Mersenne's primes\n");
+	for (int i = 1; i < 31; i++)
+	{
+		int number = (2 << i) - 1;
+		if (is_prime(number))
+		{
+			printf("%d ", number);
+		}
+	}
+	return 0;
 }
