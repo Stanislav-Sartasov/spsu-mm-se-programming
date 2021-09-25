a243488501213b6fae4951dcb83d90534a23cb4a
toadharvard <88928096+toadharvard@users.noreply.github.com>
2021-09-25 23:09:54 +0300

    Block 1 code style improvement. (Spaces replaced by tabs)

--- Vadim-Yakshigulov/Semester 1/Block 1/Task 2 (Pythagorean triples)/main.c
+++ Vadim-Yakshigulov/Semester 1/Block 1/Task 2 (Pythagorean triples)/main.c
@@ -1,57 +1,59 @@
 #include <stdio.h>
 
-unsigned int lcd(int a, int b);
+unsigned int gcd(int a, int b);
+
 int is_pythagorean_triple(int a, int b, int c);
+
 void input(char string[], int *a, int *b, int *c);
 
 int main()
 {
-    int a, b, c;
-    printf("This program checks that inputted triples are Pythagorean and prime\n");
-    input("Please enter 3 natural numbers satisfying the Pythagorean formula (a^2 + b^2 = c^2):\n", &a, &b, &c);
-    if (is_pythagorean_triple(a, b, c))
-    {
-        if (lcd(a, b) == 1 && lcd(b, c) == 1 && lcd(a, c) == 1)
-        {
-            printf("Inputted numbers are prime Pythagorean triples");
-        }
-        else
-        {
-            printf("Inputted numbers are Pythagorean triples");
-        }
-    }
-    else
-    {
-        printf("Inputted numbers are not Pythagorean triples");
-    }
-    return 0;
+	int a, b, c;
+	printf("This program checks that inputted triples are Pythagorean and prime\n");
+	input("Please enter 3 natural numbers satisfying the Pythagorean formula (a^2 + b^2 = c^2):\n", &a, &b, &c);
+	if (is_pythagorean_triple(a, b, c))
+	{
+		if (gcd(a, b) == 1 && gcd(b, c) == 1 && gcd(a, c) == 1)
+		{
+			printf("Inputted numbers are prime Pythagorean triples");
+		}
+		else
+		{
+			printf("Inputted numbers are Pythagorean triples");
+		}
+	}
+	else
+	{
+		printf("Inputted numbers are not Pythagorean triples");
+	}
+	return 0;
 }
 
 void input(char string[], int *a, int *b, int *c)
 {
-    int result;
-    do
-    {
-        printf(string);
-        result = scanf("%d%d%d", a, b, c);
-        fflush(stdin);
-    }
-    while (!(result == 3 && *a > 0 && *b > 0 && *c > 0));
+	int result;
+	do
+	{
+		printf(string);
+		result = scanf("%d%d%d", a, b, c);
+		fflush(stdin);
+	}
+	while (!(result == 3 && *a > 0 && *b > 0 && *c > 0));
 }
 
 int is_pythagorean_triple(int a, int b, int c)
 {
-    return (a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a);
+	return (a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a);
 }
 
-unsigned int lcd(int a, int b)
+unsigned int gcd(int a, int b)
 {
-    while ((a != 0) && (b != 0))
-    {
-        if (a > b)
-            a %= b;
-        else
-            b %= a;
-    }
-    return a + b;
+	while ((a != 0) && (b != 0))
+	{
+		if (a > b)
+			a %= b;
+		else
+			b %= a;
+	}
+	return a + b;
 }
--- Vadim-Yakshigulov/Semester 1/Block 1/Task 4 (Continued fractions)/main.c
+++ Vadim-Yakshigulov/Semester 1/Block 1/Task 4 (Continued fractions)/main.c
@@ -1,37 +1,37 @@
 #include <stdio.h>
 #include <math.h>
 
-
 void input(char string[], long long *a);
 
 int main()
 {
-    long long D;
-    printf("This program prints the continued fraction of square root of given natural number and it's period\n");
-    input("Please enter natural number:\n", &D);
-    unsigned long R = floor(sqrt(D));
-    int k = 1;
-    printf("[ %lu ", R);
-    unsigned long a = R, P = 0, Q = 1;
-    do {
-        P = a * Q - P;
-        Q = (D - P * P) / Q;
-        a = (R + P) / Q;
-        printf("%lu ", a);
-        k += 1;
-    }
-    while(Q != 1);
-    printf("]\nPeriod is: %d", k);
+	long long d;
+	printf("This program prints the continued fraction of square root of given natural number and it's period\n");
+	input("Please enter natural number:\n", &d);
+	unsigned long r = floor(sqrt(d));
+	int k = 1;
+	printf("[ %lu ", r);
+	unsigned long a = r, p = 0, q = 1;
+	do
+	{
+		p = a * q - p;
+		q = (d - p * p) / q;
+		a = (r + p) / q;
+		printf("%lu ", a);
+		k += 1;
+	}
+	while (q != 1);
+	printf("]\nPeriod is: %d", k);
 }
 
 void input(char string[], long long *a)
 {
-    int result;
-    do
-    {
-        printf(string);
-        result = scanf("%lld", a);
-        fflush(stdin);
-    }
-    while (!(result == 1 && *a > 0));
+	int result;
+	do
+	{
+		printf(string);
+		result = scanf("%lld", a);
+		fflush(stdin);
+	}
+	while (!(result == 1 && *a > 0));
 }
