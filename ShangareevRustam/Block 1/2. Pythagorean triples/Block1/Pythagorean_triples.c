904d3567645ca8115555bbe024080198a26cb3bb
letit6E <rustam.letitbe@gmail.com>
2021-09-26 21:49:22 +0300

    Added solution for problem 2 (Pythagorean triples)

---
+++ ShangareevRustam/Block 1/2. Pythagorean triples/Block1/Pythagorean_triples.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+void user_input(int* a, int* b, int* c)
+{
+	printf("Please enter three natural numbers separated by spaces\n");
+	while (true)
+	{
+		int status_of_read = scanf_s("%d %d %d", a, b, c);
+		if (status_of_read == 3 && *a > 0 && *b > 0 && *c > 0)
+		{
+			break;
+		}
+		else
+		{
+			printf("At least one of the parameters you entered is not a natural number. Please re-enter\n");
+			fseek(stdin, 0, 0);
+		}
+	}
+}
+
+int gcd(int a, int b)
+{
+	while (b)
+	{
+		int tmp = a % b;
+		a = b;
+		b = tmp;
+	}
+	return a;
+}
+
+void sort_triple(int* a, int* b, int* c)
+{
+	int tmp;
+	if (*c > *b)
+	{
+		tmp = *b;
+		*b = *c;
+		*c = tmp;
+	}
+	if (*b > *a)
+	{
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+	}
+	if (*c > *b)
+	{
+		tmp = *b;
+		*b = *c;
+		*c = tmp;
+	}
+}
+
+void print_answer(int a, int b, int c)
+{
+	sort_triple(&a, &b, &c);
+	if (a * a != b * b + c * c)
+	{
+		printf("The entered three numbers are not a Pythagorean triple.\n");
+	}
+	else if (gcd(a, b) == 1 || gcd(b, c) == 1 || gcd(a, c) == 1)
+	{
+		printf("The three numbers entered are the primitive Pythagorean triple.\n");
+	}
+	else
+	{
+		printf("The three numbers entered are the Pythagorean triplet.\n");
+	}
+}
+
+int main()
+{
+	int a, b, c;
+	printf("This program by the entered three natural numbers determines "
+		"whether the given triple of numbers is Pythagorean or a primitive Pythagorean\n");
+	user_input(&a, &b, &c);
+	print_answer(a, b, c);
+	return 0;
+}
