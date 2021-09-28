5fc6ca89d200e6b12ad2b6da46ebb3cbe9825a75
Ksenia Kuzmina <90759630+sayumikko@users.noreply.github.com>
2021-09-28 23:05:55 +0300

    Fixed the check of numbers

--- Ksenia Kuzmina/Task 2/Task 2/Main.c
+++ Ksenia Kuzmina/Task 2/Task 2/Main.c
@@ -16,16 +16,26 @@ int main()
 {
 	printf("This program checks if the inputted numbers are coprime Pythagorean triples.\n");
 	int a, b, c;
-	printf("Input three numbers:\n");
+	printf("Please input three natural numbers:\n");
 	scanf_s("%d%d%d", &a, &b, &c);
+
+	while ((a < 0) || (b < 0) || (c < 0))
+	{
+		printf("At least one string is not a natural number. Please try again");
+		char clean = 0;
+		while (clean != '\n' && clean != EOF)
+			clean = getchar();
+		scanf_s("%d%d%d", &a, &b, &c);
+	}
+
 	if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a))
 	{
-		if ((alg_euclid(a, b) == 1) && (alg_euclid(b, c) == 1) && (alg_euclid(a, c) == 1))
+		if ((alg_euclid(alg_euclid(a, b), c)))
 			printf("The numbers are a Pythagorean triple and coprime");
 		else
 			printf("The numbers are a Pythagorean triple and they are not coprime");
 	}
-	else if ((alg_euclid(a, b) == 1) && (alg_euclid(b, c) == 1) && (alg_euclid(a, c) == 1))
+	else if ((alg_euclid(alg_euclid(a, b), c)))
 		printf("The numbers are not a Pythagorean triple and they are coprime");
 	else
 		printf("The numbers are not a Pythagorean triple and they are not coprime");
