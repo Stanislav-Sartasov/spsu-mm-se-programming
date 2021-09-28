917161e62c91ef735225aadbe2109b7f177d7953
suldin <suldin_slava.03@mail.ru>
2021-09-28 18:45:07 +0300

    Commit for new pull request

--- Suldin_Vyacheslav/FirstTerm/Task 1.1 (MersennePrimes)/Main1.1.c
+++ Suldin_Vyacheslav/FirstTerm/Task 1.1 (MersennePrimes)/Main1.1.c
@@ -5,38 +5,38 @@ int primeTest(int number);
 
 int main()
 {
-    int N, t;
+	int N, t;
 
-    printf("Mersenne prime numbers on [1,..2^31-1] :\n");
-    t = 2;
-    for (N = 1; N <= 31; N = N + 2)
-    {
-        if (primeTest(N) && primeTest(t-1))
-        {
-            printf("N=%d, 2^N-1 = %d\n", N, t - 1);
-        }
-        if (t == 2 && primeTest(t * t - 1))
-        {
-            printf("N=%d, 2^N-1 = %d\n", N, t * t - 1);
-        }
-        t = t * 4;
-    }
-    return 0;
+	printf("Mersenne primes on [1,..2^31-1] :\n");
+	t = 2;
+	for (N = 1; N <= 31; N = N + 2)
+	{
+		if (primeTest(N) && primeTest(t - 1))
+		{
+			printf("N=%d, 2^N-1 = %d\n", N, t - 1);
+		}
+		if (t == 2 && primeTest(t * t - 1))
+		{
+			printf("N=%d, 2^N-1 = %d\n", N, t * t - 1);
+		}
+		t = t * 4;
+	}
+	return 0;
 }
 
 int primeTest(int number)
 {
-    int j;
-    for (j = 3; j <= pow(number, 0.5); j = j + 2)
-    {
-        if (number % j == 0)
-        {
-            return 0;
-        }
-    }
-    if (number == 1)
-    {
-        return 0;
-    }
-    return 1;
+	int j;
+	for (j = 3; j <= pow(number, 0.5); j = j + 2)
+	{
+		if (number % j == 0)
+		{
+			return 0;
+		}
+	}
+	if (number == 1)
+	{
+		return 0;
+	}
+	return 1;
 }
