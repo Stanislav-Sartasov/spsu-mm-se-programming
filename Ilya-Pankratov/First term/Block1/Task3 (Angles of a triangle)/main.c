1ea6f8aca8bfa7b52fdc97e64d0d1b9e3f316fcd
i-pankrat <i.pankratov.main@gmail.com>
2021-09-29 09:12:17 +0300

    Updated codestyle, fixed a bug with the program crash when entering incorrectly

--- Ilya-Pankratov/First term/Block1/Task3 (Angles of a triangle)/main.c
+++ Ilya-Pankratov/First term/Block1/Task3 (Angles of a triangle)/main.c
@@ -1,66 +1,48 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <math.h>
 
-bool isTriangle(float* sides);
-void calculateAngles(float* arr);
+#define PI 3.14159265358979323
+
+void calculateAngles(float* sides);
 
 int main()
 {
 	printf("The program determines whether it is possible, based on the three entered numbers, to build a non-degenerate triangle with the corresponding sides and shows the angles between the sides\n\n");
-	float sides[3];
+	float sides[3] = { 0 };
 	char check;
 
 	for (int i = 0; i < 3; i++)
 	{
 		printf("Input the leght of the %d side(It must be greater than zero): ", i + 1);
-		if ((scanf_s("%f%c", &sides[i], &check) != 2) || (check != '\n'))
+		while ((scanf_s("%f%c", &sides[i], &check) != 2) || (check != '\n') || sides[i] <= 0)
 		{
-			printf("\nError: you enter incorrect data(Letters or symbols)\nPlease, try again\n");
-			return 0;
-		}
+			printf("\nError: you enter incorrect data. You have number must be higher than zero.\nPlease, try again\n\n");
 
-		if (sides[i] <= 0)
-		{
-			printf("\nError: your number is less than or equal to zero\nPlease, try again\n");
-			return 0;
+			while (check != '\n') 
+				scanf_s("%c", &check);
+
+			check = '\0';
+			printf("Input the leght of the %d side(It must be greater than zero): ", i + 1);
 		}
+		check = '\0';
 	}
-
+	
 	printf("\n");
 
-	if (isTriangle(&sides))
-	{
+	if ((sides[0] + sides[1] > sides[2]) && (sides[0] + sides[2] > sides[1]) && (sides[2] + sides[1] > sides[0]))
 		calculateAngles(&sides);
-	}
 	else
-	{
-		printf("Can't create a nondegenerate tringle\n");
-	}
+		printf("Can't create a non-degenerate tringle\n");
 
 	return 0;
 }
 
-bool isTriangle(float* sides)
-{
-	if ((sides[0] + sides[1] > sides[2]) && (sides[0] + sides[1] > sides[2]) && (sides[0] + sides[1] > sides[2]))
-	{
-		return true;
-    } 
-	else
-	{
-		return false;
-	}
-
-}
 void calculateAngles(float* sides)
 {
 	float firstAngle, secondAngle, thirdAngle;
-	float const PI = 3.1415926535;
-
-	firstAngle = (acos((pow(sides[0], 2) + pow(sides[1], 2) - pow(sides[2], 2)) / (2 * sides[0] * sides[1])) * 180) / PI;
-	secondAngle = (acos((pow(sides[0], 2) + pow(sides[2], 2) - pow(sides[1], 2)) / (2 * sides[0] * sides[2])) * 180) / PI;
-	thirdAngle = (acos((pow(sides[2], 2) + pow(sides[1], 2) - pow(sides[0], 2)) / (2 * sides[2] * sides[1])) * 180) / PI;
+	firstAngle = (acos((sides[0] * sides[0] + sides[1] * sides[1] - sides[2] * sides[2]) / (2 * sides[0] * sides[1])) * 180) / PI;
+	secondAngle = (acos((sides[0] * sides[0] + sides[2] * sides[2] - sides[1] * sides[1]) / (2 * sides[0] * sides[2])) * 180) / PI;
+	thirdAngle = (acos((sides[2] * sides[2] + sides[1] * sides[1] - sides[2] * sides[2]) / (2 * sides[2] * sides[1])) * 180) / PI;
 
 	float degrees[] = { firstAngle, secondAngle, thirdAngle }, minutes[3], seconds[3];
 
