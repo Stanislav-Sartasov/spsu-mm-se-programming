18e9195c80a1866010f2be68e119cf18fafb2d76
i-pankrat <i.pankratov.main@gmail.com>
2021-09-29 09:23:36 +0300

    Updated codestyle, added output of continued fraction period, removed array because the program works correctly without it

--- Ilya-Pankratov/First term/Block1/Task4 (Continued fractions)/main.c
+++ Ilya-Pankratov/First term/Block1/Task4 (Continued fractions)/main.c
@@ -6,86 +6,51 @@ int main()
 	unsigned int number;
 	char check;
 	float checkNatural;
-	printf("This program represents the square root of an integer that is not the square of another integer as an infinite continued fraction\n\n");
+
+	printf("This program represents the square root of an integer that is not the square of another integer as an infinite continued fraction.\n\n");
 	printf("Please enter a number that is not a square of an integer and higher then 0: ");
 
-	if ((scanf_s("%f%c", &checkNatural, &check) != 2) || (check != '\n'))
+	while ((scanf_s("%f%c", &checkNatural, &check) != 2) || (check != '\n') || checkNatural <= 0 || checkNatural != (int)checkNatural)
 	{
-		printf("\nError: you enter letters or symbols, you should enter narutal number\nPlease, try again\n");
-		return 0;
-	}
-
-	if ((checkNatural != (int)checkNatural) || (checkNatural <= 0) || (pow((int)pow(checkNatural, 0.5), 2) == checkNatural))
-	{
-		printf("\nError: your number does not match the conditions\nPlease try again\n");
-		return 0;
+		printf("\nError: you enter incorrect data. You have number must be higher than zero.\nPlease, try again\n");
+		if (check != '\n')
+			check = '\0';
+		while (check != '\n')
+			scanf_s("%c", &check);
+		check = '\0';
+		printf("\nPlease enter a number that is not a square of an integer and higher then 0: ");
 	}
 
 	number = (int)checkNatural;
-	
-	/* Algorithm for constructing an infinite continued fraction from an irrational number (square root):
-	1. Allocation of the integer part from under the root and representation of the remainder of the number 
-	in the form of the root minus the integer part
-	2. Turn the fraction of the form m/n to the form 1/(n/m)
-	3. Multiply the numerator and denominator of the fraction by a number of the form sqrt (number) + dif (the difference is calculated as
-	the whole part of the root - the remainder from the previous step) to make the denominator rational.
-	4. Count the denominator
-	5. Reduce the current denominator by the past denominator (This will be an integer, you can prove it mathematically)
-	6. Remember the current denominator for the next step
-	7. Count the integer part of the numerator sqrt (number) + difference, extracting the integer part from the root
-	8. Take out the whole part from the fraction and save it in the array
-	9. Ñalculate the remainder (the remainder is stored in i + 1 elements of the array)
-	10. Repeat steps 2-10 again (When the last denominator is 1, the period closes and starts again) */
+	int remainder = 0, lastDenominator = 1, i = 0, intDenominator, intNumenator, difference;
+	int intPartOfNumber = (int)(pow(number, 0.5)); 
 
-	int period[64] = { 0 }, intDenominator, intNumenator, difference, lastDenominator = 1;
-	period[0] = (int)(pow(number, 0.5)); // step 1
+	printf("\nInfinite continued fraction representation of the square root of %d: [%d; (", number, intPartOfNumber);
 
-	for (int i = 1; i < 64; i++)
+	for ( ; ; )
 	{
-		difference = period[0] - period[i]; 
-		intDenominator = number - pow(difference, 2); // step  4
-		intDenominator = (int)(intDenominator / lastDenominator); // step 5
-		lastDenominator = intDenominator; // step 6
-		intNumenator = period[0] + difference; // step 7
-		period[i] = (int)(intNumenator / intDenominator); // step 8
-		period[i + 1] = intNumenator - period[i] * intDenominator; // step 9
+		difference = intPartOfNumber - remainder;
+		intDenominator = number - difference * difference;
+		intDenominator = (int)(intDenominator / lastDenominator); 
+		lastDenominator = intDenominator; 
+		intNumenator = intPartOfNumber + difference;
+		remainder = (int)(intNumenator / intDenominator); 
+
+		if (i == 0)
+			printf("%d", remainder);
+		else
+			printf(", %d", remainder);
+
+		remainder = intNumenator - remainder * intDenominator; 
+		i++;
 
 		if (lastDenominator == 1)
 		{
-			period[i + 1] = -1;
-			break;
-		}
-
-		if (i == 62)
-		{
-			period[i + 1] = -2;
+			printf(")].");
 			break;
 		}
 	}
-
-	printf("\nInfinite continued fraction representation of the square root of %d: [%d, (", number, period[0]);
-	int i = 1;
-
-	while(period[i] != -1 || period[i] != -2)
-	{
-		printf("%d", period[i]);
-
-		if (period[i+1] == -1)
-		{
-			printf(")]");
-			break;
-		}
-
-		if (period[i + 1] == -2)
-		{
-			printf("...)]");
-			break;
-		}
-
-		printf(", ");
-		i++;
-	}
-
-	printf("\nThe '()' indicates period of the continued fraction, which repeats over and over again\n");
+	printf("\nThe '()' indicates period of the continued fraction, which repeats over and over again.\n");
+	printf("Period of the continued fraction is %d.\n", i);
 	return 0;
 }
