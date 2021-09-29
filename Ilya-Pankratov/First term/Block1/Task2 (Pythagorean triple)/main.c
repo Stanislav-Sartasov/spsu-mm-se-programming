69fb0f469040bdf9a4bdf937cbdf6c1f24c32f6b
i-pankrat <i.pankratov.main@gmail.com>
2021-09-29 08:46:52 +0300

    Updated codestyle, fixed a bug with the program crash when entering incorrectly, minor improvements in algorithms

--- Ilya-Pankratov/First term/Block1/Task2 (Pythagorean triple)/main.c
+++ Ilya-Pankratov/First term/Block1/Task2 (Pythagorean triple)/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool isNumbersPythogoreanTriple(int FirstNum, int SecondNum, int ThirdNum);
-bool greatestCommonDivisor(int firstNumber, int secondNumber);
+bool isNumbersPythogoreanTriple(int firstNumber, int secondNumber, int thirdNumber);
+
+int gcd(int firstNumber, int secondNumber);
+
 bool isNumbersPrimitive(int* arr);
+
 void answer(int* arr);
 
 int main()
@@ -17,100 +20,68 @@ int main()
 	{
 		printf("Input the %d number of Pythogorean triple(It must be natural): ", i + 1);
 
-		if ((scanf_s("%f%c", &checkNatural, &check) != 2) || (check != '\n'))
+		while ((scanf_s("%f%c", &checkNatural, &check) != 2) || (check != '\n') || checkNatural <= 0 || checkNatural != (int)checkNatural)
 		{
-			printf("\nError: you enter incorrect data(letters or symbols)\nPlease, try again\n");
-			return 0;
-		}
+			printf("\nError: you enter incorrect data. You have to input natural number. \nPlease, try again\n");
+			
+			if (i > 0 && checkNatural == arrayOfNumbers[i-1] && check == '\n')
+				check = '\0';
 
-		if (checkNatural != (int)checkNatural)
-		{
-			printf("\nError: you entered a fractional number\nPlease try again\n");
-			return 0;
-		}
+			while (check != '\n')
+				scanf_s("%c", &check);
 
-		if (checkNatural <= 0)
-		{
-			printf("\nError: you entered an unnatural number\nPlease, try again\n");
-			return 0;
+			check = '\0';
+			printf("\nInput the %d number of Pythogorean triple(It must be natural): ", i + 1);
 		}
-
 		arrayOfNumbers[i] = (int)checkNatural;	
 	}
 
 	printf("\n");
 
 	if (isNumbersPythogoreanTriple(arrayOfNumbers[0], arrayOfNumbers[1], arrayOfNumbers[2]))
-	{
 		answer(&arrayOfNumbers);
-	}
-	else if ((isNumbersPythogoreanTriple(arrayOfNumbers[0], arrayOfNumbers[2], arrayOfNumbers[1])))
-	{
-		answer(&arrayOfNumbers);
-	}
-	else if ((isNumbersPythogoreanTriple(arrayOfNumbers[2], arrayOfNumbers[1], arrayOfNumbers[0])))
-	{
-		answer(&arrayOfNumbers);
-	}
 	else
-	{
 		printf("Your numbers are not Pythogorean triple\n");
-	}
+
 	return 0;
 }
 
-bool isNumbersPythogoreanTriple(int firstNum, int secondNum, int thirdNum)
+bool isNumbersPythogoreanTriple(int firstNumber, int secondNumber, int thirdNumber)
 {
-	if (firstNum*firstNum + secondNum*secondNum == thirdNum*thirdNum)
-	{
+	if (firstNumber * firstNumber + secondNumber * secondNumber == thirdNumber * thirdNumber)
 		return true;
-	}
+	else if (firstNumber * firstNumber + thirdNumber * thirdNumber == secondNumber * secondNumber)
+		return true;
+	else if (secondNumber * secondNumber + thirdNumber * thirdNumber == firstNumber * firstNumber)
+		return true;
 	else
-	{
 		return false;
-	}
 }
-bool greatestCommonDivisor(int firstNumber, int secondNumber)
+
+int gcd(int firstNumber, int secondNumber)
 {
 	while (firstNumber != secondNumber)
 	{
 		if (firstNumber > secondNumber)
-		{
 			firstNumber = firstNumber - secondNumber;
-		}
 		else
-		{
 			secondNumber = secondNumber - firstNumber;
-		}
 	}
-	if (firstNumber == 1)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+		return firstNumber;
 }
+
 bool isNumbersPrimitive(int* arr)
 {
-	if (greatestCommonDivisor(arr[0], arr[1]) && greatestCommonDivisor(arr[0], arr[2]) && greatestCommonDivisor(arr[2], arr[1]))
-	{
+	if (gcd(arr[0], arr[1]) == 1 && gcd(arr[0], arr[2]) == 1 && gcd(arr[2], arr[1]) == 1)
 		return true;
-	}
 	else
-	{
 		return false;
-	}
 }
+
 void answer(int* arr)
 {
 	if (isNumbersPrimitive(arr))
-	{
 		printf("Your numbers form a primitive Pythagorean triple\n");
-	}
 	else
-	{
 		printf("Your numbers form a non-primitive Pythagorean triple\n");
-	}
 }
