1f1b6abc85f40454b078ad463f47aea3a15b2625
VladYoSlav <vaifaer@gmail.com>
2021-10-01 02:11:25 +0300

    Updated solution for Block 1 Mersenne Primes with better codestyle

--- VladislavShalnev/Block 1/Task 1 (MersennePrimes)/MersennePrimes/main.c
+++ VladislavShalnev/Block 1/Task 1 (MersennePrimes)/MersennePrimes/main.c
@@ -6,14 +6,14 @@ int is_prime(int number)
 	if (number == 1) return 0;
 	for (unsigned int i = 2; i * i <= number; i++)
 	{
-		if (!(number % i)) return 0;
+		if (number % i == 0) return 0;
 	}
 	return 1;
 }
 
 int main()
 {
-	printf("This program prints all Mersenne prime numbers on the [1; 2^31 - 1] interval.\n");
+	printf("This program prints all Mersenne prime numbers on the [1; 2^31 - 1] interval.\n\n");
 	unsigned int current = 1;
 	for (int n = 0; n < 32; n++)
 	{
