1e290d580bde3cbe7c8c7b179d623f33190d3cbc
VladYoSlav <vaifaer@gmail.com>
2021-10-01 01:56:04 +0300

    Updated solution for Block 1 Pythagorean Triples

--- VladislavShalnev/Block 1/Task 2 (PythagoreanTriples)/PythagoreanTriples/main.c
+++ VladislavShalnev/Block 1/Task 2 (PythagoreanTriples)/PythagoreanTriples/main.c
@@ -1,19 +1,19 @@
 
 #include <stdio.h>
 
-int is_pythagorean_triple(unsigned int x, unsigned int y, unsigned int z)
+int is_pythagorean_triple(int x, int y, int z)
 {
 	return (x * x + y * y == z * z) || (x * x + z * z == y * y) || (y * y + z * z == x * x);
 }
 
-int is_coprime(unsigned int x, unsigned int y, unsigned int z)
+int is_coprime(int x, int y, int z)
 {
 	return gcd(x, gcd(y, z)) == 1;
 }
 
-unsigned int gcd(unsigned int firstNumber, unsigned int secondNumber)
+int gcd(int firstNumber, int secondNumber)
 {
-	while (firstNumber && secondNumber)
+	while (firstNumber != 0 && secondNumber != 0)
 	{
 		if (firstNumber > secondNumber) firstNumber %= secondNumber;
 		else secondNumber %= firstNumber;
@@ -47,28 +47,29 @@ void input(int *adress, char *message)
 
 int main()
 {
-	unsigned int x, y, z;
-	printf("This program checks if entered numbers triple is a pythagorean triple and prime pythagorean triple.\n");
-	printf("Enter natural numbers triple x y z:\n");
+	int x, y, z;
+	printf("This program checks if entered numbers triple is a pythagorean triple and prime pythagorean triple.\n\n");
+	printf("Enter natural numbers triple x y z:\n\n");
 	input(&x, "Enter first number: ");
 	input(&y, "Enter second number: ");
 	input(&z, "Enter third number: ");
+	printf("\n"); // separation between input and answer
 
 	if (is_pythagorean_triple(x, y, z))
 	{
 		printf("Triple %d %d %d is a pythagorean triple", x, y, z);
 		if (is_coprime(x, y, z))
 		{
-			printf(" and a prime pythagorean triple\n");
+			printf(" and a prime pythagorean triple.\n");
 		}
 		else
 		{
-			printf(" and not a prime pythagorean triple\n");
+			printf(" and not a prime pythagorean triple.\n");
 		}
 	}
 	else
 	{
-		printf("Triple %d %d %d is not a pythagorean triple\n", x, y, z);
+		printf("Triple %d %d %d is not a pythagorean triple.\n", x, y, z);
 	}
 
 	return 0;
