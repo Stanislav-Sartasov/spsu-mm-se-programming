0a09730637c887bc2a70781b44f38f8d6564fa25
Ilair22 <st085683@student.spbu.ru>
2021-10-01 20:41:40 +0300

    Task 1 updated.

--- Ragalison Hilaire/Block 1 (First semester)/1. MersennePrimes/1. MersennePrimes/checkPrimeNumbers.h
+++ Ragalison Hilaire/Block 1 (First semester)/1. MersennePrimes/1. MersennePrimes/checkPrimeNumbers.h
@@ -11,9 +11,13 @@ bool isPrime(int x)
 	if (x > 1)
 	{
 		for (int i = 2; i < x; i++)
+		{
 			if (x % i == 0)
+			{
 				return false;
-		return true;
+			}
+		    return true;
+		}
 	}
 	else
 		return false;
