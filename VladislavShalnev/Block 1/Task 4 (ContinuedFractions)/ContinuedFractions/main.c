b93f407d4357b31861e2f99f1bc306745ea6fd90
VladYoSlav <vaifaer@gmail.com>
2021-10-01 17:26:37 +0300

    Updated solution for Block 1 Continued Fractions

--- VladislavShalnev/Block 1/Task 4 (ContinuedFractions)/ContinuedFractions/main.c
+++ VladislavShalnev/Block 1/Task 4 (ContinuedFractions)/ContinuedFractions/main.c
@@ -1,59 +1,68 @@
 
 #include <stdio.h>
-#include <math.h>
+#define MAX_RIGHT_BORDER 3037000500
+
+void update_remainder_and_denominator(long long intermediate, long long number, long long int_part, long long* remainder, long long* denominator)
+{
+	*denominator = (number - (int_part - *remainder) * (int_part - *remainder)) / *denominator;
+
+	// updating remainder using new denominator
+	*remainder = intermediate % *denominator;
+}
 
 long long get_int_part_of_fraction(long long number, long long int_part, long long *remainder, long long *denominator)
 {
-	long long new_denominator = (number - (int_part - *remainder) * (int_part - *remainder)) / *denominator;
-
 	long long intermediate = int_part * 2 - *remainder;
+	long long int_part_of_fraction = intermediate / *denominator;
 
-	long long new_remainder = intermediate % new_denominator;
-	long long int_part_of_fraction = intermediate / new_denominator;
-
-	*remainder = new_remainder;
-	*denominator = new_denominator;
+	update_remainder_and_denominator(intermediate, number, int_part, remainder, denominator);
 
 	return int_part_of_fraction;
 }
 
 long long get_int_part_of_sqrt(long long number)
 {
-	return (long long)sqrt(number);
+	long long left_border = 1;
+
+	/*
+		max long long = 2 ^ 64 / 2 - 1
+		3037000499 < sqrt(max long long) < 3037000500
+	*/
+
+	long long right_border = (number < MAX_RIGHT_BORDER) ? number : MAX_RIGHT_BORDER;
+
+	while (left_border < right_border - 1)
+	{
+		long long middle = (left_border + right_border) / 2;
+		if (middle * middle <= number)
+			left_border = middle;
+		else
+			right_border = middle;
+	}
+
+	return left_border;
 }
 
 void print_continued_fraction(long long number)
 {
 	long long int_part = get_int_part_of_sqrt(number);
-	long long remainder = 0;
-	long long denominator = 1;
+	long long remainder = 0, denominator = 1;
 	
-	printf("Continued fraction of square root of entered number:\n");
-	printf("[%lld; %lld", int_part, get_int_part_of_fraction(number, int_part, &remainder, &denominator));
+	printf("\nContinued fraction of square root of entered number:\n");
+	printf("[%lld", get_int_part_of_fraction(number, int_part, &remainder, &denominator) / 2);
 
 	// remainder and denominator were updated after first print
+	long long start_remainder = remainder, start_denominator = denominator;
 
-	long long start_remainder = remainder;
-	long long start_denominator = denominator;
+	long long period = 0;
 
-	long long period = 1;
-
-	long long new_int_part_of_fraction;
-
-	while (1)
+	do
 	{
-		new_int_part_of_fraction = get_int_part_of_fraction(number, int_part, &remainder, &denominator);
-
-		// catching the fraction cycle
-		if (remainder == start_remainder && denominator == start_denominator)
-			break;
-
-		// if not caught
-		printf("; %lld", new_int_part_of_fraction);
+		printf(", %lld", get_int_part_of_fraction(number, int_part, &remainder, &denominator));
 		period++;
-	}
+	} while (remainder != start_remainder || denominator != start_denominator);
 
-	printf("]\n");
+	printf("]\n\n");
 	printf("The period of this continued fraction is %lld.\n", period);
 }
 
@@ -94,7 +103,7 @@ void input(long long* adress, char* message)
 
 int main()
 {
-	printf("This program prints the continued fraction and it's period of square root of entered number.\n");
+	printf("This program prints the continued fraction and it's period of square root of entered number.\n\n");
 
 	long long number;
 	input(&number, "Enter a positive integer number that isn't square of another integer: ");
