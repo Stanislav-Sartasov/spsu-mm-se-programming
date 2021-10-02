65a479909bf3756c85c3e04e8d55e2bd9f1859d3
evendelger <evendelger@gmail.com>
2021-10-02 21:24:32 +0300

    Updated solutions to the 1-3 tasks

--- Term1/Block1/Task1/main.c
+++ Term1/Block1/Task1/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-_Bool isPrime(unsigned long long N)
+_Bool is_prime(unsigned long long n)
 {
-	if (N <= 1)
+	if (n <= 1)
 		return 0;
 
-	for (int j = 2; j <= sqrt(N); j++)
-		if (N % j == 0) return 0;
+	for (int j = 2; j <= sqrt(n); j++)
+		if (n % j == 0) return 0;
 
 	return 1;
 }
 
 int main()
 {
-	printf("Output all prime Mersenne numbers from 1 to 2^31 - 1:\n");
+	printf("The program outputs all prime Mersenne numbers from 1 to 2^31 - 1:\n");
 
 	for (unsigned long long i = 2; i <= pow(2, 31); i *= 2)
-		if (isPrime(i - 1))
+		if (is_prime(i - 1))
 			printf("%llu\n", i - 1);
 
 	return 0;
--- Term1/Block1/Task2/main.c
+++ Term1/Block1/Task2/main.c
@@ -1,30 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
-int GCD(int a, int b) // greatest common divisor of two numbers
+int gcd(int a, int b) // greatest common divisor of two numbers
 {
 	if (b == 0)
 		return a;
-	return GCD(b, a % b);
+	return gcd(b, a % b);
+}
+
+int check_input(char* str) // checking the entered numbers for compliance with the conditions
+{
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return 0;
+	}
+
+	return 1;
 }
 
 int main()
 {
+	char x_input[256], y_input[256], z_input[256];
 	int x, y, z;
+	printf("The program checks whether the entered numbers are a simple or ordinary Pythagorean triple.\n\n");
 	printf("Enter three numbers:\n");
-	scanf("%d%d%d", &x, &y, &z);
+	scanf("%s%s%s", x_input, y_input, z_input);
 
-	if (x <= 0 || y <= 0 || z <= 0)
+	while (!check_input(x_input) || !check_input(y_input) || !check_input(z_input) || atoi(x_input) <= 0 || atoi(y_input) <= 0 || atoi(z_input) <= 0)
 	{
-		printf("The entered numbers must be natural. Please repeat the input.\n");
-		return 0;
+		printf("\nThe sides of the triangle must be positive numbers, please repeat the input:\n");
+		scanf("%s%s%s", x_input, y_input, z_input);
 	}
+
+	x = atoi(x_input);
+	y = atoi(y_input);
+	z = atoi(z_input);
 
 	if (y * y + z * z == x * x || x * x + z * z == y * y || x * x + y * y == z * z)
 	{
-		if (GCD(x, y) == 1 && GCD(x, z) == 1 && GCD(y, z) == 1)
+		if (gcd(gcd(x, y), z) == 1)
 			printf("The entered numbers form a primitive Pythagorean triple");
 		else
 			printf("The entered numbers form a non-primitive Pythagorean triple");
