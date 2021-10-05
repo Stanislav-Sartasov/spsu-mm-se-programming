b17087518c48b20830d90a418b2051b9dc54fff5
Вячеслав Бучин <chester-vyacheslav@yandex.ru>
2021-10-05 15:41:03 +0300

    Fixed codestyle

--- VyacheslavBuchin/Term1/Block1/Task1/main.c
+++ VyacheslavBuchin/Term1/Block1/Task1/main.c
@@ -2,26 +2,26 @@
 
 int isPrime(int n)
 {
-    if (n == 1)
-        return 0;
-    for (int i = 2; 1ll * i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return 0;
-    }
-    return 1;
+	if (n == 1)
+		return 0;
+	for (int i = 2; 1ll * i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
 }
 
 int main()
 {
-    printf("This program prints prime Mersenne numbers in range [1; 2^31 - 1].\n\n");
+	printf("This program prints prime Mersenne numbers in range [1; 2^31 - 1].\n\n");
 
-    for (int i = 1; i <= 31; i++)
-    {
-        int number = (1ll << i) - 1;
-        if (isPrime(number))
-            printf("%d\n", number);
-    }
+	for (int i = 1; i <= 31; i++)
+	{
+		int number = (1ll << i) - 1;
+		if (isPrime(number))
+			printf("%d\n", number);
+	}
 
-    return 0;
+	return 0;
 }
