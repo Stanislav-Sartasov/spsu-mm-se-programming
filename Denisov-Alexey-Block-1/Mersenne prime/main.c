e577ec8b549a80dc1bee4e8bdf472522a69188fe
Denisov Alexey <denisov.1881@yandex.ru>
2021-10-06 00:28:21 +0300

    Codestyle 2.4 improving

--- Denisov-Alexey-Block-1/Mersenne prime/main.c
+++ Denisov-Alexey-Block-1/Mersenne prime/main.c
@@ -6,31 +6,31 @@ bool isSimple(int a);
 
 int main()
 {
-    printf("This program displays simple Mersenne primes to 2^31-1\n\n");
+	printf("This program displays simple Mersenne primes to 2^31-1\n\n");
 
-    for (int n = 1; n <= 31; n++)
-    {
-        if (isSimple(pow(2, n) - 1))
-            printf("%d\n", (int)pow(2, n) - 1);
-    }
+	for (int n = 1; n <= 31; n++)
+	{
+		if (isSimple(pow(2, n) - 1))
+			printf("%d\n", (int)pow(2, n) - 1);
+	}
 }
 
 bool isSimple(int a)
 {
-    switch (a)
-    {
-    case 1:
-        return false;
-        break;
-    case 2:
-        return true;
-        break;
-    default:
-        for (int i = 2; i < a; i++)
-        {
-            if (a % i == 0)
-                return false;
-        }
-        return true;
-    }
+	switch (a)
+	{
+	case 1:
+		return false;
+		break;
+	case 2:
+		return true;
+		break;
+	default:
+		for (int i = 2; i < a; i++)
+		{
+			if (a % i == 0)
+				return false;
+		}
+		return true;
+	}
 }
--- Denisov-Alexey-Block-1/Pythagorean triple/main.c
+++ Denisov-Alexey-Block-1/Pythagorean triple/main.c
@@ -6,61 +6,61 @@ int gsd(int a, int b);
 int main()
 {
 
-    printf("This program identify primitive Pythagorean triple\n\n");
-    printf("Enter three integers: ");
+	printf("This program identify primitive Pythagorean triple\n\n");
+	printf("Enter three integers: ");
 
-    int x, y, z;
-    while (!(scanf_s("%d", &x) && scanf_s("%d", &y) && scanf_s("%d", &z)))
-    {
-        scanf_s("%*[^\n]");
-        printf("Input error. Please, enter integer variables\n\nEnter three integers: ");
-    }
+	int x, y, z;
+	while (!(scanf_s("%d", &x) && scanf_s("%d", &y) && scanf_s("%d", &z)))
+	{
+		scanf_s("%*[^\n]");
+		printf("Input error. Please, enter integer variables\n\nEnter three integers: ");
+	}
 
-    if (x >= y)
-    {
-        if (x > z)
-        {
-            x += z;
-            z = x - z;
-            x -= z;
-        }
-    }
-    else
-    {
-        if (y > z)
-        {
-            y += z;
-            z = y - z;
-            y -= z;
-        }
-    }
+	if (x >= y)
+	{
+		if (x > z)
+		{
+			x += z;
+			z = x - z;
+			x -= z;
+		}
+	}
+	else
+	{
+		if (y > z)
+		{
+			y += z;
+			z = y - z;
+			y -= z;
+		}
+	}
 
-    if (x * x + y * y == z * z)
-    {
-        if (gsd(gsd(x, y), z) == 1)
-            printf("It is primitive Pythagorean triple.\n");
-        else
-            printf("It is not primitive Pythagorean triple.\n");
-    }
-    else
-        printf("It is not Pythagorean triple.\n");
+	if (x * x + y * y == z * z)
+	{
+		if (gsd(gsd(x, y), z) == 1)
+			printf("It is primitive Pythagorean triple.\n");
+		else
+			printf("It is not primitive Pythagorean triple.\n");
+	}
+	else
+		printf("It is not Pythagorean triple.\n");
 }
 
 int gsd(int a, int b)
 {
-    if (a == b)
-        return a;
+	if (a == b)
+		return a;
 
-    while (a > 0 && b > 0)
-    {
-        if (a > b)
-            a = a % b;
-        else
-            b = b % a;
-    }
+	while (a > 0 && b > 0)
+	{
+		if (a > b)
+			a = a % b;
+		else
+			b = b % a;
+	}
 
-    if (a == 0)
-        return b;
-    else
-        return a;
+	if (a == 0)
+		return b;
+	else
+		return a;
 }
