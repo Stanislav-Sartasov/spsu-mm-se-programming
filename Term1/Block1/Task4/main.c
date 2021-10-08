780753965aa84ff899e2e2be103511708e11ec72
evendelger <evendelger@gmail.com>
2021-10-09 00:05:02 +0300

    Updated solution to 4th task

--- Term1/Block1/Task4/main.c
+++ Term1/Block1/Task4/main.c
@@ -1,85 +1,73 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define FALSE 0
+#define TRUE 1
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-long ContinuedFraction(long num, long den, int period) // the main algorithm for finding a continued fraction
+_Bool check_is_sqrt(long long int a)
 {
-	if (den == 1)
-	{
-		printf("%ld]\n", num);
-		printf("The period of this continued fraction is %d\n", period);
-	}
-	else if (den == 0)
-	{
-		printf("\b\b]\n");
-		printf("The period of this continued fraction is %d\n", period - 1);
-	}
-	else
-	{
-		printf("%ld; ", num / den);
-		long saved = den;
-		den = num - (num / den) * den;
-		num = saved;
-		period++;
-		return ContinuedFraction(num, den, period);
-	}
+	return (long long int)sqrt(a) == sqrt(a) ? FALSE : TRUE;
 }
 
-long CheckingNegative(long numerator, long denominator, int period) // reducing the denominator and numerator to the standard form
+_Bool check_char(char* a_input)
 {
-	if ((numerator < 0) ^ (denominator < 0))
+	for (int i = 0; i < strlen(a_input); i++)
 	{
-		printf("%d; ", numerator / denominator - 1);
-		long saved = denominator;
-		denominator = abs((numerator / saved - 1) * abs(saved)) - abs(numerator);
-		numerator = abs(saved);
-		period++;
-		return ContinuedFraction(numerator, denominator, period);
+		if (!(a_input[i] >= '0' && a_input[i] <= '9'))
+			return FALSE;
 	}
-	else
-		return ContinuedFraction(abs(numerator), abs(denominator), period);
+	return TRUE;
+}
+
+void check_input(char* a_input, long long int* a)
+{
+	while (TRUE)
+	{
+		if (!check_char(a_input) || atoll(a_input) <= 0)
+		{
+			printf("You must enter a natural integer, please repeat the input:\n");
+			gets(a_input);
+			continue;
+		}
+		else if (!check_is_sqrt(atoll(a_input)))
+		{
+			printf("The entered number is the square of another, please repeat the input:\n");
+			gets(a_input);
+			continue;
+		}
+		break;
+	}
+	*a = atoll(a_input);
 }
 
 int main()
 {
-	long numerator, denominator;
-	int flag, period = 0;
-	printf("Enter \"1\" if you want to enter a non-fractional number and \"2\" if you want to enter a fractional number: ");
-	scanf("%d", &flag);
+	char a_input[256];
+	long long int a, int_sqrt_a, func_output, period = 0, divided, denominator;
+	printf("The program outputs the chain fraction of the root of the entered number.\n\n");
+	printf("Enter a number:\n");
+	gets(a_input);
+	check_input(&a_input, &a);
 
-	if (flag == 1)
+	int_sqrt_a = sqrt(a);
+	func_output = int_sqrt_a;
+	divided = 0;
+	denominator = 1;
+	printf("\nContinued fraction: [%lld; ", int_sqrt_a);
+
+	do
 	{
-		float number;
-		denominator = 1;
-		printf("Enter a number: ");
-		scanf("%f", &number);
+		divided = func_output * denominator - divided;
+		denominator = (a - divided * divided) / denominator;
+		func_output = (int_sqrt_a + divided) / denominator;
+		printf("%lld; ", func_output);
+		period++;
+	} while (denominator != 1);
 
-		for (int i = 10; (number - (long)number) != 0; i *= 10, number *= 10)
-			denominator = i;
-		numerator = (long)number;
-
-		printf("[");
-		CheckingNegative(numerator, denominator, period);
-	}
-	else if (flag == 2)
-	{
-		printf("Enter the numerator of the fraction: ");
-		scanf("%ld", &numerator);
-		printf("Enter the denominator of the fraction: ");
-		scanf("%ld", &denominator);
-
-		if (denominator == 0)
-		{
-			printf("Zero can't be the denominator, please repeat the input.\n");
-			return 0;
-		}
-
-		printf("[");
-		CheckingNegative(numerator, denominator, period);
-	}
-	else
-		printf("Wrong input, please try again\n");
+	printf("\b\b]\n");
+	printf("The period of this continued fraction is %lld\n", period);
 
 	return 0;
 }
