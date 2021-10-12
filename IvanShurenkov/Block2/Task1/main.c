841cfb8fcdb913f921119f1fa64dad65e454f2ae
ivan <ivan.shurenkov@yandex.ru>
2021-10-12 18:48:49 +0300

    Solution of the block 2 task 1

---
+++ IvanShurenkov/Block2/Task1/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void print_float(int n)
+{
+	int exp = 127;
+	int degree = 1;
+	while ((degree << 1) <= n)
+	{
+		degree <<= 1;
+		exp++;
+	}
+	printf("0");
+	for (int i = 7; i >= 0; i--)
+	{
+		printf("%d", (exp >> i) & 1);
+	}
+	int left_border = exp - 127;
+	for (int i = left_border - 1; i >= 0; i--)
+	{
+		printf("%d", (n >> i) & 1);
+	}
+	for (int i = 22 - left_border; i >= 0; i--)
+	{
+		printf("0");
+	}
+}
+
+void print_double(int n)
+{
+	int exp = 1023;
+	int degree = 1;
+	while ((degree << 1) <= n)
+	{
+		degree <<= 1;
+		exp++;
+	}
+	printf("1");
+	for (int i = 10; i >= 0; i--)
+	{
+		printf("%d", (exp >> i) & 1);
+	}
+	int left_border = exp - 1023;
+	for (int i = left_border - 1; i >= 0; i--)
+	{
+		printf("%d", (n >> i) & 1);
+	}
+	for (int i = 51 - left_border; i >= 0; i--)
+	{
+		printf("0");
+	}
+}
+
+int main()
+{
+	printf("Representation of the number\n\nPrint binary representation of product my name, second name and patronymic\n");
+	__int32_t product = sizeof("Ivan") * sizeof("Shurenkov") * sizeof("Andreevich");
+	printf("A)Negative 32-bit integer: ");
+	for (int i = 32; i >= 0; i--)
+	{
+		printf("%d", ((-product) >> i) & 1);
+	}
+	printf("\nB)Positive single-precision floating point number: ");
+	print_float(product);
+	printf("\nC)Negative double-precision floating point number: ");
+	print_double(product);
+	return 0;
+}
