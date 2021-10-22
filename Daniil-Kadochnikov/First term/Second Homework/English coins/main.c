5159336e57a047093f660d4d700303a86110594f
Daniil <kadoch.daniil@gmail.com>
2021-10-22 12:35:43 +0300

    Initial commit of the second task from the second homework.

---
+++ Daniil-Kadochnikov/First term/Second Homework/English coins/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int count = 0;
+int coinsAmount[2][8] = { 200, 100, 50, 20, 10, 5, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+int checkError(float* pnumber, int* operation)
+{
+	if (*operation == 1 && fgetc(stdin) == '\n')
+	{
+		if (*pnumber > 0)
+		{
+			if ((int)*pnumber == *pnumber)
+			{
+				return 0;
+			}
+			else
+			{
+				printf("\nYou entered a number that is not an integer, try again, please.\n\n");
+				return 1;
+			}
+		}
+		else
+		{
+			printf("\nYou entered a non-positive number, try again, please.\n\n");
+			return 1;
+		}
+	}
+	else
+	{
+		printf("\nYou didn't enter a number, try again, please.\n\n");
+		return 1;
+	}
+}
+
+void scanning(float* amountOfMoney)
+{
+	int operation;
+	do
+	{
+		fseek(stdin, 0, 0);
+		printf("Enter a positive natural number which represents amount of money in peso.\nAmount of money = ");
+		operation = scanf_s("%f", amountOfMoney);
+	} while (checkError(amountOfMoney, &operation));
+}
+
+int counting(int sum, int maxCoin)
+{
+	if (sum == 0)
+	{
+		count++;
+		return 0;
+	}
+	if (coinsAmount[0][maxCoin] <= sum)
+	{
+		coinsAmount[1][maxCoin]++;
+		counting(sum - coinsAmount[0][maxCoin], maxCoin);
+		coinsAmount[1][maxCoin]--;
+	}
+	if (maxCoin < 7)
+	{
+		counting(sum, maxCoin + 1);
+	}
+}
+
+int main()
+{
+	printf("The programm counts number of ways in which the entered amount of money in peso could be exchanged using British coins.\n\n");
+	float amountOfMoney;
+	scanning(&amountOfMoney);
+	int maxCoin = 0;
+	counting((int)amountOfMoney, maxCoin);
+	printf("\n%d peso could be exchanged in %d ways.\n", (int)amountOfMoney, count);
+	return 0;
+}
