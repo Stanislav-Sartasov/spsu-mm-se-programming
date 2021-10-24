075053daac3f321dddd45266dcd2ca1a681a8395
Slava-Malygina <slava.malygina@gmail.com>
2021-10-24 23:38:40 +0300

    Solution for task 2 in 2nd homework.

---
+++ Slava-Malygina/1 semester/2 homework/Task 2/Task2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void count(int sum)
+{
+	int coins[] = {1, 2, 5, 10, 20, 50, 100, 200};
+	int m = 200;
+	long long* buffer;
+	buffer = malloc(sizeof(long long) * 9 * 201);
+	memset(buffer, 0, sizeof(long long) * 9 * 201);
+	long long** arr = malloc(sizeof(long long*) * 9);
+	for (int i = 0; i < 9; i++)
+	{
+		arr[i] = &buffer[i * 201];
+	}
+	long long** table = arr;
+
+	for (int i = 1; i <= 8; i++)
+	{
+		table[i][0] = 1;
+	}
+	for (int l = 1; l <= sum; l++)
+	{
+		int i = l % m;
+		for (int j = 1; j <= 8; j++)
+		{
+			table[j][i] = table[j][(i + m - coins[j - 1]) % m] + table[j - 1][i];
+		}
+	}
+	printf("The number of ways in which the entered amount can be converted: %lld\n", table[8][sum % m]);
+}
+
+int main()
+{
+	printf("This program counts the number of ways in which the entered amount can be made using any number of different denominations English coins.\n");
+	printf("Please, enter a natural number that denotes a certain amount of money in pence:\n");
+	int sum;
+	char d;
+	scanf("%d", &sum);
+	d = getchar();
+	while ((sum <= 0) || (isalpha(d)))
+	{
+		printf("Invalid value. The entered number is too large or input cannot be converted into coins. Please, re-enter: ");
+		scanf("%d", &sum);
+		d = getchar();
+	}
+	count(sum);
+	return 0;
+ }
