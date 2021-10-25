8b590bac079d6f314e7aadb77c6a7221c0f5f7a4
Daniil <kadoch.daniil@gmail.com>
2021-10-25 19:24:46 +0300

    Modified solution in main.c (code style 3.1), modified English coins.vcxproj.

--- Daniil-Kadochnikov/First term/Second Homework/English coins/main.c
+++ Daniil-Kadochnikov/First term/Second Homework/English coins/main.c
@@ -3,9 +3,6 @@
 
 
 
-int count = 0;
-int coinsAmount[2][8] = { 200, 100, 50, 20, 10, 5, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
-
 int checkError(float* pnumber, int* operation)
 {
 	if (*operation == 1 && fgetc(stdin) == '\n')
@@ -46,32 +43,34 @@ void scanning(float* amountOfMoney)
 	} while (checkError(amountOfMoney, &operation));
 }
 
-int counting(int sum, int maxCoin)
+void counting(int sum, int maxCoin, int* count, int coinsAmount[2][8])
 {
 	if (sum == 0)
 	{
-		count++;
+		*count+=1;
 		return 0;
 	}
 	if (coinsAmount[0][maxCoin] <= sum)
 	{
 		coinsAmount[1][maxCoin]++;
-		counting(sum - coinsAmount[0][maxCoin], maxCoin);
+		counting(sum - coinsAmount[0][maxCoin], maxCoin, count, coinsAmount);
 		coinsAmount[1][maxCoin]--;
 	}
 	if (maxCoin < 7)
 	{
-		counting(sum, maxCoin + 1);
+		counting(sum, maxCoin + 1, count, coinsAmount);
 	}
 }
 
 int main()
 {
 	printf("The programm counts number of ways in which the entered amount of money in peso could be exchanged using British coins.\n\n");
+	int count = 0;
+	int coinsAmount[2][8] = { 200, 100, 50, 20, 10, 5, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
 	float amountOfMoney;
 	scanning(&amountOfMoney);
 	int maxCoin = 0;
-	counting((int)amountOfMoney, maxCoin);
+	counting((int)amountOfMoney, maxCoin, &count, coinsAmount);
 	printf("\n%d peso could be exchanged in %d ways.\n", (int)amountOfMoney, count);
 	return 0;
 }
