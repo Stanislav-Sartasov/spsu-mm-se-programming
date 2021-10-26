bf4c7ef90d4d84a59aad1d856602eef1f95da748
Daniil <kadoch.daniil@gmail.com>
2021-10-26 19:22:25 +0300

    Updated solution for the second task from the second block, English coins.

--- Daniil-Kadochnikov/First term/Second Homework/English coins/main.c
+++ Daniil-Kadochnikov/First term/Second Homework/English coins/main.c
@@ -3,21 +3,13 @@
 
 
 
-int checkError(float* pnumber, int* operation)
+int checkError(long long int* pnumber, int* operation)
 {
 	if (*operation == 1 && fgetc(stdin) == '\n')
 	{
 		if (*pnumber > 0)
 		{
-			if ((int)*pnumber == *pnumber)
-			{
-				return 0;
-			}
-			else
-			{
-				printf("\nYou entered a number that is not an integer, try again, please.\n\n");
-				return 1;
-			}
+			return 0;
 		}
 		else
 		{
@@ -27,50 +19,51 @@ int checkError(float* pnumber, int* operation)
 	}
 	else
 	{
-		printf("\nYou didn't enter a number, try again, please.\n\n");
+		printf("\nYou didn't enter an integer, try again, please.\n\n");
 		return 1;
 	}
 }
 
-void scanning(float* amountOfMoney)
+void scanning(long long int* amountOfMoney)
 {
 	int operation;
 	do
 	{
 		fseek(stdin, 0, 0);
 		printf("Enter a positive natural number which represents amount of money in peso.\nAmount of money = ");
-		operation = scanf_s("%f", amountOfMoney);
+		operation = scanf_s("%lld", amountOfMoney);
 	} while (checkError(amountOfMoney, &operation));
-}
-
-void counting(int sum, int maxCoin, int* count, int coinsAmount[2][8])
-{
-	if (sum == 0)
-	{
-		*count+=1;
-		return 0;
-	}
-	if (coinsAmount[0][maxCoin] <= sum)
-	{
-		coinsAmount[1][maxCoin]++;
-		counting(sum - coinsAmount[0][maxCoin], maxCoin, count, coinsAmount);
-		coinsAmount[1][maxCoin]--;
-	}
-	if (maxCoin < 7)
-	{
-		counting(sum, maxCoin + 1, count, coinsAmount);
-	}
 }
 
 int main()
 {
 	printf("The programm counts number of ways in which the entered amount of money in peso could be exchanged using British coins.\n\n");
-	int count = 0;
-	int coinsAmount[2][8] = { 200, 100, 50, 20, 10, 5, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
-	float amountOfMoney;
+	long long int amountOfMoney;
+	int coins[7] = { 2, 5, 10, 20, 50, 100, 200 };   //without "1" because on the next step we consider one important thing about "1", so it is useless "coins".
 	scanning(&amountOfMoney);
-	int maxCoin = 0;
-	counting((int)amountOfMoney, maxCoin, &count, coinsAmount);
-	printf("\n%d peso could be exchanged in %d ways.\n", (int)amountOfMoney, count);
+	long long int* amountOfWays;
+	amountOfWays = (long long int*)malloc((amountOfMoney + 1) * sizeof(long long int));
+	for (int firstRow = 0; firstRow < amountOfMoney + 1; firstRow++)
+	{
+		amountOfWays[firstRow] = 1;   //first row fills fully with "1", because every number has only one way to be exchanged with coins with value 1.
+	}
+	int typeOfCoins, growingAmount;
+	for (typeOfCoins = 0; typeOfCoins < 7; typeOfCoins++)
+	{
+		for (growingAmount = 1; growingAmount < amountOfMoney + 1; growingAmount++)
+		{
+			if (coins[typeOfCoins] > growingAmount)
+			{
+				continue;
+			}
+			else
+			{
+				amountOfWays[growingAmount] += amountOfWays[growingAmount - coins[typeOfCoins]];
+			}
+		}
+	}
+	
+	printf("\n%lld peso could be exchanged in %lld ways.\n", amountOfMoney, amountOfWays[amountOfMoney]);
+	free(amountOfWays);
 	return 0;
 }
