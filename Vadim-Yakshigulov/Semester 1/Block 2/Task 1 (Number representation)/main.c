5286803312be54ab873e24099ede57e965c14d4e
toadharvard <88928096+toadharvard@users.noreply.github.com>
2021-10-26 17:37:44 +0300

    Changed (n & 2) -> (n & 1) and added a welcome message about the work of the program

--- Vadim-Yakshigulov/Semester 1/Block 2/Task 1 (Number representation)/main.c
+++ Vadim-Yakshigulov/Semester 1/Block 2/Task 1 (Number representation)/main.c
@@ -11,7 +11,7 @@ void print_as_bin(long long n, int size)
 
 	for (int i = size - 1; i > -1; --i)
 	{
-		array[i] = (n & 2) ? '1' : '0';
+		array[i] = (n & 1) ? '1' : '0';
 		n >>= 1;
 	}
 
@@ -27,14 +27,17 @@ int main()
 	char patronymic[] = "Nailevich";
 
 	int product = strlen(name) * strlen(surname) * strlen(patronymic);
-
 	int int32 = -product;
 	float float32 = product;
 	double double64 = -product;
 
+	printf("This program calculates the product of the lengths of the first name, last name and patronymic\n");
+	printf("and displays a binary representation of the values in the following formats:\n\n");
+	printf("A) a negative 32-bit integer, the modulus of which is equal to the found product:\n");
 	print_as_bin(int32, 32);
+	printf("\nB) a positive single-precision floating-point number  according to the IEEE 754 standard: \n");
 	print_as_bin(*(int *) &float32, 32);
+	printf("\nC) a negative double-precision floating-point number according to the IEEE 754 standard: \n");
 	print_as_bin(*(long long *) &double64, 64);
-
 	return 0;
 }
