9a2b6e0e451e11a48ec6f8015ab5b67f8e0f59d3
letit6E <rustam.letitbe@gmail.com>
2021-10-30 00:13:26 +0300

    added solution for problem 3 (digital root)

---
+++ ShangareevRustam/Block 2/3. Digital root/digital_root/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define max(a, b) ((a) < (b))? b : a
+
+int main()
+{
+    printf("This program calculates the sum of the maximum sums "
+        "of the digital roots of natural numbers from the range [2; 999999]\n");
+    long long sum = 0, n = 1000001, * dp = (long long*)malloc(n * sizeof(long long));
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = (i % 9) + 9 * (long long)(i % 9 == 0);
+    }
+    for (int i = 2; i < n; i++)
+    {
+        for (int j = 2; j <= n / i; j++)
+        {
+            dp[i * j] = max(dp[i * j], dp[i] + dp[j]);
+        }
+    }
+    for (int i = 2; i < n - 1; i++)
+    {
+        sum += dp[i];
+    }
+    free(dp);
+    printf("%lld", sum);
+    return 0;
+}
