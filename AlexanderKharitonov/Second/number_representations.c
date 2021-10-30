93dbbc0937f01ba4115650ecfa27defa415642f4
YITSUKEN <gametopsss@gmail.com>
2021-10-30 17:49:03 +0300

    updated task 1

--- AlexanderKharitonov/Second/number_representations.c
+++ AlexanderKharitonov/Second/number_representations.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-void binSystem(char* binRep, int size, long long nameLen)
+#define NAME strlen("Харитонов") * strlen("Александр") * strlen("Алексеевич")
+
+
+
+void binSystem(char* binRep, int size, long long product)
 {
 	while (size >= 0)
 	{
-		if (nameLen & 1) binRep[size] = '1';
+		if (product & 1) binRep[size] = '1';
 		else binRep[size] = '0';
-		nameLen = nameLen >> 1;
+		product = product >> 1;
 		size--;
 	}
 }
 
-
 int main()
 {
 	printf("The program counts the product of the lengths of the surname, name and patronymic\nand gives its representation in negative binomial 32 bytes system,\npositive Single Precision IEEE 754 Floating-Point Standard\nand negative Double Precision IEEE 754 Floating-Point Standard.\n\n");
-	long long nameLen = strlen("Харитонов") * strlen("Александр") * strlen("Алексеевич");
-	printf("The product of the lengths of my surname, name and patronymic is %d.\n\n", nameLen);
-	char res_32bytes[33] = "\0", res_single_IEEE754[33] = "\0", res_double_IEEE754[65] = "\0";
-	binSystem((char *) &res_32bytes, 31, -nameLen);
-	printf("The negative binomial representation of the product in 32 bytes is %s.\n", res_32bytes);
-	nameLen = *(long int*)&nameLen;
-	binSystem((char *) &res_single_IEEE754, 31, (long int)nameLen);
-	printf("The positive binomial representation of the product in Single Precision IEEE 754 Floating-Point Standard is\n%s.\n", res_single_IEEE754);
-	nameLen = -nameLen;
-	binSystem((char *) &res_double_IEEE754, 63, nameLen);
-	printf("The negative binomial representation of the product in Double Precision IEEE 754 Floating-Point Standard is\n%s.\n", res_double_IEEE754);
+	char negative_32_representation[33] = "\0";
+	char positive_single_representation[33] = "\0";
+	char negative_double_representation[65] = "\0";
+	int int_product = NAME;
+	float float_product =NAME;
+	double double_product = NAME;
+	binSystem(&negative_32_representation, 31, -float_product);
+	float_product = *(long int*)&float_product;
+	binSystem(&positive_single_representation, 31, (long int)float_product);
+	double_product = -double_product;
+	long long number = *(long long*)&double_product;
+	binSystem(&negative_double_representation, 63, number);
+	printf("The product of the lengths of my surname, name and patronymic is %d.\n\n", int_product);
+	printf("The negative binomial representation of the product in 32 bytes is %s.\n\n", negative_32_representation);
+	printf("The negative binomial representation of the product in Double Precision IEEE 754 Floating-Point Standard is\n%s.\n", positive_single_representation);
+	printf("The positive binomial representation of the product in Single Precision IEEE 754 Floating-Point Standard is\n%s.\n\n", negative_double_representation);
 	return 0;
 }
