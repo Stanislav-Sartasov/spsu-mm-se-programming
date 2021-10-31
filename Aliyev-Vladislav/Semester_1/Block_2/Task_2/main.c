991891c568265e1f6603632014eaa30e22a6c84b
Aliyev-Vladislav <dalv.veyila@gmail.com>
2021-10-31 15:21:47 +0300

    In task 2 of block 2, the parameters for entering numbers have been changed, a function for freeing memory has been added.

--- Aliyev-Vladislav/Semester_1/Block_2/Task_2/main.c
+++ Aliyev-Vladislav/Semester_1/Block_2/Task_2/main.c
@@ -7,18 +7,17 @@
 int main()
 {
 	int coins[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
-	int length = 8;
 	int number, correctly_read;
 
 	printf("The program displays the number of ways in which you can get the amount presented as a user-entered natural number\n using any number of any English coins.\n");
 	printf("Coins of the following denominations are used: 1 pence, 2 pence, 5 pence, 10 pence, 20 pence,\n 50 pence, 1 pound (100 pence) and 2 pence (200 pence).\n\n");
-	printf("Please enter a natural number in the range [1; 100000]: ");
+	printf("Please enter a natural number in the range [1; 36695]: ");
 
 	while (true)
 	{
 		char ch;
 		correctly_read = scanf("%d%c", &number, &ch);
-		if (correctly_read == 2 && number > 0 && number < 100001 && (ch == ' ' || ch == '\n'))
+		if (correctly_read == 2 && number > 0 && number < 36696 && (ch == ' ' || ch == '\n'))
 		{
 			break;
 		}
@@ -29,10 +28,10 @@ int main()
 		}
 	}
 
-	long long maxsize = number + 1;
-	long long** ways = (unsigned long long**) malloc(8 * sizeof(long long*));
+	unsigned long long maxsize = number + 1;
+	unsigned long long **ways = (unsigned long long **) malloc(8 * sizeof(unsigned long long*));
 	for (int i = 0; i < 8; ++i)
-		ways[i] = (long long*)malloc(maxsize * sizeof(long long));
+		ways[i] = (unsigned long long*) malloc(maxsize * sizeof(unsigned long long));
 
 	for (int i = 0; i < 8; ++i)
 	{
@@ -62,7 +61,14 @@ int main()
 	{
 		answer += ways[i][number];
 	}
+
 	printf("There are %lld ways.", answer);
+
+	for (int i = 0; i < 8; ++i)
+	{
+		free(ways[i]);
+	}
+	free(ways);
 
 	return 0;
 }
