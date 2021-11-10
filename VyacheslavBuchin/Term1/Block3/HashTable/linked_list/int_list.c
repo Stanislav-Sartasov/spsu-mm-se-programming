b80447dbefd191de5f5fb97bd99a08e2a887485a
Вячеслав Бучин <chester-vyacheslav@yandex.ru>
2021-11-10 21:30:31 +0300

    Linked int list added.

---
+++ VyacheslavBuchin/Term1/Block3/HashTable/linked_list/int_list.c
@@ -0,0 +1,96 @@
+//
+// Created by Вячеслав Бучин on 09.11.2021.
+//
+
+#include "int_list.h"
+
+list_t* list_create_node(int value)
+{
+	list_t* result = (list_t*) malloc(sizeof(list_t));
+	result->value = value;
+	result->next = NULL;
+	return result;
+}
+
+/**
+ * @return a list with one node that contains count of significant nodes in list
+ */
+list_t* empty_list()
+{
+	return list_create_node(0);
+}
+
+void free_list(list_t* node)
+{
+	if (node->next)
+		free_list(node->next);
+	free(node);
+}
+
+size_t list_size(list_t* list)
+{
+	return list->value;
+}
+
+int list_empty(list_t* list)
+{
+	return list_size(list) == 0;
+}
+
+/**
+ * @return a pointer to inserted value
+ */
+list_t* list_insert(list_t* list, int value)
+{
+	list->value++;
+	while (list->next)
+		list = list->next;
+	list->next = list_create_node(value);
+
+	return list->next;
+}
+
+/**
+ * @return a pointer to inserted value
+ */
+list_t* list_insert_after(list_t* list, list_t* node, int value)
+{
+	list->value++;
+	list_t* right = node->next;
+	node->next = list_create_node(value);
+	node->next->next = right;
+	return node->next;
+}
+
+int list_remove(list_t* list, int value)
+{
+	if (!list || list_empty(list))
+		return 0;
+	list_t* head = list;
+	while (list->next)
+	{
+		if (list->next->value == value)
+		{
+			list_t* temp = list->next;
+			list->next = temp->next;
+			free(temp);
+			head->value--;
+			return 1;
+		}
+		list = list->next;
+	}
+	return 0;
+}
+
+list_t* list_find(list_t* list, int value)
+{
+	list = list->next;
+	while (list)
+	{
+		if (list->value == value) {
+			return list;
+		}
+		list = list->next;
+	}
+	return NULL;
+}
---
+++ VyacheslavBuchin/Term1/Block3/HashTable/linked_list/int_list.h
@@ -0,0 +1,31 @@
+//
+// Created by Вячеслав Бучин on 09.11.2021.
+//
+
+#ifndef HASHTABLE_INT_LIST_H
+#define HASHTABLE_INT_LIST_H
+
+#include <stdlib.h>
+
+typedef struct list_item {
+	int value;
+	struct list_item* next;
+} list_t;
+
+list_t* empty_list();
+
+void free_list(list_t* node);
+
+size_t list_size(list_t* list);
+
+int list_empty(list_t* list);
+
+list_t* list_insert(list_t* list, int value);
+
+list_t* list_insert_after(list_t* list, list_t* node, int value);
+
+list_t* list_find(list_t* list, int value);
+
+int list_remove(list_t* list, int value);
+
+#endif //HASHTABLE_INT_LIST_H
