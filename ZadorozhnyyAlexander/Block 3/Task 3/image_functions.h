a0d671f8f601aff5d68db5f280dbe57db1b1921f
Alexander-Zadorozhnyy <sasha-zadorozhnyy@mail.ru>
2021-11-19 00:08:58 +0300

    Update location of files from Task 3/ Try 2

---
+++ ZadorozhnyyAlexander/Block 3/Task 3/image_functions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "structs.h"
+
+Image read_image(FILE* file, int height, int width, int bits_per_pixel);
+
+Image copy_image(Image copy_from);
+
+void save_bmp_file(Bitmap_file file, Image picture, char* path);
+
+void free_image(Image picture);
---
+++ ZadorozhnyyAlexander/Block 3/Task 3/structs.h
@@ -0,0 +1,46 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma pack(push)
+#pragma pack(2)
+
+typedef struct Bitmap_file
+{
+	char name[2]; // it must be BM
+	int size;
+	int garbage; // not required
+	int image_offset; // offset from where image starts in file.
+
+	int header_size;
+	int width;
+	int height;
+	unsigned short colorplanes;
+	unsigned short bitsperpixel;
+	int compression;
+	int image_size;
+
+	int horizontal_resolution;
+	int vertical_resolution;
+	int number_of_colors;
+	int number_of_important_colors;
+} Bitmap_file;
+
+#pragma pack(pop)
+
+typedef struct rgb_colors
+{
+	unsigned char blue;
+	unsigned char green;
+	unsigned char red;
+} rgb_colors;
+
+typedef struct Image
+{
+	int height;
+	int width;
+	int bits_per_pixel;
+	rgb_colors** rgb;
+} Image;
