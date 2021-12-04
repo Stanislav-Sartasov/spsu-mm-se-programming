bd9caf22247937255377914b222ecb41cad90c77
Ilya Dudnikov <dudnikoff98@gmail.com>
2021-12-04 21:26:47 +0300

    Fixed all memory errors

--- Ilya-Dudnikov/block_4/big_integer.h
+++ Ilya-Dudnikov/block_4/big_integer.h
@@ -19,9 +19,9 @@ void delete_big_int(big_int *number);
 /**
  * @param val - int
  * @param size - int
- * @return big_int with the given value and (size) digits
+ * @return pointer to a big_int with the given value and (size) digits
  */
-big_int set_value(int val, int size);
+big_int *set_value(int val, int size);
 
 /**
  * Converts big integer to hexadecimal
@@ -33,22 +33,22 @@ char *big_int_to_hexadecimal(big_int *num);
 /**
  * @param left - big integer
  * @param right - big integer
- * @return sum of two big integers
+ * @return pointer to a big_int = sum of two big integers
  */
-big_int big_int_add(big_int *left, big_int *right);
+big_int *big_int_add(big_int *left, big_int *right);
 
 /**
  * @param left - big integer
  * @param right - big integer
- * @return product of two big integers
+ * @return pointer to a big_int = product of two big integers
  */
-big_int big_int_multiply(big_int *left, big_int *right);
+big_int *big_int_multiply(big_int *left, big_int *right);
 
 /**
  * @param num - big integer
  * @param power - int
- * @return num raised to the power of parameter "power"
+ * @return pointer to a big_int = num raised to the power of parameter "power"
  */
-big_int big_int_power(big_int *num, int power);
+big_int *big_int_power(big_int *num, int power);
 
 #endif //TASK_1_BIG_INTEGER_H
--- Ilya-Dudnikov/block_4/main.c
+++ Ilya-Dudnikov/block_4/main.c
@@ -6,11 +6,13 @@ int main()
 {
 	printf("This program prints value of the number 3^5000 in hexadecimal form\n");
 
-	big_int number = set_value(3, 1);
+	big_int *tmp = set_value(3, 1);
 
-	number = big_int_power(&number, 5000);
-	char *result = big_int_to_hexadecimal(&number);
+	big_int *number = big_int_power(tmp, 5000);
+	char *result = big_int_to_hexadecimal(number);
 	printf("3^5000 = %s\n", result);
-	delete_big_int(&number);
+	delete_big_int(number);
+	delete_big_int(tmp);
+	free(result);
 	return 0;
 }
