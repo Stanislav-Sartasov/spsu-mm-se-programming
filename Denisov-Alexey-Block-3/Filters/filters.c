58c71de2ebd78483d957a34a40af5fbf1607f9ca
Denisov Alexey <denisov.1881@yandex.ru>
2021-12-04 16:58:55 +0300

    Filters solution has been added.

---
+++ Denisov-Alexey-Block-3/Filters/filters.c
@@ -0,0 +1,146 @@
+#include "filters.h"
+#include <math.h>
+
+void median(BYTE**** image, bmpHeader header, int myWidth, int channels)
+{
+	for (int i = 1; i < header.biHeight - 1; i++)
+	{
+		for (int j = 1; j < myWidth - 1; j++)
+		{
+			for (int c = 0; c < channels; c++)
+			{
+				int surroundings[9];
+				for (int k = -1; k < 2; k++)
+				{
+					for (int l = -1; l < 2; l++)
+					{
+						surroundings[(k + 1) * 3 + l + 1] = (*image)[i + k][j + l][c];
+					}
+				}
+				qsort(surroundings, 9, sizeof(int), cmp);
+				(*image)[i][j][c] = surroundings[4];
+			}
+		}
+	}
+}
+
+void gauss(BYTE**** image, bmpHeader header, int myWidth, int channels)
+{
+	for (int i = 1; i < header.biHeight - 1; i++)
+	{
+		for (int j = 1; j < myWidth - 1; j++)
+		{
+			for (int c = 0; c < channels; c++)
+			{
+				double pixel = 0;
+				for (int k = -1; k < 2; k++)
+				{
+					for (int l = -1; l < 2; l++)
+					{
+						int position = (k + 1) * 3 + l + 1;
+						if ( position == 0 || position == 2 || position == 6 || position == 8)
+							pixel += 0.0626 * (*image)[i + k][j + l][c];
+						else if (position == 1 || position == 3 || position == 5 || position == 7)
+							pixel += 0.125 * (*image)[i + k][j + l][c];
+						else
+							pixel += 0.2497 * (*image)[i + k][j + l][c];
+					}
+				}
+				(*image)[i][j][c] = (BYTE)pixel;
+			}
+		}
+	}
+}
+
+void sobelX(BYTE**** image, bmpHeader header, int myWidth, int channels)
+{
+	for (int i = 1; i < header.biHeight - 1; i++)
+	{
+		for (int j = 1; j < myWidth - 1; j++)
+		{
+			for (int c = 0; c < channels; c++)
+			{
+				double pixel = 0;
+				for (int k = -1; k < 2; k++)
+				{
+					for (int l = -1; l < 2; l++)
+					{
+						int position = (k + 1) * 3 + l + 1;
+						if (position == 0 || position == 6)
+							pixel -= (*image)[i + k][j + l][c];
+						else if (position == 2 || position == 8)
+							pixel += (*image)[i + k][j + l][c];
+						else if (position == 3)
+							pixel -= 2 * (*image)[i + k][j + l][c];
+						else if (position == 5)
+							pixel += 2 * (*image)[i + k][j + l][c];
+					}
+				}
+				if (pixel == 0)
+					(*image)[i][j][c] = 0;
+				else if (abs(pixel) < 255)
+					(*image)[i][j][c] = abs(pixel);
+				else 
+					(*image)[i][j][c] = 255;
+			}
+		}
+	}
+}
+
+void sobelY(BYTE**** image, bmpHeader header, int myWidth, int channels)
+{
+	for (int i = 1; i < header.biHeight - 1; i++)
+	{
+		for (int j = 1; j < myWidth - 1; j++)
+		{
+			for (int c = 0; c < channels; c++)
+			{
+				double pixel = 0;
+				for (int k = -1; k < 2; k++)
+				{
+					for (int l = -1; l < 2; l++)
+					{
+						int position = (k + 1) * 3 + l + 1;
+						if (position == 0 || position == 2)
+							pixel -= (*image)[i + k][j + l][c];
+						else if (position == 6 || position == 8)
+							pixel += (*image)[i + k][j + l][c];
+						else if (position == 1)
+							pixel -= 2 * (*image)[i + k][j + l][c];
+						else if (position == 7)
+							pixel += 2 * (*image)[i + k][j + l][c];
+
+					}
+				}
+				if (pixel == 0)
+					(*image)[i][j][c] = 0;
+				else if (abs(pixel) < 255)
+					(*image)[i][j][c] = abs(pixel);
+				else
+					(*image)[i][j][c] = 255;
+			}
+		}
+	}
+}
+
+void greyscale(BYTE**** image, bmpHeader header, int myWidth, int channels)
+{
+	for (int i = 1; i < header.biHeight - 1; i++)
+	{
+		for (int j = 1; j < myWidth - 1; j++)
+		{
+			for (int c = 0; c < channels; c++)
+			{
+				if (c == 3)
+					(*image)[i][j][c] = 255;
+				else
+					(*image)[i][j][c] = (BYTE)fmax((*image)[i][j][2], fmax((*image)[i][j][1], (*image)[i][j][0]));
+			}
+		}
+	}
+}
+
+int cmp(const void* p1, const void* p2)
+{
+		return (*(int*)p1 - *(int*)p2);
+}
---
+++ Denisov-Alexey-Block-3/Filters/filters.h
@@ -0,0 +1,18 @@
+#include "bmpHeader.h"
+
+#ifndef FILTERS
+#define FILTERS
+
+void median(BYTE**** image, bmpHeader header, int myWidth, int channels);
+
+void gauss(BYTE**** image, bmpHeader header, int myWidth, int channels);
+
+void sobelX(BYTE**** image, bmpHeader header, int myWidth, int channels);
+
+void sobelY(BYTE**** image, bmpHeader header, int myWidth, int channels);
+
+void greyscale(BYTE**** image, bmpHeader header, int myWidth, int channels);
+
+int cmp(const void* p1, const void* p2);
+
+#endif // FILTERS
