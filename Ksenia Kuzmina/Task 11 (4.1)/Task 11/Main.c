ac0159f9c2219f389210ba0e20438509c544d941
Ksenia Kuzmina <90759630+sayumikko@users.noreply.github.com>
2021-12-13 17:05:39 +0300

    Changed base

--- Ksenia Kuzmina/Task 11 (4.1)/Task 11/Main.c
+++ Ksenia Kuzmina/Task 11 (4.1)/Task 11/Main.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 
-void multiply(char* big_int, int number, int length)
+void multiply(unsigned char* big_int, int number, int length)
 {
-	char* buffer = (char*)malloc((length + 1) * sizeof(char));
-	memset(buffer, 0, (length + 1) * sizeof(char));
+	unsigned char* buffer = (unsigned char*)malloc((length + 1) * sizeof(unsigned char));
+	memset(buffer, 0, (length + 1) * sizeof(unsigned char));
 	int remainder = 0;
 	for (int j = 0; j < length; j++)
 	{
-		buffer[j] += (big_int[j] * number + remainder) % 16;
-		remainder = (big_int[j] * number + remainder) / 16;
+		buffer[j] += (big_int[j] * number + remainder) % 256;
+		remainder = (big_int[j] * number + remainder) / 256;
 	}
 
-	memcpy(big_int, buffer, (length + 1) * sizeof(char));
+	memcpy(big_int, buffer, (length + 1) * sizeof(unsigned char));
 	free(buffer);
 }
 
 int main()
 {
 	printf("This program counts the number 3**5000 using long arithmetic algorithms and outputs it in hexadecimal notation.\n");
-	int size = (1 + (log(3) / log(16) * 5000));
-	char* number = (char*)malloc(size * sizeof(char));
-	memset(number, 0, size * sizeof(char));
+	int size = (1 + (log(3) / log(256) * 5000));
+	unsigned char* number = (unsigned char*)malloc(size * sizeof(unsigned char));
+	memset(number, 0, size * sizeof(unsigned char));
 	number[0] = 1;
 
 	for (int i = 0; i < 5000; i++)
@@ -38,8 +38,7 @@ int main()
 		if (number[size - i - 1] && !flag)
 			flag = 1;
 		if (flag)
-			printf("%X", number[size - i - 1]);
+			printf("%02X", number[size - i - 1]);
 	}
-
 	return 0;
 }
