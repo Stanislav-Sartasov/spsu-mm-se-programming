89e28b0845335f675e1b9cfc132c11aee09a5fcf
desup6 <desup6@disroot.org>
2021-12-15 12:31:44 +0300

    fixed codestyle 2.10 & now uint32 is used to store digits

--- Matvey-Kulikov/1st term/Block 4/Task 1/main.c
+++ Matvey-Kulikov/1st term/Block 4/Task 1/main.c
@@ -7,22 +7,22 @@
 struct number
 {
 	int length;
-	uint64_t* digits;
+	uint32_t* digits;
 };
 
 typedef struct number number;
 
 void remove_useless_digits(number* num)
 {
-	uint64_t* digit_pointer = num->digits;
+	uint32_t* digit_pointer = num->digits;
 	while (!*digit_pointer)
 	{
 		digit_pointer++;
 		num->length--;
 	}
 
-	uint64_t* new_digits = (uint64_t*)malloc(sizeof(uint64_t) * num->length);
-	memcpy(new_digits, digit_pointer, sizeof(uint64_t) * num->length);
+	uint32_t* new_digits = (uint32_t*)malloc(sizeof(uint32_t) * num->length);
+	memcpy(new_digits, digit_pointer, sizeof(uint32_t) * num->length);
 	free(num->digits);
 	num->digits = new_digits;
 }
@@ -33,44 +33,44 @@ void free_num(number* num)
 	free(num);
 }
 
-number* sum(number* num_1, number* num_2)
+number* sum(number* num_one, number* num_two)
 {
 	number* output_num = (number*)malloc(sizeof(number));
-	int len_1 = num_1->length;
-	int len_2 = num_2->length;
-	int max_length = (len_1 > len_2) ? len_1 : len_2;
+	int len_one = num_one->length;
+	int len_two = num_two->length;
+	int max_length = (len_one > len_two) ? len_one : len_two;
 	max_length++;
 	output_num->length = max_length;
-	output_num->digits = (uint64_t*)calloc(max_length, sizeof(uint64_t));
+	output_num->digits = (uint32_t*)calloc(max_length, sizeof(uint32_t));
 
-	while (len_1 && len_2)
+	while (len_one && len_two)
 	{
-		uint64_t sum = num_1->digits[len_1 - 1] + num_2->digits[len_2 - 1] + output_num->digits[max_length - 1];
-		output_num->digits[max_length - 1] = sum % DIGIT_BASE;
-		output_num->digits[max_length - 2] = sum / DIGIT_BASE;
+		uint64_t sum = num_one->digits[len_one - 1] + num_two->digits[len_two - 1] + output_num->digits[max_length - 1];
+		output_num->digits[max_length - 1] = (uint32_t)(sum % DIGIT_BASE);
+		output_num->digits[max_length - 2] = (uint32_t)(sum / DIGIT_BASE);
 		max_length--;
-		len_1--;
-		len_2--;
+		len_one--;
+		len_two--;
 	}
 
 	number* left_num;
 	int left_len = 0;
-	if (len_1)
+	if (len_one)
 	{
-		left_num = num_1;
-		left_len = len_1;
+		left_num = num_one;
+		left_len = len_one;
 	}
-	else if (len_2)
+	else if (len_two)
 	{
-		left_num = num_2;
-		left_len = len_2;
+		left_num = num_two;
+		left_len = len_two;
 	}
 
 	while (left_len)
 	{
 		uint64_t sum = left_num->digits[left_len - 1] + output_num->digits[max_length - 1];
-		output_num->digits[max_length - 1] = sum % DIGIT_BASE;
-		output_num->digits[max_length - 2] = sum / DIGIT_BASE;
+		output_num->digits[max_length - 1] = (uint32_t)(sum % DIGIT_BASE);
+		output_num->digits[max_length - 2] = (uint32_t)(sum / DIGIT_BASE);
 		max_length--;
 		left_len--;
 	}
@@ -79,26 +79,26 @@ number* sum(number* num_1, number* num_2)
 	return output_num;
 }
 
-number* multiply(number* num_1, number* num_2)
+number* multiply(number* num_one, number* num_two)
 {
 	number* output_num = (number*)malloc(sizeof(number));
 	output_num->length = 1;
-	output_num->digits = (uint64_t*)calloc(1, sizeof(uint64_t));
+	output_num->digits = (uint32_t*)calloc(1, sizeof(uint32_t));
 
-	for (int index_1 = num_1->length; index_1 > 0; index_1--)
+	for (int index_one = num_one->length; index_one > 0; index_one--)
 	{
 		number* temp_num = (number*)malloc(sizeof(number));
-		int temp_num_offset = num_1->length - index_1;
-		temp_num->length = num_2->length + 1 + temp_num_offset;
-		temp_num->digits = (uint64_t*)calloc(temp_num->length, sizeof(uint64_t));
+		int temp_num_offset = num_one->length - index_one;
+		temp_num->length = num_two->length + 1 + temp_num_offset;
+		temp_num->digits = (uint32_t*)calloc(temp_num->length, sizeof(uint32_t));
 
-		uint64_t digit_1 = num_1->digits[index_1 - 1];
+		uint64_t digit_one = num_one->digits[index_one - 1];
 
-		for (int index_2 = num_2->length; index_2 > 0; index_2--)
+		for (int index_two = num_two->length; index_two > 0; index_two--)
 		{
-			uint64_t product = num_2->digits[index_2 - 1] * digit_1 + temp_num->digits[index_2];
-			temp_num->digits[index_2] = product % DIGIT_BASE;
-			temp_num->digits[index_2 - 1] = product /DIGIT_BASE;
+			uint64_t product = num_two->digits[index_two - 1] * digit_one + temp_num->digits[index_two];
+			temp_num->digits[index_two] = (uint32_t)(product % DIGIT_BASE);
+			temp_num->digits[index_two - 1] = (uint32_t)(product / DIGIT_BASE);
 		}
 
 		remove_useless_digits(temp_num);
@@ -112,10 +112,10 @@ number* multiply(number* num_1, number* num_2)
 
 int main()
 {
-	printf("This program counts value of 3^5000 using bignum arithmetic and outputs it.\n0x");
+	printf("This program counts value of 3^5000 using bignum arithmetic and outputs it.\n");
 	number* three = (number*)malloc(sizeof(number));
 	three->length = 1;
-	three->digits = (uint64_t*)malloc(sizeof(uint64_t));
+	three->digits = (uint32_t*)malloc(sizeof(uint32_t));
 	three->digits[0] = 3;
 
 	number result_number = *three;
@@ -127,9 +127,10 @@ int main()
 		result = multiply(old_result, three);
 	}
 
-	for (int i = 0; i < result->length; i++)
+	printf("0x%X", result->digits[0]);
+	for (int i = 1; i < result->length; i++)
 	{
-		printf("%08lX", result->digits[i]);
+		printf("%08X", result->digits[i]);
 	}
 	return 0;
 }
