c7e21d0481ac3a251e384e82484328c356700609
i-pankrat <i.pankratov.main@gmail.com>
2021-12-15 12:49:31 +0300

    Initial commit of the 4.1 task

---
+++ Ilya-Pankratov/First term/Block4/Task 4.1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "long_arithmetic.h"
+
+#define BASE 4096
+
+int main(int argc, char* argv[])
+{
+	printf("This program calculates 3^(5000)\n16-digit number notation of 3^(5000):\n");
+	long_number* three = create_number(1, 3, BASE);
+	long_number* result = long_power(three, 5000);
+	print_long_number(result);
+	free_number(three);
+	free_number(result);
+	return 0;
+}
