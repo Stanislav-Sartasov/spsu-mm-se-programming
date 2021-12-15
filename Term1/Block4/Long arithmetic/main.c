41adef64b17104576eda53c53c605841c4c1fed5
Anton Kraev <antonkraeww@gmail.com>
2021-12-15 20:10:42 +0300

    Now less memory is required to store long numbers (32-bit cells are now used instead of 64-bit cells)

--- Term1/Block4/Long arithmetic/main.c
+++ Term1/Block4/Long arithmetic/main.c
@@ -7,10 +7,10 @@
 typedef struct big_int
 {
 	size_t size;
-	uint64_t* chunks;
+	uint32_t* chunks;
 }big_int;
 
-big_int* new_big_int(size_t size, uint64_t value);
+big_int* new_big_int(size_t size, uint32_t value);
 void free_big_int(big_int* number);
 void print_big_int_as_hex(big_int* number);
 big_int* multiply(big_int* higher, big_int* lower);
@@ -19,7 +19,7 @@ big_int* power(big_int* number, int32_t pow);
 
 int main()
 {
-	big_int* number = new_big_int(2, 3);
+	big_int* number = new_big_int(1, 3);
 	number = power(number, 5000);
 	print_big_int_as_hex(number);
 	free_big_int(number);
@@ -27,10 +27,10 @@ int main()
 }
 
 
-big_int* new_big_int(size_t size, uint64_t value)
+big_int* new_big_int(size_t size, uint32_t value)
 {
 	big_int* number = (big_int*)malloc(sizeof(big_int));
-	number->chunks = (uint64_t*)calloc(size, sizeof(uint64_t));
+	number->chunks = (uint32_t*)calloc(size, sizeof(uint32_t));
 	number->size = size;
 	number->chunks[0] = value;
 	return number;
@@ -53,18 +53,25 @@ void free_big_int(big_int* number)
 
 big_int* multiply(big_int* higher, big_int* lower)
 {
-	big_int* result = new_big_int(higher->size + lower->size, 0);
+	uint64_t* temp = (uint64_t*)calloc(higher->size + lower->size, sizeof(uint64_t));
 	for (size_t i = 0; i < higher->size; i++)
 		for (size_t j = 0; j < lower->size; j++)
-			result->chunks[i + j] += higher->chunks[i] * lower->chunks[j];
-
-
-	for (size_t i = 0; i < result->size - 1; i++)
+			temp[i + j] += (uint64_t)higher->chunks[i] * (uint64_t)lower->chunks[j];
+	
+	size_t new_size = 1;
+	for (size_t i = 0; i < higher->size + lower->size - 1; i++)
 	{
-		result->chunks[i + 1] += result->chunks[i] / BASE;
-		result->chunks[i] %= BASE;
+		temp[i + 1] += temp[i] / BASE;
+		temp[i] %= BASE;
+		if (temp[i + 1])
+			new_size++;
 	}
 
+	big_int* result = new_big_int(new_size, 0);
+	for (size_t i = 0; i < new_size; i++)
+		result->chunks[i] = (uint32_t)temp[i];
+	
+	free(temp);
 	return result;
 }
 
