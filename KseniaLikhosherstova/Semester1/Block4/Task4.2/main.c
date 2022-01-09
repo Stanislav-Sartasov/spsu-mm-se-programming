5dc3c2485c77abc40de7d27ed372473cac4529ea
Ksenia Likhoshertova <ksenia.likhosherstova@gmail.com>
2022-01-10 01:25:25 +0300

    adding solutions to Task4.2

--- KseniaLikhosherstova/Semester1/Block4/Task4.2/main.c
+++ KseniaLikhosherstova/Semester1/Block4/Task4.2/main.c
@@ -1,197 +1,64 @@
-#pragma warning (disable:4786)
+#include "allocator.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 
 
-typedef union header 
-{
-	struct 
-	{
-		unsigned int size;
-		union header* ptr;
-	} info;
-	float dummy;
-} header;
-
-
-static int nb_alloc = 0;
-static int nb_sbrk = 0;
-static header sentinelle;
-
-int size = 10; 
-char* array = NULL; 
-
-void* my_malloc(int size) 
+int main()
 {
 
-	printf("\n\n----------	calling myMalloc	--------------\n\n");
-	int sbrk_usage = 0;
-	static header* bloc;
-	static header* next_bloc;
-	nb_alloc += 1;
-	if (bloc == NULL) 
-	{
-		bloc = &sentinelle;
-		sentinelle.info.size = 0;
-		sentinelle.info.ptr = bloc;
-	}
+    printf("The program testing function: myMalloc, myRealloc, myFree\n");
 
-	header* ptr = &sentinelle;
-	header* ptr_prec = &sentinelle;
-	while ((ptr = ptr->info.ptr) != &sentinelle) 
-	{
-		ptr_prec = ptr;
-		if (ptr->info.size >= (size + sizeof(header)))
-			break;
-	}
+    init();
 
-	if (ptr == &sentinelle) 
-	{
+    printf("\nAllocating %d bytes of memory for the first array: ", 15 * sizeof(int));
+    int* first_arr = my_malloc(15 * sizeof(int));
+    for (int i = 0; i < 15; i++)
+    {
+        first_arr[i] = i;
+        printf("%i ", first_arr[i]);
+    }
+    printf("\n");
 
-		nb_sbrk++;
-		sbrk_usage = 1;
-		bloc->info.size = 800;
-		bloc->info.ptr = &sentinelle;
-		ptr_prec->info.ptr = bloc;
-	}
+    my_free(first_arr);
 
 
-	if (sbrk_usage == 0) 
-	{
-		bloc->info.size = size + sizeof(header);
-		next_bloc = bloc + size + sizeof(header);
-		next_bloc->info.size = ((ptr->info.size) - (size + sizeof(header)));
-	}
-	else 
-	{
-		next_bloc = bloc + size + sizeof(header);
-		next_bloc->info.size = ((800) - (size + sizeof(header)));
-		printf("PASSED: %u\n", 800 - (size + sizeof(header)));
-	}
-	printf("Memory is successfuly allocated\n");
-	printf("size: %d\n", ptr->info.size);
-	next_bloc->info.ptr = &sentinelle;
-	ptr_prec->info.ptr = next_bloc;
-	bloc->info.ptr = next_bloc;
-	sentinelle.info.ptr = next_bloc;
-
-	
-	return bloc;
-}
+    printf("\nAllocating %d bytes of memory for the second array: ", 5 * sizeof(int));
+    int* second_arr = my_malloc(5 * sizeof(int));
+    for (size_t i = 0; i < 5; i++)
+    {
+        second_arr[i] = i;
+        printf("%i ", second_arr[i]);
+    }
+    printf("\n");
 
 
-void my_free(char* buffer) 
-{
+    int* digits = my_realloc(second_arr, 10 * sizeof(int));
+    if (digits == NULL)
+    {
+        printf(stderr, "Coudn't reallocate!\n");
+        my_free(second_arr);
+        exit(1);
+    }
+    printf("\nReallocating memory and adding new elements from 5 to 9: ");
+    for (size_t i = 5; i < 10; i++)
+    {
+        digits[i] = i;
+        printf("%i ", digits[i]);
+    }
+    printf("\n");
 
-	printf("\n\n----------	calling myFree	--------------\n\n");
+    printf("\nUpdate the second array of %d bytes: ", 10 * sizeof(int));
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("%i ", digits[i]);
+    }
+    printf("\n");
 
-	static unsigned char my_space[1024 * 1024];
-	int j;
-	int flag = 0;
-	int allocated = 0;
-	int x = *buffer;
-	for (j = 0; j < size; j++) 
-	{
-		printf("deleted: %c [%p]\n", buffer[j], &buffer[j]);
-		if (my_space[j] == *buffer) 
-		{
-			my_space[j] = 0x80 | *buffer;
-			flag = 1;
-		}
-	}
 
-	if (flag == 1) 
-	{
-		allocated = allocated - x;
-	}
-}
+    my_free(digits);
+    printf("Freed successful\n");
 
-void* my_memcpy(void* to, const void* from, size_t n)
-{
-	const char* f_pointer = (const char*)from;
-	char* t_pointer = (char*)to;
-	for (size_t i = 0; i < n; ++i)
-	{
-		*(t_pointer++) = *(f_pointer++);
-	}
-	return to;
-}
-
-void* my_realloc(void* ptr, size_t original_length, size_t new_length) 
-{
-
-	printf("\n\n----------	calling MyRealloc	--------------\n\n");
-	printf("Memory is successfuly reallocated\n");
-
-	if (new_length == 0)
-	{
-		my_free((char*)ptr);
-		return NULL;
-	}
-	else if (!ptr)
-	{
-		return my_malloc(new_length);
-	}
-	else if (new_length <= original_length)
-	{
-		return ptr;
-	}
-	else
-	{
-		void* ptr_new = my_malloc(new_length);
-		if (ptr_new)
-		{
-			my_memcpy(ptr_new, ptr, original_length);
-			my_free((char*)ptr);
-		}
-		return ptr_new;
-	}
-}
-
-void show_elements(char* array, const int size) 
-{
-
-	printf("\nElemets of array (size: %d)\n", size);
-
-	for (int i = 0; i < size; i++)
-		printf("%c ", array[i]);
-
-} 
-
-void init() 
-{
-
-	array = (char*)my_malloc(size * sizeof(char)); 
-
-	for (int i = 0; i < size; i++)
-		array[i] = 'o'; 
-
-	printf("\nInitial array:");
-	show_elements(array, size);
-
-	int old_size = size;
-	size *= 2;
-
-	my_realloc(array, size, old_size); 
-
-	for (int i = old_size; i < size; i++)
-		array[i] = 'f'; 
-
-	printf("\nUpdate realloced array:");
-	show_elements(array, size);
-
-}
-
-int main() 
-{
-
-	printf("The program testing function: myMalloc, myRealloc, myFree\n");
-
-	init(); 
-
-	my_free(array); 
-	printf("Free successful\n");
-
-	getchar();
-	return 0; 
-
+    deinit();
+    return 0;
 }
