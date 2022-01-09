84d318224d16b903484a1c49ccd7a7727de02cd9
Abdirazikov Manuchehr <st092181@student.spbu.ru>
2022-01-09 16:01:48 +0300

    Added Representation of numbers

--- Abdurazikov_Manuchehr/Semester_1/Block_2/Representation_of_numbers/main.c
+++ Abdurazikov_Manuchehr/Semester_1/Block_2/Representation_of_numbers/main.c
@@ -5,67 +5,40 @@
 #define SURNAME "Abdurazikov"
 #define MIDDLE_NAME "Zarifovich"
 
-void bin(long long num, int bits, char* binNum);
-
-int main()
+void bin(long long dig, int bits, char* Num)
 {
-
-	printf("This program displays different representations of number 504,\n");
-	printf("which is a product of lengths of words Denisov Alexey Gennadievich\n\n");
-
-	const int x = 504;
-
-	int a = -x; char binA[32] = { 0 };
-	float b = x; char binB[32] = { 0 };
-	double c = -x; char binC[64] = { 0 };
-
-	printf("Negative 32-bit integer representation is ");
-	bin(a, 32, &binA);
-
-	printf("Positive float IEEE 754 representation is ");
-	bin(*(int*)&b, 32, &binB);
-
-	printf("Negative double IEEE 754 representation is ");
-	bin(*(long long*)&b, 64, &binC);
-
-	return 0;
-
-}
-
-void bin(long long num, int bits, char* binNum)
-{
-	if (num > 0)
+	if (dig > 0)
 	{
 		for (int i = bits - 1; i >= 0; i--)
 		{
-			binNum[i] = num % 2 ? '1' : '0';
-			num /= 2;
+			Num[i] = dig % 2 ? '1' : '0';
+			dig /= 2;
 		}
 	}
 	else
 	{
 		for (int i = bits - 1; i >= 0; i--)
 		{
-			binNum[i] = num % 2 ? '1' : '0';
-			num /= 2;
+			Num[i] = dig % 2 ? '1' : '0';
+			dig /= 2;
 		}
 		for (int i = 0; i < bits; i++)
 		{
-			if (binNum[i] == '1')
-				binNum[i] = '0';
+			if (Num[i] == '1')
+				Num[i] = '0';
 			else
-				binNum[i] = '1';
+				Num[i] = '1';
 		}
 		for (int i = bits - 1; i >= 0; i--)
 		{
-			if (binNum[i] == '1')
+			if (Num[i] == '1')
 			{
-				while (binNum[i] == '1')
+				while (Num[i] == '1')
 				{
-					binNum[i] = '0';
+					Num[i] = '0';
 					i--;
 				}
-				binNum[i] = '1';
+				Num[i] = '1';
 				break;
 			}
 		}
@@ -73,7 +46,37 @@ void bin(long long num, int bits, char* binNum)
 
 	for (int i = 0; i < bits; i++)
 	{
-		printf("%c", binNum[i]);
+		printf("%c", Num[i]);
 	}
 	printf("\n");
+}
+
+int main()
+{
+
+	printf("This program displays different representations of number 990,\n");
+	printf("which is a product of lengths of words 'Abdurazikov Manuchehr Zarifovich'\n\n");
+
+	const int x = 990;
+	int a;
+	float b;
+	double c;
+	char mas1[32] = { 0 };
+	char mas2[32] = { 0 };
+	char mas3[64] = { 0 };
+
+	a = -x;
+	b = x;
+	c = -x;
+
+	printf(" Negative 32-bit integer representation is ");
+	bin(a, 32, &mas1);
+
+	printf(" Positive float IEEE 754 representation is ");
+	bin(*(int*)&b, 32, &mas2);
+
+	printf("Negative double IEEE 754 representation is ");
+	bin(*(long long*)&b, 64, &mas3);
+
+	return 0;
 }
