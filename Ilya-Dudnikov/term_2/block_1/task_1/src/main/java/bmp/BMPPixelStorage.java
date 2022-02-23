c8cd9c0e47a3bfc64d3bb40f4528ff667a0b9719
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-02-23 21:46:43 +0300

    Implemented kernel applier

--- Ilya-Dudnikov/term_2/block_1/task_1/src/main/java/bmp/BMPPixelStorage.java
+++ Ilya-Dudnikov/term_2/block_1/task_1/src/main/java/bmp/BMPPixelStorage.java
@@ -4,12 +4,20 @@ import java.nio.ByteBuffer;
 
 public class BMPPixelStorage {
 	private int[][] image;
+	private int width;
+	private int height;
+	private int bitsPerPixel;
 
 	public BMPPixelStorage(ByteBuffer bytes, int width, int height, short bitsPerPixel) {
 		if (width % 4 != 0 && bitsPerPixel == 24)
 			width += 4 - width % 4;
 
 		int realWidth = width * (bitsPerPixel / 8);
+
+		this.width = realWidth;
+		this.height = height;
+		this.bitsPerPixel = bitsPerPixel;
+
 		image = new int[height][];
 
 		for (int row = 0; row < height; row++) {
@@ -17,5 +25,25 @@ public class BMPPixelStorage {
 				image[row][col] = bytes.get();
 			}
 		}
+	}
+
+	public int getPixel(int i, int j) {
+		return image[i][j];
+	}
+
+	public void setPixel(int i, int j, int value) {
+		image[i][j] = value;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getBitsPerPixel() {
+		return bitsPerPixel;
 	}
 }
