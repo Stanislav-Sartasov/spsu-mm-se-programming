c0cae451e2cb984f9ddd8be7da22a2da44e0e860
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2022-02-25 17:39:53 +0300

    Fixed variable names

--- Arthur Alekseev/Term 2/Task 1.1/Task 1.1/MedianFilter.cs
+++ Arthur Alekseev/Term 2/Task 1.1/Task 1.1/MedianFilter.cs
@@ -10,9 +10,9 @@ namespace Task_1._1
     {
         public void ProcessBitmap(Bitmap bmp)
         {
-            int[] pixels_r = new int[9];
-            int[] pixels_g = new int[9];
-            int[] pixels_b = new int[9];
+            int[] pixelsR = new int[9];
+            int[] pixelsG = new int[9];
+            int[] pixelsB = new int[9];
 
             Pixel[] copy = new Pixel[bmp.Width * bmp.Height];
 
@@ -28,16 +28,16 @@ namespace Task_1._1
                     for (int i = -1; i < 2; i++)
                         for (int j = -1; j < 2; j++)
                         {
-                            pixels_r[i + j * 3 + 4] = bmp.GetPixel(Normalize(x + i, bmp.Width - 1), Normalize(y + j, bmp.Height - 1)).R;
-                            pixels_g[i + j * 3 + 4] = bmp.GetPixel(Normalize(x + i, bmp.Width - 1), Normalize(y + j, bmp.Height - 1)).G;
-                            pixels_b[i + j * 3 + 4] = bmp.GetPixel(Normalize(x + i, bmp.Width - 1), Normalize(y + j, bmp.Height - 1)).B;
+                            pixelsR[i + j * 3 + 4] = bmp.GetPixel(Normalize(x + i, bmp.Width - 1), Normalize(y + j, bmp.Height - 1)).R;
+                            pixelsG[i + j * 3 + 4] = bmp.GetPixel(Normalize(x + i, bmp.Width - 1), Normalize(y + j, bmp.Height - 1)).G;
+                            pixelsB[i + j * 3 + 4] = bmp.GetPixel(Normalize(x + i, bmp.Width - 1), Normalize(y + j, bmp.Height - 1)).B;
                         }
 
-                    Array.Sort(pixels_r);
-                    Array.Sort(pixels_g);
-                    Array.Sort(pixels_b);
+                    Array.Sort(pixelsR);
+                    Array.Sort(pixelsG);
+                    Array.Sort(pixelsB);
 
-                    copy[x + y * bmp.Width] = new Pixel(pixels_r[4], pixels_g[4], pixels_b[4]);
+                    copy[x + y * bmp.Width] = new Pixel(pixelsR[4], pixelsG[4], pixelsB[4]);
                 }
 
             for (int x = 0; x < bmp.Width; x++)
