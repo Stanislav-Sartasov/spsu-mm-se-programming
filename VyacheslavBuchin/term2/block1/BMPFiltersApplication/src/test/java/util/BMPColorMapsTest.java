ab9e148c145c02d1cc81c700d08029357db22d29
Вячеслав Бучин <chester-vyacheslav@yandex.ru>
2022-02-26 20:01:22 +0300

    Utils added

---
+++ VyacheslavBuchin/term2/block1/BMPFiltersApplication/src/test/java/util/BMPColorMapsTest.java
@@ -0,0 +1,67 @@
+package util;
+
+import bmp.BMPColor;
+import bmp.BMPColorMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMPColorMapsTest {
+
+    private BMPColorMap map;
+
+    @BeforeEach
+    void setUp() {
+        var array = new BMPColor[1][1];
+        array[0][0] = new BMPColor(1, 1, 1, 1);
+        map = new BMPColorMap(array);
+    }
+
+    @Nested
+    class EqualsTest {
+
+        private BMPColorMap anotherMap;
+
+        @BeforeEach
+        void setUp() {
+            var array = new BMPColor[1][1];
+            array[0][0] = new BMPColor(1, 1, 1, 1);
+            anotherMap = new BMPColorMap(array);
+        }
+
+        @Test
+        void EqualShouldReturnTrueIfGivenMapsAreEqual() {
+            assertTrue(BMPColorMaps.equal(map, anotherMap));
+        }
+
+        @Test
+        void EqualShouldReturnFalseIfGivenMapsAreNotEqual() {
+            anotherMap.set(0, 0, new BMPColor(32, 22, 28, 42));
+            assertFalse(BMPColorMaps.equal(map, anotherMap));
+        }
+
+        @Test
+        void EqualShouldReturnFalseIfGivenMapsAreDifferentSized() {
+            var array = new BMPColor[1][2];
+            array[0][0] = new BMPColor(1, 1, 1, 1);
+            array[0][1] = new BMPColor(1, 1, 1, 1);
+            anotherMap = new BMPColorMap(array);
+            assertFalse(BMPColorMaps.equal(map, anotherMap));
+
+            array = new BMPColor[2][1];
+            array[0][0] = new BMPColor(1, 1, 1, 1);
+            array[1][0] = new BMPColor(1, 1, 1, 1);
+            anotherMap = new BMPColorMap(array);
+            assertFalse(BMPColorMaps.equal(map, anotherMap));
+        }
+
+    }
+
+    @Test
+    void CopyShouldCreateAValidCopy() {
+        var copy = BMPColorMaps.copy(map);
+        assertTrue(BMPColorMaps.equal(map, copy));
+    }
+}
---
+++ VyacheslavBuchin/term2/block1/BMPFiltersApplication/src/test/java/util/BMPColorsTest.java
@@ -0,0 +1,23 @@
+package util;
+
+import bmp.BMPColor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMPColorsTest {
+
+    private BMPColor color;
+
+    @BeforeEach
+    void setUp() {
+        color = new BMPColor(32, 22, 28, 42);
+    }
+
+    @Test
+    void CopyShouldReturnCorrectCopy() {
+        var copy = BMPColors.copy(color);
+        assertEquals(color, copy);
+    }
+}
