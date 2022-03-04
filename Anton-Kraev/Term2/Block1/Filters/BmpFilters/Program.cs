72ebe83f039fa53b1cb155b0fe8d34d3f3aa6cd1
Anton Kraev <antonkraeww@gmail.com>
2022-03-04 15:39:25 +0300

    Added solution for 1-st task

---
+++ Anton-Kraev/Term2/Block1/Filters/BmpFilters/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BmpFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program applies one of the filters to the image.");
+            Console.WriteLine("List of available filters: gray, median, gauss, sobelx, sobely, sobelxy.\n");
+
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Incorrect number of command line arguments");
+                Console.WriteLine("Correct format: <program.exe> <input.bmp> <filtername> <output.bmp>");
+                return;
+            }
+
+            BitMapReading input;
+            try
+            {
+                input = new BitMapReading(args[0]);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The input file could not be opened");
+                return;
+            }
+
+            if (!ChooseFilter.ApplyFilter(input, args[1].ToUpper(), args[2]))
+            {
+                Console.WriteLine("Unknown filter selected");
+                Console.WriteLine("List of available filters: gray, median, gauss, sobelx, sobely, sobelxy");
+            }
+        }
+    }
+}
---
+++ Anton-Kraev/Term2/Block1/Filters/BmpFiltersTest/FiltersTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using BmpFilters;
+
+namespace BmpFiltersTest
+{
+    public class FiltersTest
+    {
+        [Test]
+        public void GrayFilterTest()
+        {
+            var input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "GRAY", "../../../images/output.bmp");
+            var file1 = new BitMapReading("../../../images/gray.bmp");
+            var file2 = new BitMapReading("../../../images/output.bmp");
+            Assert.IsTrue(file1 == file2);
+        }
+
+        [Test]
+        public void MedianFilterTest()
+        {
+            var input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "MEDIAN", "../../../images/output.bmp");
+            var file1 = new BitMapReading("../../../images/median.bmp");
+            var file2 = new BitMapReading("../../../images/output.bmp");
+            Assert.IsTrue(file1 == file2);
+        }
+
+        [Test]
+        public void GaussFilterTest()
+        {
+            var input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "GAUSS", "../../../images/output.bmp");
+            var file1 = new BitMapReading("../../../images/gauss.bmp");
+            var file2 = new BitMapReading("../../../images/output.bmp");
+            Assert.IsTrue(file1 == file2);
+        }
+
+        [Test]
+        public void SobelFilterTest()
+        {
+            var input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "SOBEL", "../../../images/output.bmp");
+            var file1 = new BitMapReading("../../../images/sobel.bmp");
+            var file2 = new BitMapReading("../../../images/output.bmp");
+            Assert.IsTrue(file1 == file2);
+        }
+
+        [Test]
+        public void SobelXFilterTest()
+        {
+            var input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "SOBELX", "../../../images/output.bmp");
+            var file1 = new BitMapReading("../../../images/sobelx.bmp");
+            var file2 = new BitMapReading("../../../images/output.bmp");
+            Assert.IsTrue(file1 == file2);
+        }
+
+        [Test]
+        public void SobelYFilterTest()
+        {
+            var input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "SOBELY", "../../../images/output.bmp");
+            var file1 = new BitMapReading("../../../images/sobely.bmp");
+            var file2 = new BitMapReading("../../../images/output.bmp");
+            Assert.IsTrue(file1 == file2);
+        }
+    }
+}
