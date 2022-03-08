479d451061a0c7b09bfd78dfe117c0f755cf6c91
Tozarin <89902951+Tozarin@users.noreply.github.com>
2022-03-08 14:29:41 +0300

    Changed accessibility levels

--- StarcevMatvey/Term 2/Block 1/Task 1/Task 1/Image.cs
+++ StarcevMatvey/Term 2/Block 1/Task 1/Task 1/Image.cs
@@ -6,11 +6,11 @@ namespace Task_1
 {
     public class Image
     {
-        public byte[] header = new byte[54];
-        public byte[][] image;
-        public int[] sizesOfImage = new int[3]; // 0 - biWidht * biBitCount
-                                                // 1 - biHight
-                                                // 2 - biBitCount
+        public byte[] Header { get; private set; } = new byte[54];
+        public byte[][] Pixels { get; private set; }
+        public int[] SizesOfImage { get; private set; } = new int[3];       // 0 - biWidht * biBitCount
+                                                                            // 1 - biHight
+                                                                            // 2 - biBitCount
 
         public Image(string fileName)
         {
@@ -18,20 +18,20 @@ namespace Task_1
             {
                 using(FileStream fileStream = new FileStream(fileName, FileMode.Open))
                 {
-                    header = header
+                    Header = Header
                         .Select(x => (byte)fileStream.ReadByte())
                         .ToArray();
                     int counter = 0;
-                    sizesOfImage = sizesOfImage
-                        .Select(x => header
+                    SizesOfImage = SizesOfImage
+                        .Select(x => Header
                             .Skip(18 + (counter / 4) * (4 + (counter / 8))) // 18, 22, 28
                             .Take(4 - (counter / 8)) // 4, 4, 2
-                            .Select(x => x * (int)Math.Pow(256, counter++ % 4) / (1 + 7 * (counter / 8))) // divides sizesOfImage[2] by 8
+                            .Select(x => x * (int)Math.Pow(256, counter++ % 4) / (1 + 7 * (counter / 8))) // divides SizesOfImage[2] by 8
                             .Sum())
                         .ToArray();
-                    sizesOfImage[0] *= sizesOfImage[2];
-                    image = new byte[sizesOfImage[1]][]
-                        .Select(x => new byte[sizesOfImage[0]]
+                    SizesOfImage[0] *= SizesOfImage[2];
+                    Pixels = new byte[SizesOfImage[1]][]
+                        .Select(x => new byte[SizesOfImage[0]]
                             .Select(y => (byte)fileStream.ReadByte())
                             .ToArray())
                         .ToArray();
@@ -40,7 +40,7 @@ namespace Task_1
             }
             catch
             {
-                // header[0] = 0
+                // Header[0] = 0
             }
         }
 
@@ -50,11 +50,11 @@ namespace Task_1
             {
                 using(FileStream fileStream = new FileStream(fileName, FileMode.Create))
                 {
-                    foreach(byte x in header)
+                    foreach(byte x in Header)
                     {
                         fileStream.WriteByte(x);
                     }
-                    foreach (byte[] x in image)
+                    foreach (byte[] x in Pixels)
                     {
                         foreach (byte y in x)
                         {
@@ -74,9 +74,9 @@ namespace Task_1
         public void GreyFilter()
         {
             int counter = 0;
-            image = image
+            Pixels = Pixels
                 .Select(x => x
-                    .GroupBy(y => counter++ / sizesOfImage[2])
+                    .GroupBy(y => counter++ / SizesOfImage[2])
                     .Select(y => y
                         .Select(z => (byte)(0.3 * y.First() + 0.59 * y.Skip(1).First() + 0.11 * y.Skip(2).First())))
                     .SelectMany(y => y)
@@ -86,27 +86,27 @@ namespace Task_1
 
         public void MiddleFilter()
         {
-            for (int i = 1; i < sizesOfImage[1] - 1; i++)
+            for (int i = 1; i < SizesOfImage[1] - 1; i++)
             {
-                for (int j = sizesOfImage[2]; j < sizesOfImage[0] - sizesOfImage[2]; j += sizesOfImage[2])
+                for (int j = SizesOfImage[2]; j < SizesOfImage[0] - SizesOfImage[2]; j += SizesOfImage[2])
                 {
                     int counter = 0;
-                    byte[] buffer = image
+                    byte[] buffer = Pixels
                         .Skip(i - 1)
                         .Take(3)
                         .Select(x => x
-                            .Skip(j - sizesOfImage[2])
-                            .Take(3 * sizesOfImage[2]))
+                            .Skip(j - SizesOfImage[2])
+                            .Take(3 * SizesOfImage[2]))
                         .SelectMany(x => x)
                         .ToArray()
-                        .GroupBy(x => counter++ % sizesOfImage[2])
+                        .GroupBy(x => counter++ % SizesOfImage[2])
                         .Select(x => x
                             .OrderBy(y => y))
                         .SelectMany(x => x)
                         .ToArray();
                     for (int k = 0; k < 3; k++)
                     {
-                        image[i][j + k] = buffer.Skip(4 + 9 * k).First();
+                        Pixels[i][j + k] = buffer.Skip(4 + 9 * k).First();
                     }
                 }
             }
@@ -115,21 +115,21 @@ namespace Task_1
         public void GaussFilter()
         {
             double[] gaussFilter = { 0.307801, 0.384396, 0.307801 };
-            for (int i = 1; i < sizesOfImage[1] - 1; i++)
+            for (int i = 1; i < SizesOfImage[1] - 1; i++)
             {
-                for (int j = sizesOfImage[2]; j < sizesOfImage[0] - sizesOfImage[2]; j += sizesOfImage[2])
+                for (int j = SizesOfImage[2]; j < SizesOfImage[0] - SizesOfImage[2]; j += SizesOfImage[2])
                 {
                     int counter = 0;
                     int anotherCounter = 0;
-                    byte[] buffer = image
+                    byte[] buffer = Pixels
                         .Skip(i - 1)
                         .Take(3)
                         .Select(x => x
-                            .Skip(j - sizesOfImage[2])
-                            .Take(3 * sizesOfImage[2]))
+                            .Skip(j - SizesOfImage[2])
+                            .Take(3 * SizesOfImage[2]))
                         .SelectMany(x => x)
                         .ToArray()
-                        .GroupBy(x => anotherCounter++ % sizesOfImage[2])
+                        .GroupBy(x => anotherCounter++ % SizesOfImage[2])
                         .Select(x => x
                             .Select(y => y * gaussFilter[(counter / 3) % 3] * gaussFilter[counter++ % 3]))
                         .Select(x => x
@@ -139,7 +139,7 @@ namespace Task_1
                         .ToArray();
                     for (int k = 0; k < 3; k++)
                     {
-                        image[i][j + k] = buffer.Skip(9 * k).First();
+                        Pixels[i][j + k] = buffer.Skip(9 * k).First();
                     }
                 }
             }
@@ -149,33 +149,33 @@ namespace Task_1
         {
             GreyFilter();
             const int limit = 73;
-            byte[][] bufferImage = new byte[sizesOfImage[1]][]
-                .Select(x => new byte[sizesOfImage[0]])
+            byte[][] bufferPixels = new byte[SizesOfImage[1]][]
+                .Select(x => new byte[SizesOfImage[0]])
                 .ToArray();
             int[] sobelFilter = { -1, 2 * (flag - 1), -1 + 2 * flag, -2 * flag, 0, 2 * flag, 1 - 2 * flag, 2 - 2 * flag, 1 }; // {-1, 0, 1, -2, 0, 2, -1, 0, 1} for flag == 1, {-1, -2, -1, 0, 0, 0, 1, 2, 1} for == 0
-            for (int i = 1; i < sizesOfImage[1] - 1; i++)
+            for (int i = 1; i < SizesOfImage[1] - 1; i++)
             {
-                for (int j = sizesOfImage[2]; j < sizesOfImage[0] - sizesOfImage[2]; j++)
+                for (int j = SizesOfImage[2]; j < SizesOfImage[0] - SizesOfImage[2]; j++)
                 {
                     int counter = 0;
-                    byte[] buffer = image
+                    byte[] buffer = Pixels
                         .Skip(i - 1)
                         .Take(3)
                         .Select(x => x
-                            .Skip(j - sizesOfImage[2])
-                            .Take(3 * sizesOfImage[2]))
+                            .Skip(j - SizesOfImage[2])
+                            .Take(3 * SizesOfImage[2]))
                         .SelectMany(x => x)
                         .ToArray()
                         .Where((x, y) => y % 3 == 0)
                         .ToArray();
-                    bufferImage[i][j / sizesOfImage[2]] = (byte)(255 * Convert.ToInt32(Math.Abs(buffer.Select(x => x * sobelFilter[counter++ % 9]).Sum()) > limit)); // 255 for > limit and 0 for <=
+                    bufferPixels[i][j / SizesOfImage[2]] = (byte)(255 * Convert.ToInt32(Math.Abs(buffer.Select(x => x * sobelFilter[counter++ % 9]).Sum()) > limit)); // 255 for > limit and 0 for <=
                 }
             }
             int k = 0;
             int l = 0;
-            image = image
+            Pixels = Pixels
                 .Select(x => x
-                    .Select(y => bufferImage[k++ / sizesOfImage[0]][(l++ % sizesOfImage[0]) / sizesOfImage[2]])
+                    .Select(y => bufferPixels[k++ / SizesOfImage[0]][(l++ % SizesOfImage[0]) / SizesOfImage[2]])
                     .ToArray())
                 .ToArray();
         }
