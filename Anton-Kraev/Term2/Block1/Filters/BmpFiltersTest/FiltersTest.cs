b1751fc4bc2c1e83f7bf509053de924adb9d3d23
Anton Kraev <antonkraeww@gmail.com>
2022-03-08 18:30:03 +0300

    It is now possible to run multiple tests at once

--- Anton-Kraev/Term2/Block1/Filters/BmpFiltersTest/FiltersTest.cs
+++ Anton-Kraev/Term2/Block1/Filters/BmpFiltersTest/FiltersTest.cs
@@ -8,60 +8,60 @@ namespace BmpFiltersTest
         [Test]
         public void GrayFilterTest()
         {
-            var input = new BitMapReading("../../../images/input.bmp");
-            ChooseFilter.ApplyFilter(input, "GRAY", "../../../images/output.bmp");
-            var file1 = new BitMapReading("../../../images/gray.bmp");
-            var file2 = new BitMapReading("../../../images/output.bmp");
+            BitMapReading input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "GRAY", "../../../images/testgray.bmp");
+            var file1 = new BitMapReading("../../../images/truegray.bmp");
+            var file2 = new BitMapReading("../../../images/testgray.bmp");
             Assert.IsTrue(file1 == file2);
         }
 
         [Test]
         public void MedianFilterTest()
         {
-            var input = new BitMapReading("../../../images/input.bmp");
-            ChooseFilter.ApplyFilter(input, "MEDIAN", "../../../images/output.bmp");
-            var file1 = new BitMapReading("../../../images/median.bmp");
-            var file2 = new BitMapReading("../../../images/output.bmp");
+            BitMapReading input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "MEDIAN", "../../../images/testmedian.bmp");
+            var file1 = new BitMapReading("../../../images/truemedian.bmp");
+            var file2 = new BitMapReading("../../../images/testmedian.bmp");
             Assert.IsTrue(file1 == file2);
         }
 
         [Test]
         public void GaussFilterTest()
         {
-            var input = new BitMapReading("../../../images/input.bmp");
-            ChooseFilter.ApplyFilter(input, "GAUSS", "../../../images/output.bmp");
-            var file1 = new BitMapReading("../../../images/gauss.bmp");
-            var file2 = new BitMapReading("../../../images/output.bmp");
+            BitMapReading input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "GAUSS", "../../../images/testgauss.bmp");
+            var file1 = new BitMapReading("../../../images/truegauss.bmp");
+            var file2 = new BitMapReading("../../../images/testgauss.bmp");
             Assert.IsTrue(file1 == file2);
         }
 
         [Test]
         public void SobelFilterTest()
         {
-            var input = new BitMapReading("../../../images/input.bmp");
-            ChooseFilter.ApplyFilter(input, "SOBEL", "../../../images/output.bmp");
-            var file1 = new BitMapReading("../../../images/sobel.bmp");
-            var file2 = new BitMapReading("../../../images/output.bmp");
+            BitMapReading input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "SOBEL", "../../../images/testsobel.bmp");
+            var file1 = new BitMapReading("../../../images/truesobel.bmp");
+            var file2 = new BitMapReading("../../../images/testsobel.bmp");
             Assert.IsTrue(file1 == file2);
         }
 
         [Test]
         public void SobelXFilterTest()
         {
-            var input = new BitMapReading("../../../images/input.bmp");
-            ChooseFilter.ApplyFilter(input, "SOBELX", "../../../images/output.bmp");
-            var file1 = new BitMapReading("../../../images/sobelx.bmp");
-            var file2 = new BitMapReading("../../../images/output.bmp");
+            BitMapReading input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "SOBELX", "../../../images/testsobelx.bmp");
+            var file1 = new BitMapReading("../../../images/truesobelx.bmp");
+            var file2 = new BitMapReading("../../../images/testsobelx.bmp");
             Assert.IsTrue(file1 == file2);
         }
 
         [Test]
         public void SobelYFilterTest()
         {
-            var input = new BitMapReading("../../../images/input.bmp");
-            ChooseFilter.ApplyFilter(input, "SOBELY", "../../../images/output.bmp");
-            var file1 = new BitMapReading("../../../images/sobely.bmp");
-            var file2 = new BitMapReading("../../../images/output.bmp");
+            BitMapReading input = new BitMapReading("../../../images/input.bmp");
+            ChooseFilter.ApplyFilter(input, "SOBELY", "../../../images/testsobely.bmp");
+            var file1 = new BitMapReading("../../../images/truesobely.bmp");
+            var file2 = new BitMapReading("../../../images/testsobely.bmp");
             Assert.IsTrue(file1 == file2);
         }
     }
