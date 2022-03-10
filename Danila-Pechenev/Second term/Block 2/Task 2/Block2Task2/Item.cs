37c829161130149e4b318e32375d812d26621285
Danila Pechenev <danil-pechenev@mail.ru>
2022-03-10 23:29:25 +0300

    Added Item class.

---
+++ Danila-Pechenev/Second term/Block 2/Task 2/Block2Task2/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace Block2Task2
+{
+    internal class Item<V>
+    {
+        public int Key { get; set; }
+        public V Value { get; set; }
+        public Item(int key, V value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
