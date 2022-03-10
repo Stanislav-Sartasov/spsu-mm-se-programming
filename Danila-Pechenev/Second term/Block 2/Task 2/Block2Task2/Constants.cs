3dffa64a84acf057459d79f331696e324695fa67
Danila Pechenev <danil-pechenev@mail.ru>
2022-03-10 23:30:18 +0300

    Added Constants class.

---
+++ Danila-Pechenev/Second term/Block 2/Task 2/Block2Task2/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Block2Task2
+{
+    internal static class Constants
+    {
+        public static readonly int StartDivisor = 7;
+        public static readonly double RebalancePolicyValue = 0.5;
+    }
+}
