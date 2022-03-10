c1453e5d27eaf7dd3b8cb9411318e8d7b005cfbb
Danila Pechenev <danil-pechenev@mail.ru>
2022-03-10 23:28:31 +0300

    Initial commit | Block 2 Task 2

---
+++ Danila-Pechenev/Second term/Block 2/Task 2/Block2Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Block2Task2
+{
+    /// <summary>
+    /// Showing hash table's work.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point to the program.
+        /// </summary>
+        /// <param name="args">Сommand line arguments.</param>
+        private static void Main(string[] args)
+        {
+            var hashTable = new HashTable<int>();
+
+        }
+    }
+}
