0b25da6547fb3f7347b1065a9097c92a944c3352
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-03-15 12:28:08 +0300

    Refactored account manager

--- Ilya-Dudnikov/term_2/block_2/task_1/src/main/java/model/service/AccountManager.java
+++ Ilya-Dudnikov/term_2/block_2/task_1/src/main/java/model/service/AccountManager.java
@@ -3,17 +3,10 @@ package model.service;
 import java.util.HashMap;
 
 public class AccountManager implements IAccountManager {
-	private static AccountManager instance;
 	private HashMap<String, Integer> balanceMap;
 
-	private AccountManager() {}
-
-	static {
-		instance = new AccountManager();
-	}
-
-	public static AccountManager getInstance() {
-		return instance;
+	public AccountManager() {
+		balanceMap = new HashMap<>();
 	}
 
 	public void createNewAccount(String id, int initialBalance) {
@@ -27,7 +20,7 @@ public class AccountManager implements IAccountManager {
 		return balanceMap.get(id);
 	}
 
-	protected void transfer(String from, String to, int value) {
+	public void transfer(String from, String to, int value) {
 		if (balanceMap.get(from) < value)
 			throw new IllegalArgumentException("Insufficient balance");
 
--- Ilya-Dudnikov/term_2/block_2/task_1/src/main/java/model/service/IAccountManager.java
+++ Ilya-Dudnikov/term_2/block_2/task_1/src/main/java/model/service/IAccountManager.java
@@ -4,6 +4,8 @@ public interface IAccountManager {
 
 	public void createNewAccount(String id, int initialBalance);
 
+	public void transfer(String from, String to, int value);
+
 	public int getBalanceById(String id);
 
 	public void deposit(String to, int value);
