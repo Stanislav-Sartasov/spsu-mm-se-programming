ee76970b1838a84f930b5236e456ddc645e6a8e3
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-03-15 14:23:14 +0300

    Added a method to calculate player's score

--- Ilya-Dudnikov/term_2/block_2/task_1/src/main/java/model/hand/Hand.java
+++ Ilya-Dudnikov/term_2/block_2/task_1/src/main/java/model/hand/Hand.java
@@ -1,6 +1,7 @@
 package model.hand;
 
 import model.card.BlackjackCard;
+import model.card.CardStatus;
 
 import java.util.ArrayList;
 
@@ -21,5 +22,40 @@ public class Hand {
 
 	public void clear() {
 		cardsArray.clear();
+	}
+
+	public int getHandScore() {
+		final int ACE_RANK = 14;
+		final int ACE_VALUE = 11;
+
+		int numberOfAces = 0;
+
+		int totalScore = 0;
+		for (var card : cardsArray) {
+			if (card.getCard().rank() == ACE_RANK) {
+				numberOfAces++;
+				continue;
+			}
+			totalScore += Integer.min(card.getCard().rank(), 10);
+		}
+
+		for (int i = 0; i < numberOfAces; i++) {
+			if (totalScore + ACE_VALUE > 21)
+				totalScore += 1;
+			else
+				totalScore += ACE_VALUE;
+		}
+
+		return totalScore;
+	}
+
+	public boolean isBlackjack() {
+		return (getHandScore() == 21 && cardsArray.size() == 2);
+	}
+
+	public void showCards() {
+		for (int i = 0; i < cardsArray.size(); i++) {
+			cardsArray.get(i).setCardStatus(CardStatus.FACE_UP);
+		}
 	}
 }
