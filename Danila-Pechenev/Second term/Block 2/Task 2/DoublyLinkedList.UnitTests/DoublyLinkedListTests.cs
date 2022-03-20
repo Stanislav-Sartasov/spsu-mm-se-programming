3d9e11fd6ff6a7792a789a66046f393f7f41cc32
Danila Pechenev <danil-pechenev@mail.ru>
2022-03-20 22:55:51 +0300

    Added unit tests for Add and Remove methods.

---
+++ Danila-Pechenev/Second term/Block 2/Task 2/DoublyLinkedList.UnitTests/DoublyLinkedListTests.cs
@@ -0,0 +1,87 @@
+using NUnit.Framework;
+
+namespace DoublyLinkedList.UnitTests;
+
+public class DoublyLinkedListTests
+{
+    [Test]
+    public void AddTest1()
+    {
+        var list = new DoublyLinkedList<int>();
+        list.Add(10);
+        Assert.AreEqual(1, list.Count);
+    }
+
+    [Test]
+    public void AddTest2()
+    {
+        var list = new DoublyLinkedList<int>();
+        list.Add(10);
+        Assert.AreEqual(10, list.Get(0));
+    }
+
+    [Test]
+    public void AddTest3()
+    {
+        var list = new DoublyLinkedList<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            list.Add(i);
+        }
+        Assert.AreEqual(10, list.Count);
+    }
+
+    [Test]
+    public void AddTest4()
+    {
+        var list = new DoublyLinkedList<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            list.Add(i);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Assert.AreEqual(i, list.Get(i));
+        }
+    }
+
+    [Test]
+    public void RemoveTest1()
+    {
+        var list = new DoublyLinkedList<int>();
+        Assert.AreEqual(list.Remove(123), false);
+    }
+
+    [Test]
+    public void RemoveTest2()
+    {
+        var list = new DoublyLinkedList<int>();
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        list.Remove(10);
+        list.Remove(20);
+        Assert.AreEqual(list.Count, 1);
+    }
+
+    [Test]
+    public void RemoveTest3()
+    {
+        var list = new DoublyLinkedList<int>();
+        for (int i = 0; i < 10; i += 2)
+        {
+            list.Add(i);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Assert.AreEqual(true, list.Remove(i));
+            }
+            else
+            {
+                Assert.AreEqual(false, list.Remove(i));
+            }
+        }
+    }
+}
