6defd2e9024d2239ca850c75a65ab016a41ce9e4
ivan <ivan.shurenkov@yandex.ru>
2022-03-24 18:51:30 +0300

    Add plugin kover and task koverVerify

--- IvanShurenkov/Second Term/Block2/Task2/build.gradle.kts
+++ IvanShurenkov/Second Term/Block2/Task2/build.gradle.kts
@@ -2,19 +2,31 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
-    application
+    id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
-
-group = "me.ivan"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
+}
+
+tasks.koverHtmlReport {
+    isEnabled = true
+    htmlReportDir.set(layout.buildDirectory.dir("kover-report/html-result"))
+}
+
+tasks.koverVerify {
+    rule {
+        name = "Minimal line coverage rate in percent"
+        bound {
+            minValue = 80
+        }
+    }
 }
 
 tasks.test {
@@ -23,8 +35,4 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
