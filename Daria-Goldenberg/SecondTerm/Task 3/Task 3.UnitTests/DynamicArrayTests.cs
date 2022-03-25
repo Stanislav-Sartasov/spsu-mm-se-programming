1a61830bc36c667ded781e94cbbb672120eef299
Daria Goldenberg <dasha@igold.pp.ru>
2022-03-25 22:16:35 +0300

    Сhanged variable name.

--- Daria-Goldenberg/SecondTerm/Task 3/Task 3.UnitTests/DynamicArrayTests.cs
+++ Daria-Goldenberg/SecondTerm/Task 3/Task 3.UnitTests/DynamicArrayTests.cs
@@ -6,24 +6,24 @@ namespace Task_3.UnitTests
 {
 	public class Tests
 	{
-		private DynamicArray<int> example;
+		private DynamicArray<int> currentArray;
 
 		[SetUp]
 		public void Setup()
 		{
-			example = new DynamicArray<int>();
+			currentArray = new DynamicArray<int>();
 		}
 
 		[Test]
 		public void AddTest()
 		{
 			for (int i = 2; i <= 16; i *= 2)
-				example.Add(i);
+				currentArray.Add(i);
 
-			Assert.AreEqual(example[0], 2);
-			Assert.AreEqual(example[1], 4);
-			Assert.AreEqual(example[2], 8);
-			Assert.AreEqual(example[3], 16);
+			Assert.AreEqual(currentArray[0], 2);
+			Assert.AreEqual(currentArray[1], 4);
+			Assert.AreEqual(currentArray[2], 8);
+			Assert.AreEqual(currentArray[3], 16);
 
 			Assert.Pass();
 		}
@@ -32,15 +32,15 @@ namespace Task_3.UnitTests
 		public void RemoveAtTest()
 		{
 			for (int i = 2; i <= 16; i *= 2)
-				example.Add(i);
+				currentArray.Add(i);
 
-			example.RemoveAt(3);
-			Assert.AreEqual(example.Count, 3);
-			example.RemoveAt(0);
-			Assert.AreEqual(example.Count, 2);
-			example.RemoveAt(1);
-			Assert.AreEqual(example.Count, 1);
-			Assert.Throws<ArgumentOutOfRangeException>(() => example.RemoveAt(10));
+			currentArray.RemoveAt(3);
+			Assert.AreEqual(currentArray.Count, 3);
+			currentArray.RemoveAt(0);
+			Assert.AreEqual(currentArray.Count, 2);
+			currentArray.RemoveAt(1);
+			Assert.AreEqual(currentArray.Count, 1);
+			Assert.Throws<ArgumentOutOfRangeException>(() => currentArray.RemoveAt(10));
 
 			Assert.Pass();
 		}
@@ -49,11 +49,11 @@ namespace Task_3.UnitTests
 		public void RemoveTest()
 		{
 			for (int i = 2; i <= 32; i *= 2)
-				example.Add(i);
+				currentArray.Add(i);
 
-			Assert.AreEqual(example.Remove(24), false);
-			Assert.AreEqual(example.Remove(32), true);
-			Assert.AreEqual(example.Contains(32), false);
+			Assert.AreEqual(currentArray.Remove(24), false);
+			Assert.AreEqual(currentArray.Remove(32), true);
+			Assert.AreEqual(currentArray.Contains(32), false);
 
 			Assert.Pass();
 		}
@@ -62,13 +62,13 @@ namespace Task_3.UnitTests
 		public void ClearTest()
 		{
 			for (int i = 2; i <= 32; i *= 2)
-				example.Add(i);
+				currentArray.Add(i);
 
-			Assert.AreEqual(example.Count, 5);
-			example.Clear();
-			Assert.AreEqual(example.Count, 0);
-			example.Clear();
-			Assert.AreEqual(example.Count, 0);
+			Assert.AreEqual(currentArray.Count, 5);
+			currentArray.Clear();
+			Assert.AreEqual(currentArray.Count, 0);
+			currentArray.Clear();
+			Assert.AreEqual(currentArray.Count, 0);
 
 			Assert.Pass();
 		}
@@ -77,10 +77,10 @@ namespace Task_3.UnitTests
 		public void ContainsTest()
 		{
 			for (int i = 2; i <= 16; i *= 2)
-				example.Add(i);
+				currentArray.Add(i);
 
-			Assert.AreEqual(example.Contains(2), true);
-			Assert.AreEqual(example.Contains(3), false);
+			Assert.AreEqual(currentArray.Contains(2), true);
+			Assert.AreEqual(currentArray.Contains(3), false);
 
 			Assert.Pass();
 		}
@@ -89,10 +89,10 @@ namespace Task_3.UnitTests
 		public void IndexOfTest()
 		{
 			for (int i = 2; i <= 32; i *= 2)
-				example.Add(i);
+				currentArray.Add(i);
 
-			Assert.AreEqual(example.IndexOf(24), -1);
-			Assert.AreEqual(example.IndexOf(32), 4);
+			Assert.AreEqual(currentArray.IndexOf(24), -1);
+			Assert.AreEqual(currentArray.IndexOf(32), 4);
 
 			Assert.Pass();
 		}
@@ -101,12 +101,12 @@ namespace Task_3.UnitTests
 		public void GetSetIndexerTest()
 		{
 			for (int i = 2; i <= 32; i *= 2)
-				example.Add(i);
+				currentArray.Add(i);
 
-			Assert.Throws<ArgumentOutOfRangeException>(() => { var tmp = example[5]; });
-			Assert.Throws<ArgumentOutOfRangeException>(() => { example[5] = 64; });
-			example[4] = 64;
-			Assert.AreEqual(example[4], 64);
+			Assert.Throws<ArgumentOutOfRangeException>(() => { var tmp = currentArray[5]; });
+			Assert.Throws<ArgumentOutOfRangeException>(() => { currentArray[5] = 64; });
+			currentArray[4] = 64;
+			Assert.AreEqual(currentArray[4], 64);
 
 			Assert.Pass();
 		}
