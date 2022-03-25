f6af329e827430a1249b1b9c693694bdce6db0f6
Tozarin <89902951+Tozarin@users.noreply.github.com>
2022-03-25 21:59:18 +0300

    Some fixes

--- StarcevMatvey/Term 2/Block 2/Task_1/BlackJack/Deck.cs
+++ StarcevMatvey/Term 2/Block 2/Task_1/BlackJack/Deck.cs
@@ -2,10 +2,11 @@
 {
     public class Deck
     {
-        public List<Card> Cards { get; } = new List<Card>();
+        public List<Card> Cards { get; }
 
         public Deck()
         {
+            Cards = new List<Card>();
             string[] suits = { "H", "D", "P", "B" };
             string[] values = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
             for (int i = 0; i < 52; i++)
