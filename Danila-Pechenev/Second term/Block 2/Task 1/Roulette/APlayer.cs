9a318f9e78c1108e0e4b128bbf56560ec2d2d817
Danila Pechenev <danil-pechenev@mail.ru>
2022-03-26 01:19:02 +0300

    Added APlayer class.

---
+++ Danila-Pechenev/Second term/Block 2/Task 1/Roulette/APlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace Roulette;
+
+public abstract class APlayer
+{
+    public int amountOfMoney;
+
+    public APlayer(int sum)
+    {
+        amountOfMoney = sum;
+    }
+
+    public abstract Bet MakeBet();
+}
