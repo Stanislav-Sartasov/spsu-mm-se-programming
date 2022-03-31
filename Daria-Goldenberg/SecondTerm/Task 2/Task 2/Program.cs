9ead1b8f6be7104bd8841500cc0b43fe5981f7d2
Daria Goldenberg <dasha@igold.pp.ru>
2022-03-31 17:51:15 +0300

    Codestyle fixes.

--- Daria-Goldenberg/SecondTerm/Task 2/Task 2/Program.cs
+++ Daria-Goldenberg/SecondTerm/Task 2/Task 2/Program.cs
@@ -8,11 +8,11 @@ namespace Task_2.Casino
 	{
 		public static void Main()
 		{
-            Game game = new Game();
-            game.Players.Add(new FirstBot(game, 1000));
-            game.Players.Add(new SecondBot(game, 1000));
-            game.Players.Add(new ThirdBot(game, 1000));
-            game.Start();
-        }
+			Game game = new Game();
+			game.Players.Add(new FirstBot(game, 1000));
+			game.Players.Add(new SecondBot(game, 1000));
+			game.Players.Add(new ThirdBot(game, 1000));
+			game.Start();
+		}
 	}
 }
