0119970d2011ef2d85883542b147854119585f22
Azim Muradov <azim.muradov.dev@gmail.com>
2022-04-03 11:45:41 +0300

    Simplify build.gradle.kts in the 4th task in the 2nd term

--- AzimMuradov/Term2/Block3/Task4/app/build.gradle.kts
+++ AzimMuradov/Term2/Block3/Task4/app/build.gradle.kts
@@ -11,26 +11,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("reflect"))
-    implementation(files(projectDir.resolve(relative = "src/main/resources/base.jar").absolutePath))
+    implementation(project(":lib:base"))
 }
 
-
-val copyLibs by tasks.registering {
-    setDependsOn(listOf(":lib:base:jar"))
-
-    doFirst {
-        tasks.findByPath(":lib:base:jar")!!.outputs.files.files.first().copyTo(
-            target = projectDir.resolve(relative = "src/main/resources/base.jar"),
-            overwrite = true
-        )
-    }
-}
 
 tasks.withType<KotlinCompile> {
-    dependsOn(copyLibs)
     kotlinOptions.jvmTarget = "1.8"
 }
-
 
 application {
     mainClass.set("casino.app.MainKt")
