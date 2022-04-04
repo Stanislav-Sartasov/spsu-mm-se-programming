a089e983b8c9e262858387db0958cbd393f37d47
ArtemKhel <ArtemKhel@gmail.com>
2022-04-04 15:38:01 +0300

    Added solution for task 3

---
+++ Khelmianov-Artem/Term2/Block2/DynamicArray/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+//package dynamicarray
+//
+//
+//fun main(args: Array<String>) {
+//    println("Hello World!")
+//
+//    var array = DynamicArray<Int>()
+//    array.add(0)
+//    for (i in 1..10) {
+//        array.print()
+//        array.add(0, i)
+//    }
+//    println(array[array.size])
+//}
