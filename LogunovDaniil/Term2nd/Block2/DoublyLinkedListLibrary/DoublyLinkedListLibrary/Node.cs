ec711010b57dd15426666b8375a79681072d2a18
Daniel Logunov <daniel.e.logunov@gmail.com>
2022-04-05 21:36:56 +0300

    DoublyLinkedList uploaded

---
+++ LogunovDaniil/Term2nd/Block2/DoublyLinkedListLibrary/DoublyLinkedListLibrary/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace DoublyLinkedListLibrary
+{
+	internal class Node<T>
+	{
+		internal T Data;
+		internal Node<T>? Next;
+		internal Node<T>? Previous;
+
+		internal Node(T data)
+		{
+			Data = data;
+			Next = null;
+			Previous = null;
+		}
+	}
+}
