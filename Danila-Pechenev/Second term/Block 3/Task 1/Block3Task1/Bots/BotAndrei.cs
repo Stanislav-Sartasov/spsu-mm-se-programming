1b533446da57ce0f55dcd636f8e4bca3b78e6d0a
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-11 21:52:13 +0300

    Copied some files from task 1 block 2

---
+++ Danila-Pechenev/Second term/Block 3/Task 1/Block3Task1/Bots/BotAndrei.cs
@@ -0,0 +1,41 @@
+﻿namespace Bots;
+using System.Security.Cryptography;
+using Roulette;
+
+public class BotAndrei : APlayer
+{
+    private int currentNumber;
+
+    public BotAndrei(int sum, Casino casino)
+        : base(sum, casino)
+    {
+        currentNumber = 0;
+    }
+
+    public override void GiveNewRules(Casino casino)
+    {
+        MinBetAmount = casino.MinBetAmount;
+        MaxBetAmount = casino.MaxBetAmount;
+
+        currentNumber = 0;
+    }
+
+    protected override Bet MakeBet()
+    {
+        if (currentNumber == 0)
+        {
+            currentNumber = RandomNumberGenerator.GetInt32(36) + 1;
+            return new Bet(BetType.Number, currentNumber, MinBetAmount);
+        }
+
+        return new Bet(BetType.Number, currentNumber, MinBetAmount);
+    }
+
+    protected override void GiveResult(bool won)
+    {
+        if (won)
+        {
+            currentNumber = RandomNumberGenerator.GetInt32(36) + 1;
+        }
+    }
+}
---
+++ Danila-Pechenev/Second term/Block 3/Task 1/Block3Task1/Roulette/BetType.cs
@@ -0,0 +1,9 @@
+﻿namespace Roulette;
+
+public enum BetType
+{
+    Number,
+    Parity,
+    Colour,
+    Dozen
+}
