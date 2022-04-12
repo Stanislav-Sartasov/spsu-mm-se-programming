62c1b3a73708c30150fc795e06013d1576fce105
Azim Muradov <azim.muradov.dev@gmail.com>
2022-04-12 16:46:19 +0300

    Remove plugin annotation

--- AzimMuradov/Term2/Block3/Task7/lib/meteo/build.gradle.kts
+++ AzimMuradov/Term2/Block3/Task7/lib/meteo/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
     id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
 
