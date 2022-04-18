7338d2977c2252f2d97610529c5eea3529e65691
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-18 16:43:45 +0300

    Added additional bot Alex (just for demonstration of work of the plugin loader)

---
+++ Danila-Pechenev/Second term/Block 3/Task 1/AdditionalBots/BotAlex.cs
@@ -0,0 +1,21 @@
+﻿namespace AdditionalBots;
+using Roulette;
+
+public class BotAlex : APlayer
+{
+    public BotAlex(int sum, Casino casino)
+        : base(sum, casino)
+    {
+
+    }
+
+    protected override Bet MakeBet()
+    {
+        return new Bet(BetType.Parity, 0, MinBetAmount);
+    }
+
+    protected override void GiveResult(bool won)
+    {
+        return;
+    }
+}
