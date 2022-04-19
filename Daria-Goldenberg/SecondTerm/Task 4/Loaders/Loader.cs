5b7d5f899b9c9c47a551af65a8ba5069c3ddf147
Daria Goldenberg <dasha@igold.pp.ru>
2022-04-19 16:32:16 +0300

    Added solution for the Task 4.

---
+++ Daria-Goldenberg/SecondTerm/Task 4/Loaders/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using BlackJack;
+
+namespace LibraryLoader
+{
+	public static class Loader
+	{
+		public static List<Player> LoadBots(string path, Game game)
+		{
+			Assembly botDll = Assembly.LoadFrom(path);
+			List<Player> bots = new List<Player>();
+
+			foreach (var botType in botDll.GetExportedTypes())
+				bots.Add((Player)botType.GetConstructors()[0].Invoke(new object[] { game, 1000 }));
+
+			return bots;
+		}
+	}
+}
