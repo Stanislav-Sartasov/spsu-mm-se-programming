0b709485ff68da0142783fafaa519bafb1154713
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2022-04-19 14:34:55 +0300

    Added solution of 3.1

---
+++ Arthur Alekseev/Term 2/Task 3.1/Roulette/Roulette.BotLoader/BotLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Roulette.Common;
+
+namespace Roulette.BotLoader
+{
+	public class BotLoader
+	{
+		private List<Type> types;
+		public readonly List<string> BotNames;
+		
+		public BotLoader(string botLibraryLocation)
+		{
+			types = new List<Type>();
+			BotNames = new List<string>();
+			Assembly botLibrary = Assembly.LoadFrom(botLibraryLocation);
+			foreach (Type type in botLibrary.GetExportedTypes())
+			{
+				if (!type.IsAbstract)
+				{
+					types.Add(type);
+					BotNames.Add(type.Name);
+				}
+			}
+		}
+
+		public IPlayer? GetBot(string name, int startingMoney)
+		{
+			foreach (var type in types)
+			{
+				if (type.Name == name)
+					return (IPlayer)type.GetConstructors()[0].Invoke(new object[] { startingMoney });
+			}
+
+			return null;
+		}
+	}
+}
