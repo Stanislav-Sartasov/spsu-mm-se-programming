a3a29db7ca4974dab8eab5e3dc0a51289e084fca
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-04-20 22:17:16 +0300

    Added `BotLoader`
    Added a class to load bots into the application launcher

--- Ilya-Dudnikov/term_2/block_3/task_1/app/src/main/java/AppLauncher.java
+++ Ilya-Dudnikov/term_2/block_3/task_1/app/src/main/java/AppLauncher.java
@@ -2,26 +2,31 @@ import game.BlackjackGameLauncher;
 import player.BlackjackPlayer;
 import player.PlayerController;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.jar.JarFile;
 
 public class AppLauncher {
 	public static void main(String[] args) {
+		BotLoader botLoader = null;
+
+		try {
+			botLoader = BotLoader.createBotLoader(args[0]);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+
+		ArrayList<PlayerController> controllerList = botLoader.loadBotsFromPath(args[0]);
+
 		String poolId = "pool1337";
 		String dealerId = "dealer1337";
 
-		PlayerController basicBot = new BasicBot(new BlackjackPlayer("BasicBot"));
-		PlayerController dealerBot = new DealerBot(new BlackjackPlayer("DealerBot"));
-		PlayerController randomBot = new RandomBot(new BlackjackPlayer("RandomBot"));
-
-		ArrayList<PlayerController> controllerList = new ArrayList<>();
-		controllerList.add(basicBot);
-		controllerList.add(dealerBot);
-		controllerList.add(randomBot);
-
 		System.out.println("Bots initially get 1000$ each");
 
-		ArrayList<Integer> balances = new ArrayList<>(Collections.nCopies(3, 1000));
+		ArrayList<Integer> balances = new ArrayList<>(Collections.nCopies(controllerList.size(), 1000));
 		int totalRounds = 40;
 		int poolBalance = 1_000_000;
 
@@ -29,12 +34,21 @@ public class AppLauncher {
 
 		System.out.println("Casino's pool initially has 1000000$\n");
 
-		BlackjackGameLauncher.run(poolId, dealerId, controllerList, balances, totalRounds, poolBalance, numberOfDecks);
+		BlackjackGameLauncher.run(
+				poolId,
+				dealerId,
+				controllerList,
+				balances,
+				totalRounds,
+				poolBalance,
+				numberOfDecks
+		);
 
 		System.out.println("Results after 40 rounds:");
-		System.out.println("Casino has " + balances.get(3) + "$");
-		System.out.println("Bot with basic strategy has " + balances.get(0) + "$");
-		System.out.println("Bot that acted like dealer has " + balances.get(1) + "$");
-		System.out.println("Bot that made random moves has " + balances.get(2) + "$");
+		System.out.println("Casino has " + balances.get(balances.size() - 1) + "$");
+
+		for (int i = 0; i < controllerList.size(); i++) {
+			System.out.println(controllerList.get(i).getPlayer().getId() + " has " + balances.get(i));
+		}
 	}
 }
