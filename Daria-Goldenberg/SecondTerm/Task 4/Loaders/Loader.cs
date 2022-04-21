7cd6356228a2500ca6e88e71bd7f64bfd46e7564
Daria Goldenberg <dasha@igold.pp.ru>
2022-04-21 20:39:24 +0300

    Added check for heir of Player class.

--- Daria-Goldenberg/SecondTerm/Task 4/Loaders/Loader.cs
+++ Daria-Goldenberg/SecondTerm/Task 4/Loaders/Loader.cs
@@ -11,7 +11,10 @@ namespace LibraryLoader
 			List<Player> bots = new List<Player>();
 
 			foreach (var botType in botDll.GetExportedTypes())
-				bots.Add((Player)botType.GetConstructors()[0].Invoke(new object[] { game, 1000 }));
+				if (botType.BaseType == typeof(Player))
+					bots.Add((Player)botType.GetConstructors()[0].Invoke(new object[] { game, 1000 }));
+				else
+					Console.WriteLine("Object is not an heir of type Player.\n");
 
 			return bots;
 		}
