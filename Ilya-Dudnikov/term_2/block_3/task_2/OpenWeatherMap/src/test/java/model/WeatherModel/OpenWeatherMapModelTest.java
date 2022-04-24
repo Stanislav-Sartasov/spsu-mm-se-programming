361f130a67b9944ea896042313a55b30c5f55071
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-04-24 22:47:15 +0300

    Heavily restructured the project
    There are now three modules: one with the base library that consists of necessary interfaces and abstractions; one with implementations and extensions of the base library for Stormglass API; one with implementations and extensions of the base library for OpenWeatherMap API

---
+++ Ilya-Dudnikov/term_2/block_3/task_2/OpenWeatherMap/src/test/java/model/WeatherModel/OpenWeatherMapModelTest.java
@@ -0,0 +1,43 @@
+package model.WeatherModel;
+
+import model.WeatherData.WeatherData;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenWeatherMapModelTest {
+
+	@Test
+	void OpenWeatherMapModelInitializationExpectNull() {
+		assertNull(new OpenWeatherMapModel().data);
+	}
+
+	@Test
+	void updateState() {
+		WeatherModel weatherModel = new OpenWeatherMapModel();
+
+		HashMap<String, Double> newStats = new HashMap<>();
+
+		newStats.put("airTemperature", 10.0);
+		newStats.put("cloudCover", 10.0);
+		newStats.put("humidity", 12.0);
+		newStats.put("precipitation", 4.0);
+		newStats.put("windDirection", 51.0);
+		newStats.put("windSpeed", 1.0);
+
+		WeatherModel expectedModel = new OpenWeatherMapModel();
+		expectedModel.data = new WeatherData(
+				10.0,
+				10.0,
+				12.0,
+				4.0,
+				51.0,
+				1.0
+		);
+		weatherModel.updateState(newStats);
+
+		assertEquals(expectedModel, weatherModel);
+	}
+}
