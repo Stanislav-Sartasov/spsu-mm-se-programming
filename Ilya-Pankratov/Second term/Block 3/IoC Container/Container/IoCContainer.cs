f070226db689f98aae42b2cd9216dc872e523b22
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-04-27 18:40:05 +0300

    Initial commit

---
+++ Ilya-Pankratov/Second term/Block 3/IoC Container/Container/IoCContainer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SiteInterface;
+using Sites;
+using Microsoft.Extensions.Hosting;
+
+namespace Container
+{
+    public static class IoCContainer
+    {
+        public static List<ISite> GetSites(WeatherParameter parameter)
+        {
+            var services = Host.CreateDefaultBuilder()
+                .ConfigureServices(services => 
+                    services.AddSingleton<ISite, TomorrowIO>(svc => new TomorrowIO(parameter))
+                        .AddSingleton<ISite, OpenWeather>(svc => new OpenWeather(parameter))
+                        .AddSingleton<ISite, StormGlass>(svc => new StormGlass(parameter))
+                )
+                .Build().Services;
+
+            return services.GetServices<ISite>().ToList();
+        }
+    }
+}
