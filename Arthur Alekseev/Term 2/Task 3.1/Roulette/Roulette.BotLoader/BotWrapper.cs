b190a6929f7e82db57da71253d5473682883e6b4
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2022-04-27 10:30:41 +0300

    Unit tests for weather getter added

---
+++ Arthur Alekseev/Term 2/Task 3.1/Roulette/Roulette.BotLoader/BotWrapper.cs
@@ -0,0 +1,46 @@
+﻿using Roulette.Common;
+using Roulette.Common.Bet;
+using System.Reflection;
+
+namespace Roulette.BotLoader
+{
+	public class BotWrapper : IPlayer
+	{
+		private Type _botType;
+		private object _instance;
+		public readonly string Name;
+		public readonly string Description;
+
+		public int Money
+		{
+			get
+			{
+				return (int)_botType.GetProperty("Money").GetValue(_instance);
+			}
+		}
+
+		public BotWrapper(Type botType, int startingMoney)
+		{
+			_botType = botType;
+			_instance = _botType.GetConstructors()[0].Invoke(new object[] { startingMoney });
+
+			Description = botType.GetField("Description").GetValue(_instance).ToString();
+			Name = botType.GetField("Name").GetValue(_instance).ToString();
+		}
+
+		public void GiveMoney(int amount)
+		{
+			_botType.InvokeMember("GiveMoney", BindingFlags.InvokeMethod, null, _instance, new object[] { amount });
+		}
+
+		public List<Bet> MakeBets()
+		{
+			return (List<Bet>)_botType.InvokeMember("MakeBets", BindingFlags.InvokeMethod, null, _instance, new object[] { });
+		}
+
+		public int TakeMoney(int requestedAmount)
+		{
+			return (int)_botType.InvokeMember("TakeMoney", BindingFlags.InvokeMethod, null, _instance, new object[] { requestedAmount });
+		}
+	}
+}
