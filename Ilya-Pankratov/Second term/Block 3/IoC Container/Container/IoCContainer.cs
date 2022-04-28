03ced5fbd67e08f97bb90c6278c01fda4e9f1f5d
Ilya Pankratov <i.pankratov.main@gmail.com>
2022-04-28 19:13:58 +0300

    Added the ability to disable the service

--- Ilya-Pankratov/Second term/Block 3/IoC Container/Container/IoCContainer.cs
+++ Ilya-Pankratov/Second term/Block 3/IoC Container/Container/IoCContainer.cs
@@ -7,17 +7,44 @@ namespace Container
 {
     public static class IoCContainer
     {
+        public static List<SitesName> ConnectedSites { get; private set; } = new List<SitesName>
+        {
+            SitesName.OpenWeather, SitesName.StormGlass, SitesName.TomorrowIO
+        };
+
         public static List<ISite> GetSites(WeatherParameter parameter)
         {
             var services = Host.CreateDefaultBuilder()
                 .ConfigureServices(services => 
-                    services.AddSingleton<ISite, TomorrowIO>(svc => new TomorrowIO(parameter))
-                        .AddSingleton<ISite, OpenWeather>(svc => new OpenWeather(parameter))
-                        .AddSingleton<ISite, StormGlass>(svc => new StormGlass(parameter))
+                    services.AddSingleton<ISite, TomorrowIO>(svc => ConnectedSites.Contains(SitesName.TomorrowIO) ? new TomorrowIO(parameter) : null)
+                        .AddSingleton<ISite, OpenWeather>(svc => ConnectedSites.Contains(SitesName.OpenWeather) ? new OpenWeather(parameter) : null)
+                        .AddSingleton<ISite, StormGlass>(svc => ConnectedSites.Contains(SitesName.StormGlass) ? new StormGlass(parameter) : null)
                 )
                 .Build().Services;
 
-            return services.GetServices<ISite>().ToList();
+            return services.GetServices<ISite>().Where(x => x != null).ToList();
+        }
+
+        public static bool RemoveSiteFromContainer(SitesName name)
+        {
+            if (ConnectedSites.Contains(name))
+            {
+                ConnectedSites.Remove(name);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool AddSiteToContainer(SitesName name)
+        {
+            if (!ConnectedSites.Contains(name))
+            {
+                ConnectedSites.Add(name);
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- Ilya-Pankratov/Second term/Block 3/IoC Container/WeatherViewer/Program.cs
+++ Ilya-Pankratov/Second term/Block 3/IoC Container/WeatherViewer/Program.cs
@@ -12,34 +12,150 @@ namespace Weather
             Console.WriteLine("The program shows the weather from 3 source\n");
 
             var sites = IoCContainer.GetSites(WeatherParameter.Current);
+            bool flag = true;
+            bool updateFlag = true;
+            bool changedFlag = false;
 
-            while (true)
+            while (flag)
             {
-                foreach (var site in sites)
+                if (updateFlag)
                 {
-                    ConsoleOutput.WriteWeather(site.GetCityWeatherForecast());
+                    foreach (var site in sites)
+                    {
+                        ConsoleOutput.WriteWeather(site.GetCityWeatherForecast());
+                    }
+
+                    updateFlag = false;
                 }
 
-                Console.WriteLine("If you want to update data from sites, then write - 'Update'.\nIf you want to close the program, then write - 'Exit'.\n");
+                if (changedFlag)
+                {
+                    sites = IoCContainer.GetSites(WeatherParameter.Current);
+                    changedFlag = false;
+                }
 
-                var userAnswer = Console.ReadLine().ToLower().Replace(" ", "");
+                Console.WriteLine("If you want to update data from sites, then write - 'Update'.\nAvailable sites: OpenWeather, StormGlass, TomorrowIO\nYou may add or delete site using such command:" +
+                                  " 'Add [siteName]' or 'Delete [siteName]'\nYou can also view the sites from which data is requested using command: 'Sites'\n" +
+                                  "If you want to close the program, then write - 'Exit'.\n");
 
-                while (userAnswer != "exit" && userAnswer != "update")
+                var userAnswer = Console.ReadLine().ToLower();
+                var commands = userAnswer.Split(" ");
+
+                while (userAnswer.Last() == ' ')
+                {
+                    userAnswer.Remove(userAnswer.Length - 1);
+                }
+
+                while (userAnswer != "exit" && userAnswer != "update" && !CheckCommand(commands))
                 {
                     Console.WriteLine("Wrong command. Try again, please.\nIf you want to update data from sites, then write - 'Update'.\n" +
-                                      "If you want to close the program, then write - 'Exit'.");
+                                      "You may add or delete site using such command: 'Add [siteName]' or 'Delete [siteName]'\nYou can also view " +
+                                      "the sites from which data is requested using command: 'Sites'\nIf you want to close the program, then write - 'Exit'.\n");
 
                     userAnswer = Console.ReadLine().ToLower().Replace(" ", "");
+                    commands = userAnswer.Split(" ");
+
+                    while (userAnswer.Last() == ' ')
+                    {
+                            userAnswer.Remove(userAnswer.Length - 1);
+                    }
                 }
 
                 if (userAnswer == "exit")
                 {
-                    break;
+                    flag = false;
+                }
+                else if (commands[0] == "sites")
+                {
+                    Console.Write("\nThese sites are connected: ");
+
+                    foreach (var site in sites)
+                    {
+                        Console.Write(site.Name + " ");
+                    }
+
+                    Console.WriteLine("\n");
+                }
+                else if (commands[0] == "add")
+                {
+                    var name = GetSite(commands[1]);
+
+                    if (IoCContainer.ConnectedSites.Contains(name))
+                    {
+                        Console.WriteLine("\nThis site is alreade connected :)");
+                    }
+                    else
+                    {
+                        IoCContainer.AddSiteToContainer(name);
+                        changedFlag = true;
+                    }
+
+                    Console.WriteLine("\n");
+                }
+                else if (commands[0] == "delete")
+                {
+                    var name = GetSite(commands[1]);
+
+                    if (!IoCContainer.ConnectedSites.Contains(name))
+                    {
+                        Console.WriteLine("\nThis site is alreade disconnected :)");
+                    }
+                    else
+                    {
+                        IoCContainer.RemoveSiteFromContainer(name);
+                        changedFlag = true;
+                    }
+
+                    Console.WriteLine("\n");
+                }
+                else if (commands[0] == "update")
+                {
+                    updateFlag = true;
                 }
             }
 
             Console.WriteLine("\nThat's all! Thank you!\n");
         }
+
+        private static bool CheckCommand(string[] command)
+        {
+            List<string> names = new List<string>()
+            {
+                "openweather", "tomorrowio", "stormglass"
+            };
+
+            if (command.Length == 1 && command[0] == "sites")
+            {
+                return true;
+            }
+            else if (command.Length != 2)
+            {
+                return false;
+            }
+            else if ((command[0] == "add" || command[0] == "delete") && names.Contains(command[1]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static SitesName GetSite(string siteName)
+        {
+            if (siteName == "openweather")
+            {
+                return SitesName.OpenWeather;
+            }
+            else if (siteName == "tomorrowio")
+            {
+                return SitesName.TomorrowIO;
+            }
+            else
+            {
+                return SitesName.StormGlass;
+            }
+        }
     }
 }
-
