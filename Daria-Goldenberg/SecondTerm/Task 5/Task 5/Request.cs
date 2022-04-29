ece078ef3c716e2335c0168dfce9a790b61132b1
Daria Goldenberg <dasha@igold.pp.ru>
2022-04-29 22:28:57 +0300

    Added solution for the Task 5.

---
+++ Daria-Goldenberg/SecondTerm/Task 5/Task 5/Request.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace Task_5
+{
+	public class Request : IRequest
+	{
+		private HttpWebRequest request;
+
+		public string Response { get; private set; }
+		public bool Connected { get; private set; }
+
+		public void Run(string address)
+		{
+			request = (HttpWebRequest)WebRequest.Create(address);
+			request.Method = "GET";
+			
+			try
+			{
+				HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+				using (Stream stream = response.GetResponseStream())
+				{
+					Response = new StreamReader(stream).ReadToEnd();
+				}
+				Connected = true;
+			}
+			catch (Exception)
+			{
+				Connected = false;
+			}
+		}
+	}
+}
