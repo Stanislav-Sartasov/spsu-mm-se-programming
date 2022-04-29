4c5bac03d487125af1537c89369a32c564c4b923
Danila Pechenev <danil-pechenev@mail.ru>
2022-04-30 01:18:42 +0300

    Add ADefaultWeatherGetter abstract class

---
+++ Danila-Pechenev/Second term/Block 3/Task 2/Block3Task2/DataGetter/ADefaultWeatherGetter.cs
@@ -0,0 +1,43 @@
+﻿namespace DataGetter;
+using System.Net;
+
+public abstract class ADefaultWeatherGetter : IWeatherGetter
+{
+    protected readonly string APIKey;
+    protected ADefaultWeatherGetter(string key)
+    {
+        APIKey = key;
+    }
+
+    public string[]? GetWeather(double[] locationСoordinates, string[] fields)
+    {
+        string URL = GenerateURL(locationСoordinates, fields);
+        var request = WebRequest.Create(URL);
+
+        try
+        {
+            using var response = request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            return ParseResponseStream(responseStream, fields);
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message.Contains("Unauthorized"))
+            {
+                return null;
+            }
+
+            string[] result = new string[fields.Length];
+            for (int i = 0; i < fields.Length; i++)
+            {
+                result[i] = "No data";
+            }
+
+            return result;
+        }
+    }
+
+    public abstract string[]? ParseResponseStream(Stream responseStream, string[] fields);
+
+    protected abstract string GenerateURL(double[] locationСoordinates, string[] fields);
+}
