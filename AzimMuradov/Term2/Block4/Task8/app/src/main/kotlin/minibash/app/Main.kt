3e276cae5672362960eee1e499de6a59c91c553f
Azim Muradov <azim.muradov.dev@gmail.com>
2022-05-05 21:56:51 +0300

    Add Main.kt

---
+++ AzimMuradov/Term2/Block4/Task8/app/src/main/kotlin/minibash/app/Main.kt
@@ -0,0 +1,34 @@
+package minibash.app
+
+import kotlinx.coroutines.runBlocking
+import minibash.MiniBashImpl
+import minibash.interpretation.InterpreterImpl
+import minibash.parsing.SimpleParser
+import minibash.pipe.commands.*
+
+fun main(args: Array<String>) {
+    when (args.size) {
+        0 -> {
+            val echo = EchoCommand(name = "echo")
+            val exit = ExitCommand(name = "exit")
+            val pwd = PwdCommand(name = "pwd")
+            val cat = CatCommand(name = "cat")
+            val wc = WcCommand(name = "wc")
+
+            val miniBash = MiniBashImpl(
+                parser = SimpleParser,
+                interpreter = InterpreterImpl(availableCommands = listOf(echo, exit, pwd, cat, wc))
+            )
+
+            runBlocking {
+                miniBash.run(System.`in`, System.out, System.err)
+            }
+        }
+        1 -> {
+            // TODO
+        }
+        else -> {
+            System.err.println("minibash: too many arguments")
+        }
+    }
+}
