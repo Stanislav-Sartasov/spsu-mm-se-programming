db8ab5d92a126325b7fbc786a4755b17a6f6893d
ch3zych3z <chester-vyacheslav@yandex.ru>
2022-05-08 18:29:54 +0300

    Fixed tests

--- VyacheslavBuchin/term2/block3/WeatherIoC/src/test/kotlin/service/weather/open_weather_map/OpenWeatherMapServiceTest.kt
+++ VyacheslavBuchin/term2/block3/WeatherIoC/src/test/kotlin/service/weather/open_weather_map/OpenWeatherMapServiceTest.kt
@@ -1,8 +1,11 @@
 package service.weather.open_weather_map
 
 import service.weather.WeatherServiceTest
+import service.weather.open_weather_map.parser.OpenWeatherMapJSONParser
 
 internal class OpenWeatherMapServiceTest: WeatherServiceTest() {
 	override val referenceName = "openweathermap.org"
-	override val service = OpenWeatherMapService()
+	override val service = OpenWeatherMapService(
+		OpenWeatherMapJSONParser()
+	)
 }
