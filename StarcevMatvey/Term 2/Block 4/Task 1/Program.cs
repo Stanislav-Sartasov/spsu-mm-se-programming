8c024bf70680a6675ea68a3c95a4b1773f6d8b6b
Tozarin <89902951+Tozarin@users.noreply.github.com>
2022-05-09 16:07:15 +0300

    Added solution for task 1

---
+++ StarcevMatvey/Term 2/Block 4/Task 1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bash = new MyBash();
+            var painter = new Painter();
+
+            painter.Draw("\n");
+
+            while (true)
+            {
+                painter.DrawBlue($"{Environment.UserName} {Environment.MachineName} ");
+                painter.DrawYellow($"{bash.Path}\n");
+                painter.Draw($"{bash.Invite}> ");
+
+                bash.InitCommands(new Reader());
+                var errors = bash.GetErrorMessenges();
+
+                if (errors.Count != 0)
+                {
+                    foreach (var error in errors)
+                    {
+                        painter.DrawMagneta(error + "\n");
+                    }
+                    painter.Draw("\n");
+                }
+                else
+                {
+                    bash.ExecuteCommands();
+                    errors = bash.GetErrorMessenges();
+
+                    if (errors.Count != 0)
+                    {
+                        foreach (var error in errors)
+                        {
+                            painter.DrawMagneta(error + "\n");
+                        }
+                        painter.Draw("\n");
+                    }
+                    else
+                    {
+                        painter.Draw(bash.Output + "\n");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StarcevMatvey/Term 2/Block 4/Task 1/Reader.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+using Interfaces;
+
+namespace Task_1
+{
+    public class Reader : IReader
+    {
+        public bool IsRead { get; private set; }
+        public string ErrorMessenge { get; private set; }
+
+        public Reader()
+        {
+            IsRead = false;
+            ErrorMessenge = "Reader was not called";
+        }
+
+        public string Read()
+        {
+            var input = ReadLine();
+            if (input == null || input.Length == 0)
+            {
+                ErrorMessenge = "Empty input";
+                return null;
+            }
+
+            if (input.First() == ' ')
+            {
+                ErrorMessenge = "Input must not start with space";
+                return null;
+            }
+
+            IsRead = true;
+
+            return input;
+        }
+    }
+}
