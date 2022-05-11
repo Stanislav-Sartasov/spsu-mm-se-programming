af9243f2fa8443c4f1a2b8d2645a8176af3a2dca
Tozarin <89902951+Tozarin@users.noreply.github.com>
2022-05-11 18:57:24 +0300

    Added new solution for task 1

---
+++ StarcevMatvey/Term 2/Block 4/Task 1/Interfaces/IReader.cs
@@ -0,0 +1,9 @@
+﻿namespace Interfaces
+{
+    public interface IReader
+    {
+        public bool IsRead { get; }
+        public string ErrorMessenge { get; }
+        public string Read();
+    }
+}
---
+++ StarcevMatvey/Term 2/Block 4/Task 1/Task 1/Analyzer.cs
@@ -0,0 +1,184 @@
+﻿using Enumerations;
+
+namespace Task_1
+{
+    public class Analyzer
+    {
+        private List<(string str, TypeOfParsedStr type)> data;
+        private List<Command> listOfCommands;
+        private Queue<(string name, string value)> localVar;
+        private Queue<string> arguments;
+        public bool IsAnalyzed { get; private set; }
+        public string ErrorMessenge { get; private set; }
+
+        public Analyzer(List<(string str, TypeOfParsedStr type)> data)
+        {
+            this.data = data;
+            IsAnalyzed = false;
+            listOfCommands = new List<Command>();
+            localVar = new Queue<(string name, string value)>();
+            arguments = new Queue<string>();
+            ErrorMessenge = "Analyzer was not called";
+        }
+
+        public void Analyze()
+        {
+            var listOfCmds = new List<Command>();
+            var localV = new Queue<(string name, string value)>();
+            var lastIsPipeline = true;
+            var lastIsCommand = false;
+            IsAnalyzed = true;
+
+            for (var i = 0; i < data.Count; i++)
+            {
+                if (data[i].type == TypeOfParsedStr.Argument)
+                {
+                    if (lastIsPipeline)
+                    {
+                        IsAnalyzed = false;
+                        ErrorMessenge = data[i].str + ": command not found";
+                        break;
+                    }
+
+                    else if (!lastIsCommand)
+                    {
+                        var lastArg = arguments.Dequeue();
+                        lastArg += "\n" + data[i].str;
+                        arguments.Enqueue(lastArg);
+                        var lastCommand = listOfCmds.Last();
+                        listOfCmds = listOfCmds.SkipLast(1).ToList();
+                        listOfCmds.Add(Command.Pipeline);
+                        listOfCmds.Add(lastCommand);
+                        lastIsCommand = false;
+                        lastIsPipeline = false;
+                    }
+
+                    else
+                    {
+                        arguments.Enqueue(data[i].str);
+                        var lastCommand = listOfCmds.Last();
+                        listOfCmds = listOfCmds.SkipLast(1).ToList();
+                        listOfCmds.Add(Command.SetArgument);
+                        listOfCmds.Add(lastCommand);
+                        lastIsCommand = false;
+                        lastIsPipeline = false;
+                    }
+                }
+
+                else if (data[i].type == TypeOfParsedStr.ProcessStart)
+                {
+                    arguments.Enqueue(data[i].str);
+                    listOfCmds.Add(Command.SetArgument);
+                    listOfCmds.Add(Command.ProcessStart);
+                    
+                    lastIsCommand = true;
+                    lastIsPipeline = false;
+                }
+
+                else if (data[i].type == TypeOfParsedStr.PipeLine)
+                {
+                    if (lastIsPipeline)
+                    {
+                        IsAnalyzed = false;
+                        ErrorMessenge = "Two | in a row";
+                        break;
+                    }
+
+                    else
+                    {
+                        listOfCmds.Add(Command.Pipeline);
+                        lastIsPipeline = true;
+                        lastIsCommand = false;
+                    }
+                }
+
+                else if (data[i].type == TypeOfParsedStr.DeclaringLocalVariable)
+                {
+                    if (!lastIsPipeline || lastIsCommand)
+                    {
+                        IsAnalyzed = false;
+                        ErrorMessenge = "Two commands in a row";
+                        break;
+                    }
+
+                    else
+                    {
+                        lastIsPipeline = false;
+                        lastIsCommand = true;
+                        var command = data[i].str.Split('=');
+                        listOfCmds.Add(Command.SetLocalVar);
+                        localV.Enqueue((command.First(), command.Last()));
+                    }
+                }
+
+                else if (data[i].type == TypeOfParsedStr.Command)
+                {
+                    if (!lastIsPipeline)
+                    {
+                        IsAnalyzed = false;
+                        ErrorMessenge = "Two commands in a row";
+                        break;
+                    }
+
+                    else
+                    {
+                        lastIsPipeline= false;
+                        lastIsCommand= true;
+                        listOfCmds.Add(GetCommand(data[i].str));
+                    }
+                }
+            }
+
+            if (IsAnalyzed)
+            {
+                listOfCommands = listOfCmds;
+                localVar = localV;
+            }
+        }
+
+        public Command GetCommand(string command)
+        {
+            switch (command)
+            {
+                case "exit":
+                    return Command.Exit;
+                case "echo":
+                    return Command.Echo;
+                case "pwd":
+                    return Command.Pwd;
+                case "cat":
+                    return Command.Cat;
+                case "cd":
+                    return Command.Cd;
+                case "wc":
+                    return Command.Wc;
+                case "whoami":
+                    return Command.Whoami;
+                case "|":
+                    return Command.Pipeline;
+                case "$":
+                    return Command.SetLocalVar;
+                case "clear":
+                    return Command.Clear;
+                default:
+                    return Command.Exit;
+            }
+
+        }
+
+        public List<Command> GetCommands()
+        {
+            return new List<Command>(listOfCommands);
+        }
+
+        public Queue<(string name, string value)> GetArgumentsForSetLocalVar()
+        {
+            return new Queue<(string name, string value)>(localVar);
+        }
+
+        public Queue<string> GetArguments()
+        {
+            return new Queue<string>(arguments);
+        }
+    }
+}
