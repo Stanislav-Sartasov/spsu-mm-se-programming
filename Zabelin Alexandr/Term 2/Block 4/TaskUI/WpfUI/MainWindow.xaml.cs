e021ccba1cae0a40390f7927ada7ec153b9b326c
Alexandr Zabelin <alexandre.zabelin@gmail.com>
2022-05-15 20:51:44 +0300

    Added solution for 4.1 Task

---
+++ Zabelin Alexandr/Term 2/Block 4/TaskUI/WpfUI/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WeatherWebAPI;
+using WeatherLibrary;
+using DateTimeManager;
+
+namespace WpfUI
+{
+    public partial class MainWindow : Window
+    {
+        private TomorrowParser tomorrowParser = new TomorrowParser();
+        private StormGlassParser stormGlassParser = new StormGlassParser();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            weatherTable.Items.Add(tomorrowParser.GetWeather());
+            weatherTable.Items.Add(stormGlassParser.GetWeather());
+        }
+
+        private void updateWeatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            Date.UpdateTime();
+
+            weatherTable.Items.Clear();
+            weatherTable.Items.Add(tomorrowParser.GetWeather());
+            weatherTable.Items.Add(stormGlassParser.GetWeather());
+        }
+    }
+}
