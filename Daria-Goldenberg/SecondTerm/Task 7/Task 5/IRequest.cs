1ab16c1615a97b47113398103604234d302cda59
Daria Goldenberg <dasha@igold.pp.ru>
2022-05-15 12:05:57 +0300

    Added solution for the Task 7.

---
+++ Daria-Goldenberg/SecondTerm/Task 7/Task 5/IRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_5
+{
+	public interface IRequest
+	{
+		public string Response { get; }
+		public bool Connected { get; }
+
+		public void Run(string address);
+	}
+}
---
+++ Daria-Goldenberg/SecondTerm/Task 7/Task 7/WPF/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task_5;
+
+namespace WPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		OpenWeather openWeather = new OpenWeather(new Request());
+		TomorrowIo tomorrowIo = new TomorrowIo(new Request());
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			UpdateData();
+		}
+
+		private void UpdateButtonClick(object sender, RoutedEventArgs e)
+		{
+			UpdateData();
+		}
+
+		private void ExitButtonClick(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		private void UpdateData()
+		{
+			string[] dataFromTomorrowIo = new string[5];
+			string[] dataFromOpenWeather = new string[5];
+
+			for (int i = 0; i < 5; i++)
+			{
+				((TextBlock)tomorrowIoGrid.Children[i]).Text = "";
+				((TextBlock)openWeatherGrid.Children[i]).Text = "";
+			}
+
+			try
+			{
+				dataFromTomorrowIo = GetData(tomorrowIo.GetData());
+				for (int i = 0; i < 5; i++)
+					((TextBlock)tomorrowIoGrid.Children[i]).Text = dataFromTomorrowIo[i];
+			}
+			catch (Exception ex)
+			{
+				((TextBlock)tomorrowIoGrid.Children[0]).Text = ex.Message;
+			}
+
+			try
+			{
+				dataFromOpenWeather = GetData(openWeather.GetData());
+				for (int i = 0; i < 5; i++)
+					((TextBlock)openWeatherGrid.Children[i]).Text = dataFromOpenWeather[i];
+			}
+			catch (Exception ex)
+			{
+				((TextBlock)openWeatherGrid.Children[0]).Text = ex.Message;
+			}
+		}
+
+		private string[] GetData(Weather weather)
+		{
+			return new string[]
+			{
+				"Temperature: " + CheckNull(weather.TemperatureCelsius) + "°C (" + CheckNull(weather.TemperatureFahrenheit) + "°F)",
+				"Wind: " + CheckNull(weather.WindSpeed) + " m/s (" + CheckNull(weather.WindDirection) + ")",
+				"Cloud coverage: " + CheckNull(weather.CloudCover) + "%",
+				"Precipitation: " + CheckNull(weather.Precipitation),
+				"Humidity: " + CheckNull(weather.Humidity) + "%"
+			};
+		}
+
+		private static object CheckNull(object? data)
+		{
+			if (data != null)
+				return data;
+			return "No Data";
+		}
+	}
+}
