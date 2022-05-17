568400eca124da1dc91d3477b9fa26166b1bcf5e
Denisov Alexey <denisov.1881@yandex.ru>
2022-05-17 22:15:28 +0300

    Task 4 has been added.

---
+++ Semester 2/Block 3/Task 4/LibLoader/BotsLibLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using BotsLib;
+using RouletteLib;
+
+namespace LibLoader
+{
+	public class BotsLibLoader
+	{
+		public List<Bot> Bots { get; private set; }
+
+		public bool Load(string path, BetEssence bet, int startCash)
+		{
+			Bots = new List<Bot>();
+
+			Assembly assembly = default;
+			try
+			{
+				assembly = Assembly.LoadFrom(path);
+			}
+			catch (System.IO.FileNotFoundException)
+			{
+				Console.WriteLine("File not found");
+				return false;
+			}
+
+			foreach (Type type in assembly.GetTypes())
+			{
+				if (type.BaseType == typeof(Bot))
+					Bots.Add((Bot)Activator.CreateInstance(type, new object[] { bet, startCash }));
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Semester 2/Block 3/Task 4/RouletteLib/Roulette.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RouletteLib
+{
+	public class Roulette
+	{
+		public bool Spin (BetEssence bet)
+		{
+			Random spinResult = new Random();
+			int number = spinResult.Next(37);
+
+			if (bet.VictoryList.Contains(number))
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
