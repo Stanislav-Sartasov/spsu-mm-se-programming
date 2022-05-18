ee96ede56ae7570c0b89f9c028bdcdba3e75c941
ch3zych3z <chester-vyacheslav@yandex.ru>
2022-05-19 01:51:14 +0300

    Channel refactored

--- VyacheslavBuchin/term2/block4/ElementaryBash/src/main/kotlin/channel/StringChannel.kt
+++ VyacheslavBuchin/term2/block4/ElementaryBash/src/main/kotlin/channel/StringChannel.kt
@@ -1,13 +1,21 @@
 package channel
 
-class StringChannel(val separator: String = System.lineSeparator()) : Channel<String> {
+import java.io.InputStream
+import java.io.OutputStream
 
+class StringChannel(val separator: String = System.lineSeparator()) : Channel<String> {
 	companion object {
 		fun nullChannel() = object : Channel<String> {
+			override val inputStream = "".byteInputStream()
+			override val outputStream = OutputStream.nullOutputStream()
 			override fun read() = ""
 			override fun write(value: String) { }
 		}
 	}
+
+	override val inputStream: InputStream
+		get() = read().byteInputStream()
+	override val outputStream: OutputStream = OutputStream.nullOutputStream()
 
 	private val stringBuilder = StringBuilder()
 
