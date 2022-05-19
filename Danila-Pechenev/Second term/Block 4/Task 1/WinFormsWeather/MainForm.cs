52162b25aef61d47a4c6f7bee9fbe06ff21d5ac7
Danila Pechenev <danil-pechenev@mail.ru>
2022-05-19 19:57:44 +0300

    Add Winforms application

---
+++ Danila-Pechenev/Second term/Block 4/Task 1/WinFormsWeather/MainForm.cs
@@ -0,0 +1,38 @@
+﻿namespace WinFormsWeather;
+using General;
+
+public partial class MainForm : Form
+{
+    private Model model;
+
+    public MainForm()
+    {
+        model = new Model();
+        InitializeComponent();
+    }
+
+    private void usedServiceBox_TextUpdate(object sender, EventArgs e)
+    {
+        if (!usedServiceBox.Items.Contains(usedServiceBox.Text))
+        {
+            errorTextBox.Text = "Please select one of the suggested data sources!";
+        }
+        else
+        {
+            errorTextBox.Text = "";
+        }
+    }
+
+    private void usedServiceBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        errorTextBox.Text = "";
+    }
+
+    private void updateWeatherButton_Click(object sender, EventArgs e)
+    {
+        if (usedServiceBox.Items.Contains(usedServiceBox.Text))
+        {
+            weatherTextBox.Text = model.GetData(usedServiceBox.Text);
+        }
+    }
+}
