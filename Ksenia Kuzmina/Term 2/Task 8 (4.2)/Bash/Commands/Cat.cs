b136d72852f1741e0099c41462c6f08bcb61c7ba
Sayumiko <90759630+sayumikko@users.noreply.github.com>
2022-05-20 18:22:43 +0300

    Added solution for the task 4.2

---
+++ Ksenia Kuzmina/Term 2/Task 8 (4.2)/Bash/Commands/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bash.Commands
+{
+	public class Cat : ICommand
+	{
+		public string[] RunCommand(string[] arguments)
+		{
+			string[] lines = new string[] { };
+
+			for (int i = 0; i < arguments.Length; i++)
+			{
+				try
+				{
+					lines = lines.Concat(File.ReadAllText(arguments[i]).Split(Environment.NewLine)).ToArray();
+				}
+				catch
+				{
+					Console.WriteLine("The file " + arguments[i] +  " doesn't exist.");
+				}
+			}
+
+			return lines;
+		}
+	}
+}
---
+++ Ksenia Kuzmina/Term 2/Task 8 (4.2)/Bash/Commands/Exit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bash.Commands
+{
+	public class Exit : ICommand
+	{
+		private IExiter exiter;
+
+		public Exit(IExiter exiter)
+		{
+			this.exiter = exiter;
+		}
+
+		public string[] RunCommand(string[] arguments)
+		{
+			exiter.Exit();
+			return new string[] { };
+		}
+	}
+}
