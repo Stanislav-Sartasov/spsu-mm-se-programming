faf6090a3e071c5fc582bb78cc7dc80da0544c9b
Danila Pechenev <danil-pechenev@mail.ru>
2022-05-24 23:56:41 +0300

    Add unit tests for Runtime class

---
+++ Danila-Pechenev/Second term/Block 4/Task 2/Tools.UnitTests/RuntimeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tools.UnitTests;
+using NUnit.Framework;
+using Tools;
+
+public class RuntimeTests
+{
+    [Test]
+    public void AddNonexistentVariableTest()
+    {
+        Runtime.AddVariable("name", "Danya");
+        Assert.IsTrue(Runtime.LocalVariables.ContainsKey("name"));
+        Assert.AreEqual("Danya", Runtime.LocalVariables["name"]);
+    }
+
+    [Test]
+    public void AddExistentVariableTest()
+    {
+        Runtime.AddVariable("var", "5");
+        Runtime.AddVariable("var", "7");
+        Assert.IsTrue(Runtime.LocalVariables.ContainsKey("var"));
+        Assert.AreEqual("7", Runtime.LocalVariables["var"]);
+    }
+}
