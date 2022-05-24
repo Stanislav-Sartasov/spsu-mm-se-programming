6ffcdb3ae0cfff58c55f980e6d309eb3e90d932c
Danila Pechenev <danil-pechenev@mail.ru>
2022-05-24 23:56:06 +0300

    Add unit tests for ProgramLauncher

---
+++ Danila-Pechenev/Second term/Block 4/Task 2/Tools.UnitTests/ProgramLauncherTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Tools.UnitTests;
+using NUnit.Framework;
+using Tools;
+
+public class ProgramLauncherTests
+{
+    [Test]
+    public void LaunchProgramTest()
+    {
+        bool success = Programlauncher.TryLaunchProgram("whoami", null, out string text);
+        Assert.IsTrue(success);
+        Assert.IsTrue(text.Length > 0);
+    }
+}
