cc2a3d27e9ef23ef4b7f36fb3522fde90f8e6c94
Danila Pechenev <danil-pechenev@mail.ru>
2022-05-24 23:42:24 +0300

    Add `Runtime.cs`

---
+++ Danila-Pechenev/Second term/Block 4/Task 2/Tools/Runtime.cs
@@ -0,0 +1,18 @@
+﻿namespace Tools;
+
+public static class Runtime
+{
+    public static Dictionary<string, string> LocalVariables { get; private set; } = new Dictionary<string, string>();
+
+    public static void AddVariable(string name, string value)
+    {
+        if (LocalVariables.ContainsKey(name))
+        {
+            LocalVariables[name] = value;
+        }
+        else
+        {
+            LocalVariables.Add(name, value);
+        }
+    }
+}
