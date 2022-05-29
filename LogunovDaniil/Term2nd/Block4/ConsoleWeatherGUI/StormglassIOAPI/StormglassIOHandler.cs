6ea93f6bbc18e534d044f043f6e87686bf122b79
Daniel Logunov <daniel.e.logunov@gmail.com>
2022-05-30 02:14:40 +0300

    added solution for the Task7 from the Block4

---
+++ LogunovDaniil/Term2nd/Block4/ConsoleWeatherGUI/StormglassIOAPI/StormglassIOHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+using WeatherRequesterResourceLibrary;
+using WebWeatherRequester;
+
+namespace StormglassIOAPI
+{
+	public class StormglassIOHandler : AWebServiceAPI
+	{
+		private static string StormSource = "sg";
+
+		public StormglassIOHandler(string key) : base(key)
+		{
+		}
+
+		public override RequestDataContainer GetServiceRequestInfo()
+		{
+			return new RequestDataContainer
+			{
+				ServiceURL = "https://api.stormglass.io/v2/weather/point"
+					+ "?lat=59.9386"
+					+ "&lng=30.3141"
+					+ "&params=windDirection,windSpeed,cloudCover,humidity,airTemperature,precipitation"
+					+ "&source=sg"
+					+ $"&start={DateTimeOffset.Now.ToUnixTimeSeconds()}"
+					+ $"&end={DateTimeOffset.Now.ToUnixTimeSeconds()}",
+				HeaderKeys = new List<string>() { "Authorization" },
+				HeaderValues = new List<string>() { apiKey }
+			};
+		}
+
+		public override WeatherDataContainer? ParseJSONObject(string json)
+		{
+			try
+			{
+				using (JsonDocument doc = JsonDocument.Parse(json))
+				{
+					var weather = doc.RootElement
+						.GetProperty("hours").EnumerateArray().First();
+
+					double temp = weather.GetProperty("airTemperature").GetProperty(StormSource).GetDouble();
+					double precipitation = weather.GetProperty("precipitation").GetProperty(StormSource).GetDouble();
+					double windAngle = weather.GetProperty("windDirection").GetProperty(StormSource).GetDouble();
+					PrecipitationTypes type;
+
+					if (precipitation < 0.005)
+						type = PrecipitationTypes.None;
+					else if (temp > 0)
+						type = PrecipitationTypes.Rain;
+					else
+						type = PrecipitationTypes.Snow;
+
+					return new WeatherDataContainer
+					{
+						SourceName = "StormglassIO",
+
+						TempC = temp,
+						TempF = GeneralConversions.ConvertTempFromCToF(temp),
+						Humidity = (int)weather.GetProperty("humidity").GetProperty(StormSource).GetDouble(),
+						WindSpeed = weather.GetProperty("windSpeed").GetProperty(StormSource).GetDouble(),
+						WindDirection = GeneralConversions.GetDirectionFromAngle(windAngle),
+						PrecipitationVolumetric = precipitation,
+						Precipitation = type,
+						CloudCover = (int)weather.GetProperty("cloudCover").GetProperty(StormSource).GetDouble()
+					};
+				}
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ LogunovDaniil/Term2nd/Block4/ConsoleWeatherGUI/WeatherRequesterResourceLibrary.UnitTests/FetchWeatherLogUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace WeatherRequesterResourceLibrary.UnitTests
+{
+	public class FetchWeatherLogUnitTests
+	{
+		private static FetchWeatherStatus Status = FetchWeatherStatus.Success;
+		private static string Error = "broiler 747 terpit krushenie";
+
+		private FetchWeatherLog Log = new FetchWeatherLog
+		{
+			Status = Status,
+			Message = Error
+		};
+
+		[Test]
+		public void ValueRetentionTest()
+		{
+			Assert.AreEqual(Status, Log.Status);
+			Assert.AreEqual(Error, Log.Message);
+		}
+	}
+}
