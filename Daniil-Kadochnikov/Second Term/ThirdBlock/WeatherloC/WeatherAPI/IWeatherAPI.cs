aedf99cfb2427e481b26ebe501d911ff9cc24b0d
Kadochnikov-Daniil <kadoch.daniil@gmail.com>
2022-06-04 00:41:07 +0300

    Initial commit of the third task from the third block.

---
+++ Daniil-Kadochnikov/Second Term/ThirdBlock/WeatherloC/WeatherAPI/IWeatherAPI.cs
@@ -0,0 +1,14 @@
+﻿using WeatherForecastModel;
+namespace WeatherAPI
+{
+	public interface IWeatherAPI
+	{
+		string Name { get; }
+		string URL { get; }
+		static HttpClient Client { get; }
+
+		Task<string> GetDataAsync();
+
+		WeatherModel GetWeatherModelAsync(string responseString);
+	}
+}
