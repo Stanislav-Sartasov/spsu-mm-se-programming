e57e0bd9389ddef3327ed502c0a1777cd3359b83
Denisov Alexey <denisov.1881@yandex.ru>
2022-09-05 19:24:26 +0300

    Task 8 has been added.

---
+++ Semester 2/Block 4/Task 8/BashLib/Commands/Echo.cs
@@ -0,0 +1,12 @@
+﻿using BashLib.IO;
+
+namespace BashLib.Commands
+{
+	public class Echo : ICommand
+	{
+		public string Run(string[] arguments)
+		{
+			return string.Join(" ", arguments);
+		}
+	}
+}
