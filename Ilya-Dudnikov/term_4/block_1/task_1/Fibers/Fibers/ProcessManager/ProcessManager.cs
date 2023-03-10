09d63c17e2305c039e36bf02cf0e540735eb2a69
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-03-10 20:40:56 +0300

    Implement ProcessManager

--- Ilya-Dudnikov/term_4/block_1/task_1/Fibers/Fibers/ProcessManager/ProcessManager.cs
+++ Ilya-Dudnikov/term_4/block_1/task_1/Fibers/Fibers/ProcessManager/ProcessManager.cs
@@ -1,9 +1,23 @@
-﻿namespace Fibers.ProcessManager;
+﻿using Fibers.Fibers;
+
+namespace Fibers.ProcessManager;
 
 public static class ProcessManager
 {
+    private static FiberScheduler scheduler = new DefaultScheduler();
+
+    public static void Run(List<Process> processes, FiberScheduler? fiberScheduler = null)
+    {
+        scheduler = fiberScheduler ?? scheduler;
+        processes.ForEach(process => scheduler.ScheduleFiber(new Fiber(process.Run)));
+        scheduler.Execute();
+        scheduler.Dispose();
+    }
+
     public static void Switch(bool fiberFinished)
     {
-        // a place for fiber magic
+        if (fiberFinished) scheduler.RemoveRunningFiber();
+
+        scheduler.RunNextFiber();
     }
 }
