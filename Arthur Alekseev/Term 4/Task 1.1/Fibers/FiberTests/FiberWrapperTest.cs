c51fe9a74e94b29ab6fefdb1c25a6fb494e51322
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2023-03-12 00:49:28 +0300

    Uploaded solution for 4.1.1

---
+++ Arthur Alekseev/Term 4/Task 1.1/Fibers/FiberTests/FiberWrapperTest.cs
@@ -0,0 +1,46 @@
+﻿using Fibers.Fibers;
+using Fibers.ProcessManager;
+
+namespace FiberTests;
+
+public class FiberWrapperTest
+{
+	[Test]
+	public void FiberWrapperCreate()
+	{
+		Assert.DoesNotThrow(() =>
+		{
+			Process sampleProcess = new();
+
+			FiberWrapper f = new(sampleProcess);
+
+			Assert.IsNotNull(f);
+
+			Assert.NotNull(f.Id);
+
+			Assert.AreEqual(f.Priority, sampleProcess.Priority);
+		});
+	}
+
+
+
+	[Test]
+	public void FiberWrapperSwitchToPrimary()
+	{
+		Process sampleProcess = new();
+
+		FiberWrapper f = new(sampleProcess);
+
+		Assert.DoesNotThrow(() => { f.SwitchToPrimary(); });
+	}
+
+	[Test]
+	public void FiberWrapperDelete()
+	{
+		Process sampleProcess = new();
+
+		FiberWrapper f = new(sampleProcess);
+
+		Assert.DoesNotThrow(() => { f.Delete(); });
+	}
+}
