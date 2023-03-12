655a7f1e27147dac60d0bfc5a9c731b700610254
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-03-12 18:04:00 +0300

    Add tests

---
+++ Ilya-Dudnikov/term_4/block_1/task_1/Fibers/Fibers.Tests/PrioritySchedulerTests.cs
@@ -0,0 +1,57 @@
+﻿using Fibers.Fibers;
+using Fibers.ProcessManager;
+using static Fibers.ProcessManager.ProcessManager;
+
+namespace Fibers.Tests;
+
+public class PrioritySchedulerTests
+{
+    private FiberScheduler scheduler;
+
+    [SetUp]
+    public void Setup()
+    {
+        scheduler = new PriorityScheduler();
+    }
+
+    [Test]
+    public void ScheduleProcessTestOneProcess()
+    {
+        Run(new List<Process> { new() }, scheduler);
+        Assert.Pass();
+    }
+
+    [Test]
+    public void ScheduleProcessTestFiveProcesses()
+    {
+        var processList = Enumerable.Range(0, 5).Select(_ => new Process()).ToList();
+        Run(processList, scheduler);
+        Assert.Pass();
+    }
+
+    [Test]
+    public void ScheduleProcessTestTenProcesses()
+    {
+        var processList = Enumerable.Range(0, 10).Select(_ => new Process()).ToList();
+        Run(processList, scheduler);
+        Assert.Pass();
+    }
+
+    [Test]
+    public void RemoveProcessOnEmpty()
+    {
+        Assert.Throws<InvalidOperationException>(() => scheduler.RemoveRunningFiber());
+    }
+
+    [Test]
+    public void ExecuteOnEmpty()
+    {
+        Assert.Throws<InvalidOperationException>(() => scheduler.Execute());
+    }
+
+    [Test]
+    public void RunNextFiberOnEmpty()
+    {
+        Assert.Throws<InvalidOperationException>(() => scheduler.RunNextFiber());
+    }
+}
