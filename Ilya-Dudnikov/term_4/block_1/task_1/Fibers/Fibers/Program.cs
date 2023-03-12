4bd9d545be8cad2aba91f5513808fd8e7adf93ea
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-03-12 18:14:19 +0300

    Cosmetics

--- Ilya-Dudnikov/term_4/block_1/task_1/Fibers/Fibers/Program.cs
+++ Ilya-Dudnikov/term_4/block_1/task_1/Fibers/Fibers/Program.cs
@@ -7,13 +7,52 @@ namespace Fibers;
 
 internal class Program
 {
+    private static void HelpMessage()
+    {
+        Console.WriteLine("Usage: first argument is the number of fibers to be ran" + Environment.NewLine +
+                          "second argument is the fiber scheduler to be used. Options for scheduler: " + Environment.NewLine +
+                          "- Default: default fiber scheduler that uses trivial FIFO policy" + Environment.NewLine + 
+                          "- Priority: priority-based scheduling with aging technique" + Environment.NewLine);
+    }
+    
     private static int Main(string[] args)
     {
-        var processList = new List<Process>();
-        for (var i = 0; i < 5; i++) processList.Add(new Process());
+        if (args.Length < 2)
+        {
+            HelpMessage();
+            return 0;
+        }
 
-        Run(processList, new PriorityScheduler());
+        int processesCount;
+        if (int.TryParse(args[0], out processesCount))
+        {
+            var schedulerType = args[1];
+            FiberScheduler scheduler;
 
+            if (schedulerType == "Default")
+            {
+                scheduler = new DefaultScheduler();
+            }
+            else if (schedulerType == "Priority")
+            {
+                scheduler = new PriorityScheduler();
+            }
+            else
+            {
+                HelpMessage();
+                return 0;
+            }
+        
+            var processList = new List<Process>();
+            for (var i = 0; i < processesCount; i++) processList.Add(new Process());
+
+            Run(processList, scheduler);
+        }
+        else
+        {
+            HelpMessage();
+        }
+        
         return 0;
     }
 }
