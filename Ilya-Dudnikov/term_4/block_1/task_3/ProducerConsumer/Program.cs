c735748c8e7133e879fd838e8571b8b0043d1837
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-04-02 19:33:27 +0300

    Implement main class

--- Ilya-Dudnikov/term_4/block_1/task_3/ProducerConsumer/Program.cs
+++ Ilya-Dudnikov/term_4/block_1/task_3/ProducerConsumer/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace ProducerConsumer;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    private const int NumberOfConsumers = 5;
+    private const int NumberOfProducers = 5;
+
+    public static void Main(string[] args)
+    {
+        var manager = new ProducerConsumerManager(NumberOfConsumers, NumberOfProducers);
+
+        Console.WriteLine("Running...");
+        manager.Run();
+
+        Console.ReadKey(true);
+        manager.Stop();
+
+        Console.WriteLine("Stopped!");
+    }
+}
