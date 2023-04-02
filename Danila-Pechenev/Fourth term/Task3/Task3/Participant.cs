ffe3aad8e2033d7872bfc718346e52628df78d21
Danila Pechenev <danil-pechenev@mail.ru>
2023-04-02 15:33:03 +0300

    Initial commit of Task 3

---
+++ Danila-Pechenev/Fourth term/Task3/Task3/Participant.cs
@@ -0,0 +1,32 @@
+﻿namespace Task3;
+
+public abstract class Participant
+{
+    protected static Random random = new Random();
+    protected Thread thread;
+    protected Semaphore semaphore;
+    protected int maxObjectsNumber;
+    protected int pauseBetweenActions;
+    protected List<int> numbers;
+
+    public Participant(Semaphore semaphore, int maxObjectsNumber, int pauseBetweenActions, List<int> numbers)
+    {
+        thread = new Thread(Act);
+        this.semaphore = semaphore;
+        this.maxObjectsNumber = maxObjectsNumber;
+        this.pauseBetweenActions = pauseBetweenActions;
+        this.numbers = numbers;
+    }
+
+    protected abstract void Act();
+
+    protected void StartAction()
+    {
+        thread.Start();
+    }
+
+    protected void FinishAction()
+    {
+        thread.Join();
+    }
+}
---
+++ Danila-Pechenev/Fourth term/Task3/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task3;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        // Determine number of producers and consumers:
+        int producersNumber = 4;
+        int consumersNumber = 4;
+
+        return 0;
+    }
+}
