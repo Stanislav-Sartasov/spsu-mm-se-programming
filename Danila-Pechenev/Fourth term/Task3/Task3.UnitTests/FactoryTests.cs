2db7805b4a40cd87ccb5cc9943392ca45e48b8ac
Danila Pechenev <danil-pechenev@mail.ru>
2023-04-02 19:50:06 +0300

    Add unit tests for factory

---
+++ Danila-Pechenev/Fourth term/Task3/Task3.UnitTests/FactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Task3.UnitTests;
+
+public class FactoryTests
+{
+    [Test]
+    public void RemoveElementTest()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        var factory = new Factory(6, 6, 10);
+
+        factory.Start();
+        Thread.Sleep(3000);
+        stopwatch.Start();
+        factory.Stop();
+        stopwatch.Stop();
+        Assert.Less(stopwatch.ElapsedMilliseconds, 500);
+    }
+}
