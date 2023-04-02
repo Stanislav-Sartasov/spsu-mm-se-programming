f6b051ac5732f8a9f2a2535577a48a297aa7c2c0
Danila Pechenev <danil-pechenev@mail.ru>
2023-04-02 15:42:20 +0300

    Add Consumer and Producer classes

---
+++ Danila-Pechenev/Fourth term/Task3/Task3/Consumer.cs
@@ -0,0 +1,24 @@
+﻿namespace Task3;
+
+public class Consumer : Participant
+{
+    public Consumer(Semaphore semaphore, int maxObjectsNumber, int pauseBetweenActions, List<int> numbers)
+        : base(semaphore, maxObjectsNumber, pauseBetweenActions, numbers)
+    {
+    }
+
+    protected override void Act()
+    {
+        int n = random.Next(1, maxObjectsNumber + 1);
+
+        semaphore.WaitOne();
+
+        n = Math.Min(n, numbers.Count);
+        for (int i = 0; i < n; i++)
+        {
+            numbers.RemoveAt(0);
+        }
+
+        semaphore.Release();
+    }
+}
---
+++ Danila-Pechenev/Fourth term/Task3/Task3/Producer.cs
@@ -0,0 +1,23 @@
+﻿namespace Task3;
+
+public class Producer : Participant
+{
+    public Producer(Semaphore semaphore, int maxObjectsNumber, int pauseBetweenActions, List<int> numbers)
+        : base(semaphore, maxObjectsNumber, pauseBetweenActions, numbers)
+    {
+    }
+
+    protected override void Act()
+    {
+        int n = random.Next(1, maxObjectsNumber + 1);
+
+        semaphore.WaitOne();
+
+        for (int i = 0; i < n; i++)
+        {
+            numbers.Add(random.Next(1000000));
+        }
+
+        semaphore.Release();
+    }
+}
