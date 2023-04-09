2a00ef52344b4d8503c791442eca815279f72a75
Alexander-Zadorozhnyy <sasha.zadorojnii2013@yandex.ru>
2023-04-09 21:31:39 +0300

    Added solution to Task 4/Block 2

---
+++ ZadorozhnyyAlexander/Semester 4/Block 2/ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ThreadPool;
+
+public class Program
+{
+    static Random random = new Random();
+
+    private static Func<string> GenarateTask = () =>
+    {
+        var length = random.Next(100);
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    };
+
+    private static Action GenerateTask = () =>
+    {
+        var task = GenarateTask();
+        Console.WriteLine($"--> Start do task {task}...");
+        Thread.Sleep(random.Next(1000));
+        Console.WriteLine($"--> Task {task} was finished.");
+    };
+
+    public static int Main(string[] args)
+    {
+        try
+        {
+            using (var threadPool = new ThreadPool(10))
+            {
+                for (int i = 0; i < 20; i++)
+                    threadPool.EnqueueTask(GenerateTask);
+
+                Console.ReadKey();
+            }
+
+            Console.ReadKey();
+            return 0;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error occured: {e}");
+            return -1;
+        }
+    }
+}
---
+++ ZadorozhnyyAlexander/Semester 4/Block 2/ThreadPool/ThreadPool/ThreadPool.cs
@@ -0,0 +1,107 @@
+﻿namespace ThreadPool
+{
+    public class ThreadPool : IDisposable
+    {
+        volatile bool isStop = false;
+
+        List<Thread> threads;
+        Queue<(Action<object?> Task, object? Param)> tasks;
+
+        AutoResetEvent isRunningNotification;
+        Semaphore enqueueSemaphore;
+
+        public ThreadPool(int maxThreadsCount)
+        {
+            if (maxThreadsCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(maxThreadsCount), maxThreadsCount, "The number of threads in the pool must be >= 1");
+
+            isRunningNotification = new(true);
+            enqueueSemaphore = new Semaphore(1, 1);
+
+            tasks = new();
+            threads = (from _ in Enumerable.Range(0, maxThreadsCount) select CreateThread()).ToList();
+
+            Console.WriteLine("ThreadPool was created!");
+        }
+
+        private Thread CreateThread()
+        {
+            var thread = new Thread(ThreadWorking);
+            thread.Start();
+            return thread;
+        }
+
+        public void EnqueueTask(Action task) => EnqueueTask(null, _ => task());
+
+        public void EnqueueTask(object? param, Action<object?> task)
+        {
+            if (isStop)
+                throw new InvalidOperationException("Thread pool already destroyed!");
+
+            // Blocking Queue realization by simple Queue and individual semaphore
+            enqueueSemaphore.WaitOne();
+
+            if (isStop)
+                throw new InvalidOperationException("Thread pool already destroyed!");
+            tasks.Enqueue((task, param));
+
+            enqueueSemaphore.Release();
+
+            isRunningNotification.Set();
+        }
+
+        private void ThreadWorking()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} was started...");
+
+            while (!isStop)
+            {
+                isRunningNotification.WaitOne();
+                if (isStop) break; // Check if ThreadPool was stopped while we were waiting
+
+                enqueueSemaphore.WaitOne(); // Requesting access to queues
+
+                while (tasks.Count == 0) // Wait until the tasks appear in queue
+                {
+                    enqueueSemaphore.Release(); // Release the queue for another thread
+                    isRunningNotification.WaitOne(); // Waiting for execution
+                    if (isStop) break;
+
+                    enqueueSemaphore.WaitOne(); // Requesting access to queues
+                }
+
+                var (task, param) = tasks.Dequeue();
+
+                if (tasks.Count > 0) // Check if exist another task in queue and start it
+                    isRunningNotification.Set();
+
+                enqueueSemaphore.Release(); // Allow another thread use queue
+
+                try
+                {
+                    task(param); // Execute task
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error {e} was accured in {Thread.CurrentThread.ManagedThreadId} thread!!!");
+                }
+            }
+
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} was stopped.");
+            isRunningNotification.Set();
+        }
+
+        public void Dispose()
+        {
+            Volatile.Write(ref isStop, true);
+
+            isRunningNotification.Set();
+            foreach (var thread in threads)
+                thread.Join();
+
+            enqueueSemaphore.Dispose();
+            isRunningNotification.Dispose();
+            Console.WriteLine("ThreadPool was destroyed!");
+        }
+    }
+}
