0327698df6d7f6fd945dd6b3c592d974562e856f
SuldinVyacheslav <suldin_slava.03@mail.ru>
2023-04-10 05:35:33 +0300

    task 3 initial commit

---
+++ Suldin_Vyacheslav/FourthTerm/Block2/Task3/Task3/Consumer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Consumer<T> : Participant<T>
+    {
+        public Consumer(List<T> buffer) : base(buffer)
+        {
+        }
+
+        public override void Operate()
+        {
+            while (!stop)
+            {
+                lock (Sync)
+                {
+                    while (buffer.Count == 0)
+                    {
+                        Monitor.Wait(Sync);
+                    }
+                    Consume();
+                    Monitor.PulseAll(Sync);
+                }
+                Thread.Sleep(pause);
+            }
+        }
+
+        public void Consume()
+        {
+            //Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} consumes");
+            buffer.RemoveAt(0);
+        }
+    }
+}
