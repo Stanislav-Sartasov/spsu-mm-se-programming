60bb929081b92a31ee68199e6165e8e40cc2b634
Anton Kraev <antonkraeww@gmail.com>
2023-04-12 01:41:07 +0300

    initial commit of producer-consumer task

---
+++ Anton-Kraev/Term4/ProducerConsumer/ProducerConsumer/Manager.cs
@@ -0,0 +1,45 @@
+﻿namespace ProducerConsumer;
+
+public class Manager<T>: IDisposable
+{
+    private Mutex mutex = new();
+    private Buffer<T> buffer = new();
+    private List<Producer<T>> producers;
+    private List<Consumer<T>> consumers;
+    private List<Thread> threads;
+
+    public Manager(int producersCount, int consumersCount, Func<T> produceFunc, Action<T> consumeAction)
+    {
+        producers = Enumerable.Range(0, producersCount)
+            .Select(_ => new Producer<T>(mutex, buffer, produceFunc))
+            .ToList();
+
+        consumers = Enumerable.Range(0, consumersCount)
+            .Select(_ => new Consumer<T>(mutex, buffer, consumeAction))
+            .ToList();
+
+        threads = producers.Select(producer => new Thread(producer.Run))
+            .Concat(consumers.Select(consumer => new Thread(consumer.Run)))
+            .ToList();
+    }
+
+    public void Start()
+    {
+        threads.ForEach(thread => thread.Start());
+    }
+
+    public void Stop()
+    {
+        producers.ForEach(producer => producer.Stop());
+        consumers.ForEach(consumer => consumer.Stop());
+        threads.ForEach(thread => thread.Join());
+    }
+
+    public void Dispose()
+    {
+        producers.Clear();
+        consumers.Clear();
+        threads.Clear();
+        mutex.Dispose();
+    }
+}
---
+++ Anton-Kraev/Term4/ProducerConsumer/ProducerConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ProducerConsumer;
+
+internal class Program
+{
+    private static int ProduceFunc()
+    {
+        // work emulation
+        Thread.Sleep(100);
+        return 0;
+    }
+
+    private static void ConsumeAction(int item)
+    {
+        // work emulation
+        Thread.Sleep(100);
+    }
+
+    private const int ProducersCount = 4;
+    private const int ConsumersCount = 4;
+
+    public static void Main(string[] args)
+    {
+        var manager = new Manager<int>(ProducersCount, ConsumersCount, ProduceFunc, ConsumeAction);
+
+        manager.Start();
+
+        Console.ReadKey(true);
+
+        manager.Stop();
+        manager.Dispose();
+    }
+}
