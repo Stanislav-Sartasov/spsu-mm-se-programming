254bc7aede3ff2eac06a9f09a76ddc2a7e1a163c
Anton Kraev <antonkraeww@gmail.com>
2023-04-13 21:57:21 +0300

    initial commit of thread pool

---
+++ Anton-Kraev/Term4/ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ThreadPool;
+
+internal class Program
+{
+    public static void Main()
+    {
+        var x1 = DateTime.Now;
+        using (var tp = new ThreadPool())
+        {
+            tp.Enqueue(() => { Console.WriteLine($"das1 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das2 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das3 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das4 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das5 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das6 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das7 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das8 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das9 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+            tp.Enqueue(() => { Console.WriteLine($"das10 on {Thread.CurrentThread.ManagedThreadId}"); Thread.Sleep(500); });
+        }
+        var x2 = DateTime.Now;
+
+        Console.WriteLine(x2 - x1);
+    }
+}
---
+++ Anton-Kraev/Term4/ThreadPool/ThreadPool/ThreadPool.cs
@@ -0,0 +1,69 @@
+﻿namespace ThreadPool;
+
+public class ThreadPool : IDisposable
+{
+    private const int ThreadCount = 4;
+
+    private List<Thread> threads;
+    private Queue<Action> queue = new();
+
+    private volatile bool isEmpty = true;
+    private volatile bool isDisposed = false;
+
+    public ThreadPool()
+    {
+        threads = Enumerable.Range(0, ThreadCount)
+            .Select(_ => new Thread(ThreadMethod))
+            .ToList();
+
+        threads.ForEach(thread => thread.Start());
+    }
+
+    public void Enqueue(Action action)
+    {
+        if (isDisposed) throw new InvalidOperationException(
+            "Error adding a new task to the queue, because the thread pool was destroyed"
+            );
+
+        lock (queue)
+        {
+            queue.Enqueue(action);
+            isEmpty = false;
+        }
+    }
+    
+    private void ThreadMethod()
+    {
+        while (true)
+        {
+            if (isEmpty) continue;
+
+            Action? task = null;
+
+            lock (queue)
+            {
+                if (queue.Any())
+                {
+                    task = queue.Dequeue();
+                }
+                isEmpty = !queue.Any();
+            }
+
+            task?.Invoke();
+
+            if (isDisposed && isEmpty) return;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (isDisposed) return;
+
+        isDisposed = true;
+
+        threads.ForEach(th => th.Join());
+
+        threads.Clear();
+        queue.Clear();
+    }
+}
