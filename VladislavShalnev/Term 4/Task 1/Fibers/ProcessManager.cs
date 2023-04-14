dafac083e1f75424e3642c7ef1447b22bc431d25
VladYoSlav <vaifaer@gmail.com>
2023-04-15 01:00:54 +0300

    Add solution for Term 4 Task 1

---
+++ VladislavShalnev/Term 4/Task 1/Fibers/ProcessManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Fibers
+{
+	public static class ProcessManager
+	{
+		private static readonly List<Fiber> Fibers = new List<Fiber>();
+		private static bool _isPrioMode;
+		private static Fiber _current;
+
+		public static void Start(List<Process> processes, bool prioMode)
+		{
+			foreach (var process in processes)
+				Fibers.Add(new Fiber(process.Run, process.TotalDuration, process.Priority));
+
+			_isPrioMode = prioMode;
+
+			_current = Fibers.First();
+			Fiber.Switch(_current.Id);
+		}
+
+		public static void Switch(bool fiberFinished)
+		{
+			if (fiberFinished)
+			{
+				Console.WriteLine($"Fiber [{_current.Id}] has finished");
+				Fibers.Remove(_current);
+			}
+
+			if (Fibers.Count == 0)
+			{
+				Console.WriteLine("Switching to primary fiber");
+				Fiber.Switch(Fiber.PrimaryId);
+			}
+
+			if (_isPrioMode)
+			{
+				int maxPriority = Fibers.Aggregate(Fibers[0].Priority, (acc, c) => Math.Max(acc, c.Priority));
+
+				var prioFibers = Fibers.FindAll(fiber => fiber.Priority == maxPriority);
+
+				// Get fiber with max priority and min duration
+				_current = prioFibers.Aggregate(prioFibers.First(),
+					(acc, c) => c.TotalDuration < acc.TotalDuration ? c : acc);
+			}
+			else
+			{
+				int index = Fibers.IndexOf(_current);
+				_current = Fibers[(index + 1) % Fibers.Count];
+			}
+
+			Console.WriteLine($"Switching to [{_current.Id}] fiber");
+			Fiber.Switch(_current.Id);
+		}
+	}
+}
