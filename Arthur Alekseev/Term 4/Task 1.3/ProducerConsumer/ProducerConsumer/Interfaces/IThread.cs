d683aa7f11934f78481aa921f8f97d8d4b5e278b
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2023-04-16 22:06:20 +0300

    Add solution for ProducerConsumer task

---
+++ Arthur Alekseev/Term 4/Task 1.3/ProducerConsumer/ProducerConsumer/Interfaces/IThread.cs
@@ -0,0 +1,8 @@
+﻿namespace ProducerConsumer.Interfaces;
+
+public interface IThread
+{
+	public void Start();
+	public void Stop();
+	public void Wait();
+}
---
+++ Arthur Alekseev/Term 4/Task 1.3/ProducerConsumer/ProducerConsumerTests/Logger/StringLogger.cs
@@ -0,0 +1,19 @@
+﻿using ILogger = ProducerConsumer.Interfaces.ILogger;
+
+namespace ProducerConsumerTests.Logger
+{
+	internal class StringLogger : ILogger
+	{
+		public List<string> FullLog;
+
+		public StringLogger()
+		{
+			FullLog = new List<string>();
+		}
+
+		public void Log(string message)
+		{
+			FullLog.Add(message);
+		}
+	}
+}
