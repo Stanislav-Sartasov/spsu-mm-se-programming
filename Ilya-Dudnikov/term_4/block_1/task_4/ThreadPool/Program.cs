0e96535a03c429bfedd778aad04882a1cf5b6e7f
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-04-18 21:38:12 +0300

    Implement main program

--- Ilya-Dudnikov/term_4/block_1/task_4/ThreadPool/Program.cs
+++ Ilya-Dudnikov/term_4/block_1/task_4/ThreadPool/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace ThreadPool;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var threadCount = 5;
+        var taskCount = 100;
+        var random = new Random();
+
+        ThreadPool threadPool = new(threadCount);
+        for (var i = 0; i < taskCount; i++)
+            threadPool.Enqueue(() =>
+            {
+                Thread.Sleep(random.Next(200)); // work emulation
+            });
+
+        Console.WriteLine("Running a thread pool...");
+
+        threadPool.Start();
+        Thread.Sleep(100);
+        threadPool.Dispose();
+
+        Console.WriteLine("Thread pool finished successfully!");
+    }
+}
