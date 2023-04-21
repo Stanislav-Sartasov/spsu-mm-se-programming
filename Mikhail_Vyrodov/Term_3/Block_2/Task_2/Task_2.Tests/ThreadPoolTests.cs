c394d4d03ced1d37185a9b12ede3151bbf30bcaa
Mikhail Vyrodov <m_vyrodov@mail.ru>
2023-04-21 18:08:34 +0300

    Add solution for the 2nd task of 2nd block

---
+++ Mikhail_Vyrodov/Term_3/Block_2/Task_2/Task_2.Tests/ThreadPoolTests.cs
@@ -0,0 +1,67 @@
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace Task_2.Tests
+{
+    public class ThreadPoolTests
+    {
+        private static Random random = new Random();
+
+        private static int completedTaskCounter = 0;
+
+        private static void Task()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} started doing some task.");
+            Thread.Sleep(random.Next(200));
+            completedTaskCounter += 1;
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} finished its task.");
+        }
+
+        [Test]
+        public void ExceptionAfterDisposeTest()
+        {
+            try
+            {
+                completedTaskCounter = 0;
+                ThreadPool threadPool = new ThreadPool(5);
+                for (int i = 0; i < 30; i++)
+                {
+                    threadPool.Enqueue(Task);
+                }
+                Thread.Sleep(30 * 200);
+                threadPool.Dispose();
+                threadPool.Enqueue(Task);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                string message = "Trying to call enqueue method when pool was already disposed.";
+                Assert.AreEqual(e.Message, message);
+            }
+        }
+
+        [Test]
+        public void PoolTest()
+        {
+            try
+            {
+                completedTaskCounter = 0;
+                ThreadPool threadPool = new ThreadPool(5);
+                for (int i = 0; i < 30; i++)
+                {
+                    threadPool.Enqueue(Task);
+                }
+                Thread.Sleep(30 * 200);
+                threadPool.Dispose();
+                Assert.AreEqual(completedTaskCounter, 30);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ Mikhail_Vyrodov/Term_3/Block_2/Task_2/Task_2/ThreadPool.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Action = System.Action;
+
+namespace Task_2
+{   public class ThreadPool : IDisposable
+    {
+        private Queue<Action> tasksQueue;
+        private Thread[] threads; 
+        private volatile bool disposeFlag = false;
+        private volatile int tasksCount = 0;
+
+        private object sync;
+
+        public ThreadPool(int threadsCount)
+        {
+            sync = new object();
+            threads = new Thread[threadsCount];
+            tasksQueue = new Queue<Action>();
+
+            for (int i = 0; i < threadsCount; i++)
+            {
+                threads[i] = new Thread(Run);
+                threads[i].Start();
+            }
+        }
+
+        public void Enqueue(Action a)
+        {
+            if (disposeFlag)
+            {
+                throw new Exception("Trying to call enqueue method when pool was already disposed.");
+            }
+
+            lock (sync)
+            {
+                tasksQueue.Enqueue(a);
+                Interlocked.Increment(ref tasksCount);
+                Monitor.Pulse(sync);
+            }
+        }
+
+        public void Run()
+        {
+            while (!disposeFlag)
+            {
+                lock (sync)
+                {
+                    while (tasksCount == 0 && !disposeFlag)
+                    {
+                        Monitor.Wait(sync);
+                    }
+
+                    if (disposeFlag)
+                    {
+                        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} was stopped.");
+                        return;
+                    }
+
+                    Action task = tasksQueue.Dequeue();
+
+                    task();
+
+                    Interlocked.Decrement(ref tasksCount);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            disposeFlag = true;
+
+            lock (sync)
+            {
+                Monitor.PulseAll(sync);
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            tasksQueue.Clear();
+            Console.WriteLine("Pool was disposed.");
+        }
+    }
+}
