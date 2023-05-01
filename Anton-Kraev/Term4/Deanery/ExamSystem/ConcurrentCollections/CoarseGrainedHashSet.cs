a286bc7abc1e4b79cf3488dc9e0e5c9c0eaf46da
Anton Kraev <antonkraeww@gmail.com>
2023-05-01 23:36:04 +0300

    added first implementation of exam system

--- Anton-Kraev/Term4/Deanery/ExamSystem/ConcurrentCollections/CoarseGrainedHashSet.cs
+++ Anton-Kraev/Term4/Deanery/ExamSystem/ConcurrentCollections/CoarseGrainedHashSet.cs
@@ -1,29 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ExamSystem.Interface;
+﻿namespace ExamSystem.ConcurrentCollections;
 
-namespace ExamSystem.ConcurrentCollections
+public class CoarseGrainedHashSet<T>
 {
-    public class CoarseGrainedHashSetExamSystem : IExamSystem
+    // TODO: RWLock
+
+    private List<T>[] _table;
+    private int _setSize;
+    private readonly Mutex _lock = new();
+
+    public CoarseGrainedHashSet(int capacity)
     {
-        public int Count => throw new NotImplementedException();
-
-        public void Add(long studentId, long courseId)
+        _setSize = 0;
+        _table = new List<T>[capacity];
+        for (int i = 0; i < capacity; i++)
         {
-            throw new NotImplementedException();
+            _table[i] = new List<T>();
         }
+    }
 
-        public bool Contains(long studentId, long courseId)
-        {
-            throw new NotImplementedException();
-        }
+    private bool PolicyDemandsResize => _setSize / _table.Length > 4;
 
-        public void Remove(long studentId, long courseId)
+    // TODO: remove unused arg
+    private void Acquire(T x) => _lock.WaitOne();
+    private void Release(T x) => _lock.ReleaseMutex();
+
+    private void Resize()
+    {
+        int oldCapacity = _table.Length;
+        _lock.WaitOne();
+        try
         {
-            throw new NotImplementedException();
+            if (oldCapacity != _table.Length)
+            {
+                return; // someone beat us to it
+            }
+            int newCapacity = 2 * oldCapacity;
+            List<T>[] oldTable = _table;
+            _table = new List<T>[newCapacity];
+            for (int i = 0; i < newCapacity; i++)
+                _table[i] = new List<T>();
+            foreach (List<T> bucket in oldTable)
+            {
+                foreach (T x in bucket)
+                {
+                    _table[x.GetHashCode() % _table.Length].Add(x);
+                }
+            }
         }
+        finally
+        {
+            _lock.ReleaseMutex();
+        }
+    }
+
+    public bool Contains(T x)
+    {
+        Acquire(x);
+        try
+        {
+            int myBucket = Math.Abs(x.GetHashCode() % _table.Length);
+            return _table[myBucket].Contains(x);
+        }
+        finally
+        {
+            Release(x);
+        }
+    }
+    public bool Add(T x)
+    {
+        bool result = false;
+        Acquire(x);
+        try
+        {
+            int myBucket = Math.Abs(x.GetHashCode() % _table.Length);
+            if (!_table[myBucket].Contains(x))
+            {
+                _table[myBucket].Add(x);
+                result = true;
+                _setSize++;
+            }
+        }
+        finally
+        {
+            Release(x);
+        }
+        if (PolicyDemandsResize)
+            Resize();
+        return result;
     }
 }
