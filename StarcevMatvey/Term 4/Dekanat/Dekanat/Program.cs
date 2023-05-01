71f6877d70c1fd3f1a6f3881aded510e17b04674
Startsev <tozarin@mail.ru>
2023-05-01 06:07:07 +0300

    Clears programm
    Signed-off-by: Startsev <tozarin@mail.ru>

--- StarcevMatvey/Term 4/Dekanat/Dekanat/Program.cs
+++ StarcevMatvey/Term 4/Dekanat/Dekanat/Program.cs
@@ -6,15 +6,6 @@ namespace Dekanat
     {
         public static void Main(string[] args)
         {
-            var table = new StripedHashSet(4);
-
-            table.Add(1, 1);
-
-            Console.WriteLine(table.Count());
-
-            table.Remove(1, 1);
-
-            Console.WriteLine(table.Count());
 
         }
     }
