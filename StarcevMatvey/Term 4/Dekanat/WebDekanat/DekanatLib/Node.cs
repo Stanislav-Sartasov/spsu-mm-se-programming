925a0b5d55691fd5cd4a3d6d4cf838a36e986106
Startsev <tozarin@mail.ru>
2023-05-01 21:22:23 +0300

    Adds web
    Signed-off-by: Startsev <tozarin@mail.ru>

---
+++ StarcevMatvey/Term 4/Dekanat/WebDekanat/DekanatLib/Node.cs
@@ -0,0 +1,37 @@
+﻿namespace Dekanat.DekanatLib
+{
+    public class Node : IEquatable<Node>
+    {
+        public long StudentId { get; private set; }
+        public long CourseId { get; private set; }
+
+        public Node(long studentId, long courseId)
+        {
+            StudentId = studentId;
+            CourseId = courseId;
+        }
+
+        public Node WithStudentId(long studentId)
+        {
+            return new Node(studentId, CourseId);
+        }
+
+        public Node WithCourseId(long courseId)
+        {
+            return new Node(StudentId, courseId);
+        }
+
+        public override bool Equals(object o) => this.Equals(o as Node);
+
+        public bool Equals(Node node)
+        {
+            if (node == null) return false;
+
+            if (Object.ReferenceEquals(this, node)) return true;
+
+            if (this.GetType() != node.GetType()) return false;
+
+            return this.StudentId == node.StudentId && this.CourseId == node.CourseId;
+        }
+    }
+}
