c7413b3f61622267be2f1ead596b32e892a8d093
Danila Pechenev <danil-pechenev@mail.ru>
2023-05-02 09:25:20 +0300

    Initial commit of the 6th task (implement AParticipant)

---
+++ Danila-Pechenev/Fourth term/Task6/ChatParticipants/AParticipant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChatParticipants
+{
+    public abstract class AParticipant : IDisposable
+    {
+        public abstract string IP { get; protected set; }
+
+        public abstract int Port { get; protected set; }
+
+        public abstract void Start();
+
+        public abstract void Dispose();
+
+        protected abstract void Listen();
+    }
+}
