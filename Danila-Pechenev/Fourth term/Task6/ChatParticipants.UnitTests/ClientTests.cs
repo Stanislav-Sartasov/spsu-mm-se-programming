c0b27d4a5609a966a3b7ec9d2781af4301ff9c2a
Danila Pechenev <danil-pechenev@mail.ru>
2023-05-02 09:29:05 +0300

    Implement unit tests for client

---
+++ Danila-Pechenev/Fourth term/Task6/ChatParticipants.UnitTests/ClientTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Threading;
+
+namespace ChatParticipants.UnitTests
+{
+    public class ClientTests
+    {
+        [Test]
+        public void CreateAndDisposeClientTest()
+        {
+            var client = new Client("127.0.0.1", 4040, Mock);
+            client.Start();
+            Thread.Sleep(500);
+            client.Dispose();
+
+            Assert.Pass();
+        }
+
+        private void Mock(string message)
+        {
+        }
+    }
+}
