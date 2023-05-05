bace5ff75d23ea7fca78774db9bce1b3b7000712
IvanShurenkov <ivan.shurenkov@yandex.com>
2023-05-05 20:15:56 +0300

    Solution of Task 5

---
+++ IvanShurenkov/Fourth term/Block3/Task5/Task5/AtomicTypes/IAtomicMarkableReference.cs
@@ -0,0 +1,10 @@
+﻿namespace Task5.AtomicTypes;
+
+public interface IAtomicMarkableReference<T>
+{
+    bool CompareAndSet(T expectedReference, T newReference, bool expectedMark, bool newMark);
+    //bool AttemptMark(T expectedReference, bool newMark);
+    T Get(out bool marked);
+    //void Set(T newReference, bool newMark);
+    T GetReference();
+}
