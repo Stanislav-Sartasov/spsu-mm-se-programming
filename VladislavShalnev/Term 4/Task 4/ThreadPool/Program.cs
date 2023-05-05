2b3dad6866f939356ce0a2497927de10157e46b1
VladYoSlav <vaifaer@gmail.com>
2023-05-05 22:03:22 +0300

    Add solution for Term 4 Task 4

---
+++ VladislavShalnev/Term 4/Task 4/ThreadPool/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ThreadPool;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("This program implements a thread pool.");
+
+		var task = () =>
+		{
+			Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} has started task");
+			Thread.Sleep(new Random().Next(1000));
+			Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} has finished task");
+		};
+
+		var threadPool = new ThreadPool();
+
+		for (int i = 0; i < 10; i++)
+			threadPool.Enqueue(task);
+
+		Thread.Sleep(10000);
+
+		threadPool.Dispose();
+	}
+}
