23705036f205d92580974dfbd16847b00a47f9db
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-05-05 07:44:21 +0300

    Add Core Cockoo hash set
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

---
+++ Ilya-Pankratov/Fourth term/Block 3/Exam/ExamLib/HashTables/CoreCockooHashSet.cs
@@ -0,0 +1,64 @@
+namespace ExamLib;
+
+public class CoreCockooHashSet<T> : APhasedCuckooHashSet<T>
+{
+    private Mutex mutex = new();
+        
+    public CoreCockooHashSet(int size) : base(size)
+    {
+    }
+
+    public CoreCockooHashSet(int size, IEqualityComparer<T> comparer) : base(size, comparer)
+    {
+    }
+
+    protected override void Acquire(T x)
+    {
+        mutex.WaitOne();
+    }
+
+    protected override void Release(T x)
+    {
+        mutex.ReleaseMutex();
+    }
+
+    protected override void Resize()
+    {
+        var oldCapacity = capacity;
+        mutex.WaitOne();
+        try
+        {
+            if (capacity != oldCapacity)
+            {
+                return;
+            }
+
+            var oldTable = table;
+            capacity = 2 * capacity;
+            table = new List<T>[2, capacity];
+
+            for (var i = 0; i < 2; i++)
+            {
+                for (var j = 0; j < capacity; j++)
+                {
+                    table[i, j] = new List<T>(ListSize);
+                }
+            }
+
+            for (var i = 0; i < 2; i++)
+            {
+                for (var j = 0; j < oldCapacity; j++)
+                {
+                    foreach (var z in oldTable[i, j])
+                    {
+                        Add(z);
+                    }
+                }
+            }
+        }
+        finally
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+}
