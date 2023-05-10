c346f0447e55d82a7118f074caace98a5b8ed0c1
Azim Muradov <azim.muradov.dev@gmail.com>
2023-05-11 00:58:30 +0300

    Update build scripts

--- AzimMuradov/Term4/Block4/Task6/app/build.gradle.kts
+++ AzimMuradov/Term4/Block4/Task6/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm")
-
     id("org.jetbrains.compose") version "1.4.0"
 }
 
@@ -10,13 +9,9 @@ dependencies {
 
     implementation(compose.desktop.currentOs)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
 
-    implementation("io.github.microutils:kotlin-logging-jvm:2.1.20")
-    implementation("org.slf4j:slf4j-log4j12:2.0.7")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 }
 
 
--- AzimMuradov/Term4/Block4/Task6/common/build.gradle.kts
+++ AzimMuradov/Term4/Block4/Task6/common/build.gradle.kts
@@ -7,14 +7,11 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
-    implementation("io.github.microutils:kotlin-logging-jvm:2.1.20")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
     implementation("org.slf4j:slf4j-log4j12:2.0.7")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 }
 
 
