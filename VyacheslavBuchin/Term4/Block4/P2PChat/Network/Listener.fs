2a7201c5ac711fbe97b8aee400a7811d8394aead
chez <chester-vyacheslav@yandex.ru>
2023-05-11 22:52:27 +0300

    [task 6] Chat added

---
+++ VyacheslavBuchin/Term4/Block4/P2PChat/Network/Listener.fs
@@ -0,0 +1,47 @@
+namespace Network
+
+open System
+open System.IO
+open System.Threading
+open System.Threading.Tasks
+open SocketExtension
+
+open System.Net
+open System.Net.Sockets
+
+
+type NetworkStreamReader(socket: Socket, token : CancellationToken) =
+    member this.ReadExactlyAsync n =
+        async {
+            let buffer = Array.zeroCreate<byte> n
+            let mutable alreadyReadCount = 0
+            while alreadyReadCount <> n do
+                let! count =
+                    let buffer = Memory<byte>(buffer, alreadyReadCount, n - alreadyReadCount)
+                    socket.ReceiveAsync(buffer, SocketFlags.None, token).AsTask()
+                    |> Async.AwaitTask
+                alreadyReadCount <- count + alreadyReadCount
+            return buffer
+        }
+
+type Listener (address, port, maxConnectionQueueSize, token : CancellationToken, processClient: NetworkStreamReader -> Async<unit>) =
+    member listener.Run(): unit =
+        let thread =
+            (fun () ->
+                async {
+                    use socket = Socket.TcpListenerOf address port
+                    socket.Listen(maxConnectionQueueSize)
+                    while true do
+                        let! client = socket.AcceptAsync(token).AsTask() |> Async.AwaitTask
+                        let processClient () =
+                            async {
+                                let stream = NetworkStreamReader(client, token)
+                                while not token.IsCancellationRequested do
+                                    do! processClient stream
+                            }
+                            |> tryIo
+                            |> Async.RunSynchronously
+                        Thread(processClient).Start()
+                } |> tryIo |> Async.RunSynchronously
+            ) |> Thread
+        thread.Start()
