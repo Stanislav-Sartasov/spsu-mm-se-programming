635ea9fd16e85c0c4f971ee6bf8ca675707ed55c
Azim Muradov <azim.muradov.dev@gmail.com>
2023-05-11 21:56:36 +0300

    Add more tests for utils

--- AzimMuradov/Term4/Block4/Task6/common/src/test/kotlin/chat/UtilsTest.kt
+++ AzimMuradov/Term4/Block4/Task6/common/src/test/kotlin/chat/UtilsTest.kt
@@ -1,6 +1,11 @@
 package chat
 
+import org.junit.jupiter.api.RepeatedTest
+import java.net.ServerSocket
+import java.net.Socket
+import kotlin.concurrent.thread
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
@@ -15,5 +20,36 @@ class UtilsTest {
     @Test
     fun `try to run code and get null value due to exception`() {
         assertNull(tryOrNull<Int> { error("") })
+    }
+
+    @RepeatedTest(10)
+    fun `test socket reader`() {
+        val c: Char
+        ServerSocket(5000).use {
+            Socket("localhost", 5000).use {
+                it.getOutputStream().write('a'.code)
+            }
+            it.accept().use { socket ->
+                c = socket.reader().read().toChar()
+            }
+        }
+        assertEquals(expected = 'a', actual = c)
+    }
+
+    @Test
+    fun `test socket printer`() {
+        var c: Char? = null
+        ServerSocket(5000).use {
+            val thread = thread {
+                Socket("localhost", 5000).use { socket ->
+                    c = socket.getInputStream().read().toChar()
+                }
+            }
+            it.accept().use { socket ->
+                socket.printer().println('a')
+            }
+            thread.join()
+        }
+        assertEquals(expected = 'a', actual = c)
     }
 }
