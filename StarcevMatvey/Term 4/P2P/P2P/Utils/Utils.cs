4748bcf3010404aa02d9b0ae22a54e72b6a68b16
Startcev <tozarin@mail.ru>
2023-05-13 22:23:44 +0300

    Adds utils
    Signed-off-by: Startcev <tozarin@mail.ru>

---
+++ StarcevMatvey/Term 4/P2P/P2P/Utils/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P.Utils
+{
+    public static class Utils
+    {
+        public static int GetPositiveInt(string s)
+        {
+            int rez;
+
+            if (Int32.TryParse(s, out rez)) return rez > 0 ? rez : 0;
+
+            return 0;
+        }
+    }
+}
