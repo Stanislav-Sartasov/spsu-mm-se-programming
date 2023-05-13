e0a2307cdb711874c55a61d399685774d2609ff0
Startcev <tozarin@mail.ru>
2023-05-13 19:25:59 +0300

    Adds NET folder with Lisenter and Connection classes
    Signed-off-by: Startcev <tozarin@mail.ru>

---
+++ StarcevMatvey/Term 4/P2P/P2P/Net/Connect.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P.Net
+{
+    public class Connect : IDisposable
+    {
+        const int MESSENGE_LENGTH = 1024; // bytes
+
+        private readonly Socket _socket;
+
+        public Connect(IPEndPoint peerEndPoint)
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Connect(peerEndPoint);
+        }
+
+        public Connect(Socket socket)
+        {
+            _socket = socket;
+        }
+
+        public Connect WithSocket(Socket socket) => new Connect(socket);
+
+        public IPEndPoint LocalEndPoint => (IPEndPoint)_socket.LocalEndPoint;
+        public IPEndPoint RemoteEndPoint => (IPEndPoint)_socket.RemoteEndPoint;
+
+        public void Send(string messenge)
+        {
+            Console.WriteLine($"I am sending \"{messenge}\"");
+
+            var mes = Encoding.UTF8.GetBytes(messenge);
+            mes = mes.Length <= MESSENGE_LENGTH ? mes : mes.Take(MESSENGE_LENGTH).ToArray();
+
+            if (_socket.Send(mes) <= 0) throw new Exception("I could't send your messenge T.T");
+        }
+
+        public string Receive()
+        {
+            byte[] mes = new byte[MESSENGE_LENGTH];
+            var getted = _socket.Receive(mes);
+
+            if (getted <= 0) throw new Exception("I can't get yout messenge >.<");
+
+            var messenge = Encoding.UTF8.GetString(mes, 0, getted);
+            Console.WriteLine($"I get your messenge \"{messenge}\"");
+
+            return messenge;
+        }
+
+        public void Close()
+        {
+            _socket.Close();
+        }
+
+        public void Dispose()
+        {
+            Close();
+            _socket.Dispose();
+        }
+    }
+}
---
+++ StarcevMatvey/Term 4/P2P/P2P/Net/Listener.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace P2P.Net
+{
+    public class Listener : IDisposable
+    {
+        private readonly Socket _socket;
+        private readonly IPEndPoint _peerEndPoint;
+
+        public Listener(int port)
+        {
+            _peerEndPoint = new IPEndPoint(IPAddress.Any, port);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public Listener(Socket socket, IPEndPoint peerEndPoint)
+        {
+            _socket = socket;
+            _peerEndPoint = peerEndPoint;
+        }
+
+        public Listener WithSocket(Socket socket) => new Listener(socket, _peerEndPoint);
+
+        public Listener WithEndpoint(IPEndPoint peerEndPoint) => new Listener(_socket, peerEndPoint);
+
+        public void Start()
+        {
+            _socket.Bind(_peerEndPoint);
+            _socket.Listen(1000);
+        }
+
+        public Connect Accept() => new Connect(_socket.Accept());
+
+        public void Close()
+        {
+            _socket.Close();
+        }
+
+        public void Dispose()
+        {
+            Close();
+            _socket.Dispose();
+        }
+    }
+}
