4a94e5b24cee98a1320b0b5eb89453ffbf449216
Danila Pechenev <danil-pechenev@mail.ru>
2023-05-20 19:18:01 +0300

    Fix connection bug

--- Danila-Pechenev/Fourth term/Task6/ChatParticipants/Client.cs
+++ Danila-Pechenev/Fourth term/Task6/ChatParticipants/Client.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System;
+using System.Linq;
 
 namespace ChatParticipants
 {
@@ -44,7 +45,30 @@ namespace ChatParticipants
         public void Start(string participantIP, int participantPort)  // Join an existing chat
         {
             var participantEndPoint = new IPEndPoint(IPAddress.Parse(participantIP), participantPort);
-            socket.SendTo(Encoding.UTF8.GetBytes($"JOIN;{IP};{Port}"), participantEndPoint);
+
+            socket.SendTo(Encoding.UTF8.GetBytes($"JOIN1;{IP};{Port}"), participantEndPoint);
+
+            // Get number of chat users
+            var buffer = new byte[Constants.MessageBufferSize];
+            int size = socket.Receive(buffer);
+            string message = Encoding.UTF8.GetString(buffer, 0, size);
+            int usersNumber = int.Parse(message);
+
+            // Get users' addresses
+            buffer = new byte[100 * usersNumber];
+            size = socket.Receive(buffer);
+            message = Encoding.UTF8.GetString(buffer, 0, size);
+            if (message != "")
+            {
+                string[] words = message.Split(new char[] { ';' });
+                IPEndPoint currentEndPoint;
+                for (int i = 0; i < words.Length; i += 2)
+                {
+                    currentEndPoint = new IPEndPoint(IPAddress.Parse(words[i]), int.Parse(words[i + 1]));
+                    socket.SendTo(Encoding.UTF8.GetBytes($"JOIN;{IP};{Port}"), currentEndPoint);
+                    clientEndPoints.Add($"{words[i]};{words[i + 1]}", currentEndPoint);
+                }
+            }
 
             clientEndPoints.Add($"{participantIP};{participantPort}", participantEndPoint);
 
@@ -121,7 +145,17 @@ namespace ChatParticipants
                     continue;
                 }
 
-                if (words[0] == "JOIN")
+                if (words[0] == "JOIN1")
+                {
+                    endPoint = new IPEndPoint(IPAddress.Parse(words[1]), int.Parse(words[2]));
+
+                    socket.SendTo(Encoding.UTF8.GetBytes($"{clientEndPoints.Count}"), endPoint);
+                    string endPointsMessage = string.Join(";", clientEndPoints.Keys.ToArray());
+                    socket.SendTo(Encoding.UTF8.GetBytes(endPointsMessage), endPoint);
+
+                    clientEndPoints.Add(address, endPoint);
+                }
+                else if (words[0] == "JOIN")
                 {
                     endPoint = new IPEndPoint(IPAddress.Parse(words[1]), int.Parse(words[2]));
                     clientEndPoints.Add(address, endPoint);
@@ -136,16 +170,6 @@ namespace ChatParticipants
                 else if (words[0] == "MESSAGE")
                 {
                     callback(words[3]);
-
-                    foreach (var item in clientEndPoints)
-                    {
-                        if (item.Key == address)
-                        {
-                            continue;
-                        }
-
-                        socket.SendTo(Encoding.UTF8.GetBytes($"MESSAGE;{IP};{Port};{words[3]}"), item.Value);
-                    }
                 }
                 else
                 {
