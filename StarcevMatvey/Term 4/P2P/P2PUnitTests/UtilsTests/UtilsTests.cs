89837f8b3d237c530e256faaf969f82ca6db27a1
Startcev <tozarin@mail.ru>
2023-05-25 11:25:33 +0300

    Adds try port
    Signed-off-by: Startcev <tozarin@mail.ru>

--- StarcevMatvey/Term 4/P2P/P2PUnitTests/UtilsTests/UtilsTests.cs
+++ StarcevMatvey/Term 4/P2P/P2PUnitTests/UtilsTests/UtilsTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using P2P.Utils;
+using System.Net;
 
 namespace P2PUnitTests
 {
@@ -12,6 +13,16 @@ namespace P2PUnitTests
             Assert.AreEqual(0, Utils.GetPositiveInt("0"));
             Assert.AreEqual(0, Utils.GetPositiveInt("-1"));
             Assert.AreEqual(0, Utils.GetPositiveInt("odin"));
+        }
+
+        [Test]
+        public void TryPort()
+        {
+            Assert.Null(Utils.TryPort($"funny_port_228"));
+
+            var p = $"127.0.0.1:1111";
+            var adr = IPEndPoint.Parse($"127.0.0.1:1111");
+            Assert.AreEqual(adr, Utils.TryPort(p));
         }
     }
 }
