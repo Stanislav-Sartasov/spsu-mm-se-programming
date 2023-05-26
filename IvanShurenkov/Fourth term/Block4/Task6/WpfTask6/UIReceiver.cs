36d1df7fa3caee2e8ac5bb1f2795b400b000454a
IvanShurenkov <ivan.shurenkov@yandex.com>
2023-05-26 10:48:24 +0300

    Solution of task6

---
+++ IvanShurenkov/Fourth term/Block4/Task6/WpfTask6/UIReceiver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Task6.Network;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfTask6
+{
+    internal class UIReceiver : IReceiver
+    {
+        public string Text = String.Empty;
+
+        void IReceiver.Connect(EndPoint endPoint, string status)
+        {
+            lock (Text)
+            {
+                if (status == "Y\n")
+                    Text += String.Format("Connect to {0}\n", endPoint);
+                else
+                    Text += String.Format("Not connected\n");
+            }
+        }
+
+        void IReceiver.Receive(IPAddress remoteIP, string text)
+        {
+            lock (Text)
+            {
+                Text += String.Format("{0}: {1}", remoteIP, text);
+            }
+        }
+
+        void IReceiver.Send(string text)
+        {
+            lock (Text)
+            {
+                Text += String.Format("Me: {0}\n", text);
+            }
+        }
+    }
+}
