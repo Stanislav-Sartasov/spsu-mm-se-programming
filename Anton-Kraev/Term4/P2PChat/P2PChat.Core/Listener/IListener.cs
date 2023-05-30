00168014b07d85247083a04dcaa9dc2bcfc3607c
Anton Kraev <antonkraeww@gmail.com>
2023-05-30 16:13:27 +0300

    implemented chat core

--- Anton-Kraev/Term4/P2PChat/P2PChat.Core/Listener/IListener.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace P2PChat.Core.Listener;
-
-public interface IListener
-{ 
-    public void Start();
-    public void Stop();
-}
---
+++ Anton-Kraev/Term4/P2PChat/P2PChat.Core/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace P2PChat.Core;
+
+public record Message(MessageType Type, IPEndPoint Sender, string? Text = null)
+{
+    public byte[] Serialize()
+    {
+        return JsonSerializer.SerializeToUtf8Bytes(this);
+    }
+
+    public static Message? Deserialize(byte[] data)
+    {
+        return JsonSerializer.Deserialize<Message>(data);
+    }
+
+    public override string ToString()
+    {
+        return "";
+    }
+}
