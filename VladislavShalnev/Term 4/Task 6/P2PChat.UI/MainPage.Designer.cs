ebfc2fa8d05604eba65472c0c0cb14b17f507788
VladYoSlav <vaifaer@gmail.com>
2023-05-31 05:39:40 +0300

    Add solution for Term 4 Task 6

---
+++ VladislavShalnev/Term 4/Task 6/P2PChat.UI/MainPage.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace P2PChat.UI;
+
+partial class MainPage
+{
+	/// <summary>
+	///  Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	/// <summary>
+	///  Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && (components != null))
+		{
+			components.Dispose();
+		}
+
+		base.Dispose(disposing);
+	}
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	///  Required method for Designer support - do not modify
+	///  the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{
+            this.msgBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.chatBox = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msgBox
+            // 
+            this.msgBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.msgBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.msgBox.Location = new System.Drawing.Point(11, 409);
+            this.msgBox.Name = "msgBox";
+            this.msgBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.msgBox.Size = new System.Drawing.Size(641, 27);
+            this.msgBox.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Location = new System.Drawing.Point(658, 408);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(130, 28);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "Отправить";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.OnSendButtonClick);
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 82.72841F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.27159F));
+            this.tableLayoutPanel.Controls.Add(this.msgBox, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.sendButton, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.chatBox, 0, 0);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 1);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(8);
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.17002F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.829978F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(799, 447);
+            this.tableLayoutPanel.TabIndex = 2;
+            // 
+            // chatBox
+            // 
+            this.chatBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tableLayoutPanel.SetColumnSpan(this.chatBox, 2);
+            this.chatBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chatBox.FormattingEnabled = true;
+            this.chatBox.ItemHeight = 20;
+            this.chatBox.Items.AddRange(new object[] {
+            "Введите порт, на котором будет работать чат:"});
+            this.chatBox.Location = new System.Drawing.Point(11, 11);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.chatBox.Size = new System.Drawing.Size(777, 391);
+            this.chatBox.TabIndex = 2;
+            // 
+            // MainPage
+            // 
+            this.AcceptButton = this.sendButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "MainPage";
+            this.Text = "Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnClosing);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+	}
+
+    #endregion
+
+    private TextBox msgBox;
+    private Button sendButton;
+    private TableLayoutPanel tableLayoutPanel;
+    private ListBox chatBox;
+}
---
+++ VladislavShalnev/Term 4/Task 6/P2PChat/Chat.cs
@@ -0,0 +1,167 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace P2PChat;
+
+public class Chat : IDisposable
+{
+	private volatile bool _isStopped;
+
+	private readonly Socket _acceptor;
+
+	private readonly Thread _acceptorThread;
+	private readonly List<Thread> _listeners = new();
+	private readonly Dictionary<IPEndPoint, Connection> _participants = new();
+
+	// Locker for participants and OnEvent execution
+	private readonly object _locker = new();
+
+	public delegate void EventHandler(ChatEvent e, IPEndPoint sender, string? payload);
+
+	public event EventHandler? OnEvent;
+
+	public Chat(int port)
+	{
+		var endPoint = new IPEndPoint(IPAddress.Any, port);
+
+		_acceptor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		_acceptor.Bind(endPoint);
+		_acceptor.Listen();
+
+		_acceptorThread = new Thread(AcceptIncoming);
+		_acceptorThread.Start();
+	}
+
+	private void AddToChat(IPEndPoint endPoint, Connection conn)
+	{
+		var listener = new Thread(() => Listen(endPoint, conn));
+		_listeners.Add(listener);
+		listener.Start();
+
+		lock (_locker)
+		{
+			_participants.Add(endPoint, conn);
+		}
+	}
+
+	private void AcceptIncoming()
+	{
+		while (!_isStopped)
+		{
+			try
+			{
+				var conn = new Connection(_acceptor.Accept());
+
+				string port = conn.Receive();
+
+				// Setting the right port
+				var endPoint = IPEndPoint.Parse($"{conn.RemoteEndPoint!.Address}:{port}");
+
+				lock (_locker)
+				{
+					conn.Send(_participants.Count > 0 ? string.Join(' ', _participants.Keys) : "NO");
+				}
+
+				AddToChat(endPoint, conn);
+				OnEvent?.Invoke(ChatEvent.Connect, endPoint, null);
+			}
+			catch
+			{
+				if (!_isStopped) throw;
+			}
+		}
+	}
+
+	private void Listen(IPEndPoint endPoint, Connection conn)
+	{
+		while (!_isStopped)
+		{
+			try
+			{
+				string received = conn.Receive();
+				OnEvent?.Invoke(ChatEvent.Message, endPoint, received);
+			}
+			catch (SocketException e)
+			{
+				lock (_locker)
+				{
+					switch (e.SocketErrorCode)
+					{
+						case SocketError.ConnectionReset:
+							_participants.Remove(endPoint);
+							OnEvent?.Invoke(ChatEvent.Disconnect, endPoint, null);
+							break;
+						default:
+							if (!_isStopped)
+								OnEvent?.Invoke(ChatEvent.Error, endPoint, e.Message);
+							break;
+					}
+				}
+
+				break;
+			}
+		}
+	}
+
+	private string InnerConnect(int selfPort, IPEndPoint endPoint)
+	{
+		var conn = new Connection(endPoint);
+		conn.Send(selfPort.ToString());
+		var received = conn.Receive();
+		AddToChat(endPoint, conn);
+
+		return received;
+	}
+
+	public void Connect(IPEndPoint endPoint)
+	{
+		int selfPort = ((IPEndPoint) _acceptor.LocalEndPoint!).Port;
+
+		lock (_locker)
+		{
+			if (_participants.Count > 0)
+				throw new Exception("You're already connected to some chat");
+
+			if (endPoint.Port == selfPort)
+				throw new Exception("Can't connect to myself");
+
+			if (_participants.Keys.Contains(endPoint))
+				throw new Exception($"Already connected to {endPoint}");
+		}
+
+		string addresses = InnerConnect(selfPort, endPoint);
+
+		if (addresses != "NO")
+		{
+			foreach (var address in addresses.Split())
+				InnerConnect(selfPort, IPEndPoint.Parse(address));
+		}
+
+		OnEvent?.Invoke(ChatEvent.Connect, endPoint, null);
+	}
+
+	public void Send(string msg)
+	{
+		lock (_locker)
+		{
+			OnEvent?.Invoke(ChatEvent.Message, (IPEndPoint) _acceptor.LocalEndPoint!, msg);
+
+			foreach (var conn in _participants.Values)
+				conn.Send(msg);
+		}
+	}
+
+	public void Dispose()
+	{
+		_isStopped = true;
+
+		_acceptor.Close();
+		_acceptorThread.Join();
+
+		foreach (var conn in _participants.Values)
+			conn.Close();
+
+		foreach (var listener in _listeners)
+			listener.Join();
+	}
+}
