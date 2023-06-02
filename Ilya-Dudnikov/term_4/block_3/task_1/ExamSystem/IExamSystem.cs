4df544ec01f6e38da05e99be1a2a045e1c9e670c
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-06-02 21:56:43 +0300

    Implement `ExamSystem`

---
+++ Ilya-Dudnikov/term_4/block_3/task_1/ExamSystem/IExamSystem.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamSystem;
+
+public interface IExamSystem
+{
+    public void Add(long studentId, long courseId);
+
+    public void Remove(long studentId, long courseId);
+
+    public bool Contains(long studentId, long courseId);
+    
+    public int Count { get; }
+}
